216c15547df35dff8844c864fd4f17d755b13d29
Gabe Kangas <gabek@real-ity.com>
2020-06-01 17:47:53 -0700

    Use relative path to websocket

--- webroot/js/main.js
+++ webroot/js/main.js
@@ -4,7 +4,7 @@ define(
 		"MessageList"
 	],
 	function(MessageList) {
-		var ws = new WebSocket("ws://localhost:8080/entry");
+		var ws = new WebSocket("ws://" + location.host + "/entry");
 		var list = new MessageList(ws);
 		ko.applyBindings(list);
 	}
