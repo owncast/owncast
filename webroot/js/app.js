4553ae628bde5faa9e6e60f3752e8f208b7bd022
Gabe Kangas <gabek@real-ity.com>
2020-06-13 22:36:12 -0700

    Move to videojs and point to remote video on goth.land

--- webroot/js/app.js
+++ webroot/js/app.js
@@ -109,37 +109,6 @@ getStatus()
 setupWebsocket()
 // setInterval(getStatus, 5000)
 
-// HLS Video setup.  Commented out for local html work.  Uncomment to make HLS work.
-// const video = document.getElementById("video")
-// const videoSrc = "hls/stream.m3u8"
-// if (Hls.isSupported()) {
-//   var hls = new Hls()
-//   hls.loadSource(videoSrc)
-//   hls.attachMedia(video)
-//   hls.on(Hls.Events.MANIFEST_PARSED, function () {
-//     video.play()
-//   });
-// }
-// // hls.js is not supported on platforms that do not have Media Source
-// // Extensions (MSE) enabled.
-// //
-// // When the browser has built-in HLS support (check using `canPlayType`),
-// // we can provide an HLS manifest (i.e. .m3u8 URL) directly to the video
-// // element through the `src` property. This is using the built-in support
-// // of the plain video element, without using hls.js.
-// //
-// // Note: it would be more normal to wait on the 'canplay' event below however
-// // on Safari (where you are most likely to find built-in HLS support) the
-// // video.src URL must be on the user-driven white-list before a 'canplay'
-// // event will be emitted; the last video event that can be reliably
-// // listened-for when the URL is not on the white-list is 'loadedmetadata'.
-// else if (video.canPlayType("application/vnd.apple.mpegurl")) {
-//   video.src = videoSrc
-//   video.addEventListener("loadedmetadata", function () {
-//     video.play()
-//   });
-// }
-
 function scrollSmoothToBottom(id) {
   const div = document.getElementById(id);
   $('#' + id).animate({
