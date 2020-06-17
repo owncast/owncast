431c1a54e72a7b214d0bc0e19e458aa1f14c27e5
Gabe Kangas <gabek@real-ity.com>
2020-06-17 16:35:47 -0700

    Split out status polling into own file

---
+++ webroot/js/status.js
@@ -0,0 +1,31 @@
+async function getStatus() {
+  const url = "/status";
+
+  try {
+    const response = await fetch(url);
+    const status = await response.json();
+
+    if (!app.isOnline && status.online) {
+      // The stream was offline, but now it's online.  Force start of playback after an arbitrary
+      // delay to make sure the stream has actual data ready to go.
+      setTimeout(function () {
+        restartPlayer();
+      }, 3000)
+
+    }
+
+    app.streamStatus = status.online
+      ? "Stream is online."
+      : "Stream is offline."
+
+    app.viewerCount = status.viewerCount;
+    app.sessionMaxViewerCount = status.sessionMaxViewerCount;
+    app.overallMaxViewerCount = status.overallMaxViewerCount;
+    app.isOnline = status.online;
+
+  } catch (e) {
+    app.streamStatus = "Stream server is offline."
+    app.viewerCount = 0
+  }
+
+}
