f20d8b3179aa1c0035f9f21bdaa07c1b02b0b62f
Gabe Kangas <gabek@real-ity.com>
2020-06-17 17:48:33 -0700

    Troubleshoot player not getting ready event on production server

--- webroot/js/player/player.js
+++ webroot/js/player/player.js
@@ -1,12 +1,11 @@
 // style hackings
 window.VIDEOJS_NO_DYNAMIC_STYLE = true;
 
-var waitingTimeoutTimer;
-
 // Wait until the player is setup before we start polling status
 const player = videojs('video');
 
 player.on('ready', function () {
+  console.log('Player ready.')
   getStatus();
   setInterval(getStatus, 5000);
   setupPlayerEventHandlers();
@@ -16,41 +15,36 @@ function setupPlayerEventHandlers() {
   const player = videojs('video');
 
   player.on('error', function (e) {
-    console.log(e);
+    console.log("Player error: ", e);
   })
 
-  player.on('loadeddata', function (e) {
-    console.log("loadeddata");
-  })
+  // player.on('loadeddata', function (e) {
+  //   console.log("loadeddata");
+  // })
 
-  player.on('ended', function (e) {
-    console.log("ended");
-  })
-
+  // player.on('ended', function (e) {
+  //   console.log("ended");
+  // })
+  //
   // player.on('abort', function (e) {
   //   console.log("abort");
   // })
-
+  //
   // player.on('durationchange', function (e) {
   //   console.log("durationchange");
   // })
-
+  //
   // player.on('stalled', function (e) {
   //   console.log("stalled");
   // })
-  
-  player.on('playing', function (e) {
-    // console.log("playing");
-    clearTimeout(waitingTimeoutTimer);
-  })
-
-  player.on('waiting', function (e) {
-    // console.log("waiting");
-
-    // waitingTimeoutTimer = setTimeout(function () {
-    //   restartPlayer();
-    // }, 3000)
-  })
+  //
+  // player.on('playing', function (e) {
+  //   // console.log("playing");
+  // })
+  //
+  // player.on('waiting', function (e) {
+  //   // console.log("waiting");
+  // })
 }
 
 function restartPlayer() {
