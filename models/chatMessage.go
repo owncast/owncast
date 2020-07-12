d7a2ffcca45304ca76b3f2195bcf135fee27c6cc
Gabe Kangas <gabek@real-ity.com>
2020-07-11 19:08:47 -0700

    Add timestamp to messages.  For #26

--- models/chatMessage.go
+++ models/chatMessage.go
@@ -1,14 +1,17 @@
 package models
 
+import "time"
+
 //ChatMessage represents a single chat message
 type ChatMessage struct {
 	ClientID string `json:"-"`
 
-	Author      string `json:"author"`
-	Body        string `json:"body"`
-	Image       string `json:"image"`
-	ID          string `json:"id"`
-	MessageType string `json:"type"`
+	Author      string    `json:"author"`
+	Body        string    `json:"body"`
+	Image       string    `json:"image"`
+	ID          string    `json:"id"`
+	MessageType string    `json:"type"`
+	Timestamp   time.Time `json:"timestamp"`
 }
 
 //Valid checks to ensure the message is valid
