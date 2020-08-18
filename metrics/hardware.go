11c4df8ddff3a185e7961b9ad5fb64aabffb7616
Gabe Kangas <gabek@real-ity.com>
2020-08-17 22:14:44 -0700

    Limit max number of hardware values collected

--- metrics/hardware.go
+++ metrics/hardware.go
@@ -5,7 +5,14 @@ import (
 	"github.com/shirou/gopsutil/mem"
 )
 
+// Max number of metrics we want to keep.
+const maxCollectionValues = 500
+
 func collectCPUUtilization() {
+	if len(Metrics.CPUUtilizations) > maxCollectionValues {
+		Metrics.CPUUtilizations = Metrics.CPUUtilizations[1:]
+	}
+
 	value, err := cpu.Percent(0, false)
 	if err != nil {
 		panic(err)
@@ -15,6 +22,10 @@ func collectCPUUtilization() {
 }
 
 func collectRAMUtilization() {
+	if len(Metrics.RAMUtilizations) > maxCollectionValues {
+		Metrics.RAMUtilizations = Metrics.RAMUtilizations[1:]
+	}
+
 	value, _ := mem.VirtualMemory()
 	Metrics.RAMUtilizations = append(Metrics.RAMUtilizations, int(value.UsedPercent))
 }
