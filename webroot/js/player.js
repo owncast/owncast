dc29ddbffb538934bf0734645669d03e35d7a0e9
Ruxton <ruxton@rhythmandpoetry.net>
2020-08-25 10:21:06 +0800

    Whitespace cleanup

--- webroot/js/player.js
+++ webroot/js/player.js
@@ -137,26 +137,26 @@ class OwncastPlayer {
       if (window.WebKitPlaybackTargetAvailabilityEvent) {
         var videoJsButtonClass = videojs.getComponent('Button');
         var concreteButtonClass = videojs.extend(videoJsButtonClass, {
-    
-          // The `init()` method will also work for constructor logic here, but it is 
+
+          // The `init()` method will also work for constructor logic here, but it is
           // deprecated. If you provide an `init()` method, it will override the
           // `constructor()` method!
           constructor: function () {
             videoJsButtonClass.call(this, player);
           }, // notice the comma
-    
+
           handleClick: function () {
             const videoElement = document.getElementsByTagName('video')[0];
             videoElement.webkitShowPlaybackTargetPicker();
           }
         });
-    
+
         var concreteButtonInstance = this.vjsPlayer.controlBar.addChild(new concreteButtonClass());
         concreteButtonInstance.addClass("vjs-airplay");
       }
-    });  
+    });
   }
-  
+
 
 }
 
