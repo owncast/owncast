63bb6479900e388463e7c71cdec912938919285d
Gabe Kangas <gabek@real-ity.com>
2020-09-24 23:50:01 -0700

    Created a fake chat message generator script for testing

--- test/README.md
+++ test/README.md
@@ -6,3 +6,12 @@
 1. Start an instance of the server on localhost.
 1. `artillery run httpGetTest.yaml` for endpoint load tests.
 1. `artillery run websocketTest.yaml` for websocket load tests.
+
+
+## Chat test
+
+This will send automated fake chat messages to your localhost instance.
+Edit the messages, usernames or point to a different instance.
+
+1. `npm install`
+1. `node fakeChat.js`
---
+++ test/fakeChat.js
@@ -0,0 +1,61 @@
+const usernames = [
+  'User ' + Math.floor(Math.random() * 100),
+  'User ' + Math.floor(Math.random() * 100),
+  'User ' + Math.floor(Math.random() * 100),
+  'User ' + Math.floor(Math.random() * 100),
+];
+
+const messages = [
+  'I am a test message',
+  'this is fake',
+  'i write emoji 😀',
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+  'Sed pulvinar proin gravida hendrerit. Mauris in aliquam sem fringilla ut morbi tincidunt augue. In cursus turpis massa tincidunt dui.',
+  'Feugiat in ante metus dictum at tempor commodo ullamcorper. Nunc aliquet bibendum enim facilisis gravida neque convallis a. Vitae tortor condimentum lacinia quis vel eros donec ac odio.',
+];
+
+var availableMessages = messages.slice();
+
+const WebSocket = require('ws');
+
+const ws = new WebSocket('ws://localhost:8080/entry', {
+  origin: 'http://localhost:8080',
+});
+
+ws.on('open', function open() {
+  setTimeout(sendMessage, 4000);
+});
+
+ws.on('error', function incoming(data) {
+  console.log(data);
+});
+
+function sendMessage() {
+  if (availableMessages.length == 0) {
+    availableMessages = messages.slice();
+  }
+
+  const id = Math.random().toString(36).substring(7);
+  const username = usernames[Math.floor(Math.random() * usernames.length)];
+  const messageIndex = Math.floor(Math.random() * availableMessages.length);
+  const message = availableMessages[messageIndex];
+  availableMessages.splice(messageIndex, 1);
+
+  const testMessage = {
+    author: username,
+    body: message,
+    image: 'https://robohash.org/' + username,
+    id: id,
+    type: 'CHAT',
+    visible: true,
+    timestamp: new Date().toString(),
+  };
+
+  ws.send(JSON.stringify(testMessage));
+
+  const nextMessageTimeout = (Math.floor(Math.random() * (15 - 3)) + 3) * 1000;
+  setTimeout(sendMessage, nextMessageTimeout);
+}
+
+
+
