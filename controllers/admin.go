db31a32fd38bca7e2e0b5e48840c9677e4262681
Gabe Kangas <gabek@real-ity.com>
2020-09-28 18:37:59 -0700

    Add a simple success response to API requests

--- controllers/admin.go
+++ controllers/admin.go
@@ -3,11 +3,17 @@ package controllers
 import (
 	"net/http"
 
+	"github.com/gabek/owncast/core"
 	"github.com/gabek/owncast/core/rtmp"
 )
 
 // DisconnectInboundConnection will force-disconnect an inbound stream
 func DisconnectInboundConnection(w http.ResponseWriter, r *http.Request) {
+	if !core.GetStatus().Online {
+		writeSimpleResponse(w, false, "no inbound stream connected")
+		return
+	}
+
 	rtmp.Disconnect()
-	w.WriteHeader(http.StatusOK)
+	writeSimpleResponse(w, true, "inbound stream disconnected")
 }
