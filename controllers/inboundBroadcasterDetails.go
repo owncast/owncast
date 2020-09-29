149f39b418280c01e0f64e672b286fc51d376791
Gabe Kangas <gabek@real-ity.com>
2020-09-29 15:32:22 -0700

    Add /api/admin/broadcaster endpoint

---
+++ controllers/inboundBroadcasterDetails.go
@@ -0,0 +1,37 @@
+package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/gabek/owncast/core"
+	"github.com/gabek/owncast/models"
+	"github.com/gabek/owncast/router/middleware"
+)
+
+// GetInboundBroadasterDetails gets the details of the inbound broadcaster
+func GetInboundBroadasterDetails(w http.ResponseWriter, r *http.Request) {
+	middleware.EnableCors(&w)
+
+	broadcaster := core.GetBroadcaster()
+	if broadcaster == nil {
+		writeSimpleResponse(w, false, "no broadcaster connected")
+		return
+	}
+
+	response := inboundBroadasterDetailsResponse{
+		models.BaseAPIResponse{
+			true,
+			"",
+		},
+		broadcaster,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+
+type inboundBroadasterDetailsResponse struct {
+	models.BaseAPIResponse
+	Broadcaster *models.Broadcaster `json:"broadcaster"`
+}
