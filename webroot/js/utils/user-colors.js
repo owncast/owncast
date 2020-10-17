f4815e331cad0db78220173060dafdf19b09faf4
Gabe Kangas <gabek@real-ity.com>
2020-10-16 17:36:11 -0700

    Update chat styling

--- webroot/js/utils/user-colors.js
+++ webroot/js/utils/user-colors.js
@@ -6,9 +6,25 @@ export function messageBubbleColorForString(str) {
   }
 
   // Tweak these to adjust the result of the color
-  const saturation = 75;
-  const lightness = 65;
-  const alpha = 1.0;
+  const saturation = 25;
+  const lightness = 45;
+  const alpha = 0.3;
+  const hue = parseInt(Math.abs(hash), 16) % 360;
+
+  return `hsla(${hue}, ${saturation}%, ${lightness}%, ${alpha})`;
+}
+
+export function textColorForString(str) {
+  let hash = 0;
+  for (let i = 0; i < str.length; i++) {
+    // eslint-disable-next-line
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+
+  // Tweak these to adjust the result of the color
+  const saturation = 80;
+  const lightness = 80;
+  const alpha = 0.8;
   const hue = parseInt(Math.abs(hash), 16) % 360;
 
   return `hsla(${hue}, ${saturation}%, ${lightness}%, ${alpha})`;
