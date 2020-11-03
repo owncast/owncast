9e78a5ea5bcac16d5a2cca4b69333acfe069d74e
Gabe Kangas <gabek@real-ity.com>
2020-11-02 16:20:13 -0800

    Update admin bundler to work with binary bundling

--- scripts/bundleAdmin.sh
+++ scripts/bundleAdmin.sh
@@ -1,13 +1,40 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2059
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+INSTALL_TEMP_DIRECTORY="$(mktemp -d)"
 PROJECT_SOURCE_DIR=$(pwd)
-mkdir $TMPDIR/admin 2> /dev/null
-cd $TMPDIR/admin
+cd $INSTALL_TEMP_DIRECTORY
+
+shutdown () {
+  rm -rf "$PROJECT_SOURCE_DIR/admin"
+  rm -rf "$INSTALL_TEMP_DIRECTORY"
+}
+trap shutdown INT TERM ABRT EXIT
+
+echo "Cloning owncast admin into $INSTALL_TEMP_DIRECTORY..."
 git clone --depth 1 https://github.com/owncast/owncast-admin 2> /dev/null
 cd owncast-admin
+
+echo "Installing npm modules for the owncast admin..."
 npm --silent install 2> /dev/null
+
+echo "Building owncast admin..."
+rm -rf .next
 (node_modules/.bin/next build && node_modules/.bin/next export) | grep info
+
+echo "Copying admin to project directory..."
 ADMIN_BUILD_DIR=$(pwd)
 cd $PROJECT_SOURCE_DIR
-mkdir webroot/admin 2> /dev/null
-cd webroot/admin
+mkdir -p admin 2> /dev/null
+cd admin
 cp -R ${ADMIN_BUILD_DIR}/out/* .
-rm -rf $TMPDIR/admin
+
+echo "Bundling admin into owncast codebase..."
+~/go/bin/pkger
+
+shutdown
+echo "Done."
