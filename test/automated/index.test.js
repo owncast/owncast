6bff7a4ad0a9e874f75deca6440ca6a937726604
Gabe Kangas <gabek@real-ity.com>
2020-11-19 15:24:37 -0800

    Split out different test suites

--- test/automated/index.test.js
+++ test/automated/index.test.js
@@ -9,50 +9,13 @@ test('service is online', (done) => {
         });
 });
 
-
-test('stream details are correct', (done) => {
-    request.get('/api/admin/status').auth('admin', 'abc123').expect(200)
+test('frontend configuration is correct', (done) => {
+    request.get('/api/config').expect(200)
         .then((res) => {
-            expect(res.body.broadcaster.streamDetails.width).toBe(320);
-            expect(res.body.broadcaster.streamDetails.height).toBe(180);
-            expect(res.body.broadcaster.streamDetails.framerate).toBe(24);
-            expect(res.body.broadcaster.streamDetails.videoBitrate).toBe(1269);
-            expect(res.body.broadcaster.streamDetails.videoCodec).toBe('H.264');
-            expect(res.body.broadcaster.streamDetails.audioCodec).toBe('AAC');
-            expect(res.body.online).toBe(true);
+            expect(res.body.title).toBe('Owncast');
+            expect(res.body.logo).toBe('/img/logo.svg');
+            expect(res.body.socialHandles[0].platform).toBe('github');
+            expect(res.body.socialHandles[0].url).toBe('http://github.com/owncast/owncast');
             done();
         });
 });
-
-test('configuration is correct', (done) => {
-    request.get('/api/admin/serverconfig').auth('admin', 'abc123').expect(200)
-        .then((res) => {
-            expect(res.body.instanceDetails.name).toBe('Owncast');
-            expect(res.body.instanceDetails.title).toBe('Owncast');
-            expect(res.body.instanceDetails.summary).toBe('This is brief summary of whom you are or what your stream is. You can edit this description in your config file.');
-            expect(res.body.instanceDetails.logo).toBe('/img/logo.svg');
-            expect(res.body.instanceDetails.tags).toStrictEqual(['music', 'software', 'streaming']);
-
-            expect(res.body.videoSettings.segmentLengthSeconds).toBe(4);
-            expect(res.body.videoSettings.numberOfPlaylistItems).toBe(5);
-
-            expect(res.body.videoSettings.videoQualityVariants[0].framerate).toBe(24);
-            expect(res.body.videoSettings.videoQualityVariants[0].encoderPreset).toBe('veryfast');
-
-            expect(res.body.videoSettings.numberOfPlaylistItems).toBe(5);
-
-            expect(res.body.yp.enabled).toBe(false);
-            expect(res.body.streamKey).toBe('abc123');
-            done();
-        });
-});
-
-
-test('correct number of log entries exist', (done) => {
-    request.get('/api/admin/logs').auth('admin', 'abc123').expect(200)
-        .then((res) => {
-            expect(res.body).toHaveLength(4);
-            done();
-        });
-});
-
