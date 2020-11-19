4f365b2d4db44bafe8c01cef81f978b9d9909c4e
Gabe Kangas <gabek@real-ity.com>
2020-11-19 13:19:00 -0800

    Add comments

--- test/automated/run.sh
+++ test/automated/run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# Install the node test framework
 npm install --silent > /dev/null
 
+# Download a specific version of ffmpeg
 if [ ! -d "ffmpeg" ]; then
   mkdir ffmpeg
   pushd ffmpeg > /dev/null
@@ -13,18 +15,21 @@ fi
 
 pushd ../.. > /dev/null
 
+# If there is no existing config file then copy the default.
 if [ ! -f "config.yaml" ]; then
   cp config-example.yaml config.yaml
 fi
 
+# Build and run owncast from source
 go build -o owncast main.go pkged.go
-
 ./owncast &
 SERVER_PID=$!
 
 popd > /dev/null
 sleep 5
 
+# Start streaming the test file over RTMP to
+# the local owncast instance.
 ffmpeg -hide_banner -loglevel panic -re -i test.mp4 -vcodec libx264 -profile:v main -sc_threshold 0 -b:v 1300k -acodec copy -f flv rtmp://127.0.0.1/live/abc123 &
 FFMPEG_PID=$!
 
@@ -35,4 +40,5 @@ trap finish EXIT
 
 sleep 15
 
+# Run the tests against the instance.
 npm test
