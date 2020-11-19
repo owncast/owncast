bc142665f6197298917565e814c0fb786b5f6587
Gabe Kangas <gabek@real-ity.com>
2020-11-19 12:48:37 -0800

    Use already downloaded repo to build. Do not re-clone

--- test/automated/run.sh
+++ test/automated/run.sh
@@ -2,21 +2,22 @@
 
 npm install --silent > /dev/null
 
-# Check out a fresh copy of owncast if required.
-# If running under a Github workflow it will have already been cloned.
-if [ ! -d "owncast" ]; then
-  git clone https://github.com/owncast/owncast
-fi
-
-pushd owncast > /dev/null
-
-cp config-default.yaml config.yaml
-go build -o owncast main.go pkged.go
-
 if [ ! -d "ffmpeg" ]; then
+  mkdir ffmpeg
+  pushd ffmpeg > /dev/null
   curl -sL https://github.com/vot/ffbinaries-prebuilt/releases/download/v4.2.1/ffmpeg-4.2.1-linux-64.zip --output ffmpeg.zip > /dev/null
   unzip -o ffmpeg.zip > /dev/null
+  PATH=$PATH:$(pwd)
+  popd > /dev/null
 fi
+
+pushd ../.. > /dev/null
+
+if [ ! -f "config.yaml" ]; then
+  cp config-example.yaml config.yaml
+fi
+
+go build -o owncast main.go pkged.go
 
 ./owncast &
 SERVER_PID=$!
@@ -24,7 +25,7 @@ SERVER_PID=$!
 popd > /dev/null
 sleep 5
 
-./owncast/ffmpeg -hide_banner -loglevel panic -re -i test.mp4 -vcodec libx264 -profile:v main -sc_threshold 0 -b:v 1300k -acodec copy -f flv rtmp://127.0.0.1/live/abc123 &
+ffmpeg -hide_banner -loglevel panic -re -i test.mp4 -vcodec libx264 -profile:v main -sc_threshold 0 -b:v 1300k -acodec copy -f flv rtmp://127.0.0.1/live/abc123 &
 FFMPEG_PID=$!
 
 function finish {
