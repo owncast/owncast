71b0f2b4e93f1ebfee6c07382642ce71445a9ad1
SeaLife <mircotries@hotmail.de>
2020-12-23 22:39:05 +0100

    Added new endpoints to modify settings in-memory

--- router/router.go
+++ router/router.go
@@ -61,6 +61,15 @@ func Start() error {
 	// Change the current streaming key in memory
 	http.HandleFunc("/api/admin/changekey", middleware.RequireAdminAuth(admin.ChangeStreamKey))
 
+	// Change the current streaming name in memory
+	http.HandleFunc("/api/admin/changename", middleware.RequireAdminAuth(admin.ChangeStreamName))
+
+	// Change the current streaming name in memory
+	http.HandleFunc("/api/admin/changetitle", middleware.RequireAdminAuth(admin.ChangeStreamTitle))
+
+	// Change the current streaming name in memory
+	http.HandleFunc("/api/admin/changetags", middleware.RequireAdminAuth(admin.ChangeStreamTags))
+
 	// Change the extra page content in memory
 	http.HandleFunc("/api/admin/changeextrapagecontent", middleware.RequireAdminAuth(admin.ChangeExtraPageContent))
 
