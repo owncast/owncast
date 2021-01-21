80e98b50df7fe0ac6f212c31c268eaf576ec780a
Gabe Kangas <gabek@real-ity.com>
2021-01-21 12:30:07 -0800

    Add yp/reset to api spec. return status in response

--- controllers/admin/yp.go
+++ controllers/admin/yp.go
@@ -3,6 +3,7 @@ package admin
 import (
 	"net/http"
 
+	"github.com/owncast/owncast/controllers"
 	"github.com/owncast/owncast/core/data"
 	log "github.com/sirupsen/logrus"
 )
@@ -11,5 +12,8 @@ func ResetYPRegistration(w http.ResponseWriter, r *http.Request) {
 	log.Traceln("Resetting YP registration key")
 	if err := data.SetDirectoryRegistrationKey(""); err != nil {
 		log.Errorln(err)
+		controllers.WriteSimpleResponse(w, false, err.Error())
+		return
 	}
+	controllers.WriteSimpleResponse(w, true, "reset")
 }
