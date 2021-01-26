a531e965d7f3c0512737dc7875016827af654c5d
Gabe Kangas <gabek@real-ity.com>
2021-01-26 00:25:23 -0800

    Return social handles with YP data. First half of https://github.com/owncast/owncast-yp/issues/28

--- yp/api.go
+++ yp/api.go
@@ -5,21 +5,23 @@ import (
 	"net/http"
 
 	"github.com/owncast/owncast/core/data"
+	"github.com/owncast/owncast/models"
 	"github.com/owncast/owncast/utils"
 	log "github.com/sirupsen/logrus"
 )
 
 type ypDetailsResponse struct {
-	Name                  string   `json:"name"`
-	Description           string   `json:"description"`
-	StreamTitle           string   `json:"streamTitle,omitempty"`
-	Logo                  string   `json:"logo"`
-	NSFW                  bool     `json:"nsfw"`
-	Tags                  []string `json:"tags"`
-	Online                bool     `json:"online"`
-	ViewerCount           int      `json:"viewerCount"`
-	OverallMaxViewerCount int      `json:"overallMaxViewerCount"`
-	SessionMaxViewerCount int      `json:"sessionMaxViewerCount"`
+	Name                  string                `json:"name"`
+	Description           string                `json:"description"`
+	StreamTitle           string                `json:"streamTitle,omitempty"`
+	Logo                  string                `json:"logo"`
+	NSFW                  bool                  `json:"nsfw"`
+	Tags                  []string              `json:"tags"`
+	Online                bool                  `json:"online"`
+	ViewerCount           int                   `json:"viewerCount"`
+	OverallMaxViewerCount int                   `json:"overallMaxViewerCount"`
+	SessionMaxViewerCount int                   `json:"sessionMaxViewerCount"`
+	Social                []models.SocialHandle `json:"social"`
 
 	LastConnectTime utils.NullTime `json:"lastConnectTime"`
 }
@@ -50,6 +52,7 @@ func GetYPResponse(w http.ResponseWriter, r *http.Request) {
 		OverallMaxViewerCount: status.OverallMaxViewerCount,
 		SessionMaxViewerCount: status.SessionMaxViewerCount,
 		LastConnectTime:       status.LastConnectTime,
+		Social:                data.GetSocialHandles(),
 	}
 
 	w.Header().Set("Content-Type", "application/json")
