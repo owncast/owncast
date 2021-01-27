4fac736844062f83c7b7aeb22a7ec5bf82d4d2d9
Gabe Kangas <gabek@real-ity.com>
2021-01-26 22:15:27 -0800

    Start modeling out codecs

--- core/transcoder/codecs.go
+++ core/transcoder/codecs.go
@@ -7,6 +7,39 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+// ./ffmpeg -hwaccel qsv -c:v h264_qsv -i ~/Videos/BigBuckBunny.mp4 -vf hwdownload,format=nv12 -pix_fmt yuv420p -c:v h264_qsv test.mp4
+
+// ffmpeg -y -loglevel debug -init_hw_device qsv=hw -filter_hw_device hw -hwaccel qsv -hwaccel_output_format qsv \
+//-i simpsons.mp4 -vf 'format=qsv,hwupload=extra_hw_frames=64'  \
+//-c:v h264_qsv \
+//-bf 3 -b:v 15M -maxrate:v 15M -bufsize:v 2M -r:v 30 -c:a copy -f mp4 trolled.mp4
+
+type QuickSync struct {
+	BaseCodec
+}
+
+func (c *QuickSync) Name() string {
+	return "Intel QuickSync"
+}
+
+type Nvenc struct {
+	BaseCodec
+}
+
+func (c *Nvenc) Name() string {
+	return "NVENC"
+}
+
+type Codec interface {
+	Name() string
+	ToString() string
+}
+
+type BaseCodec struct {
+	Bitrate int
+	Quality string
+}
+
 func GetCodecs(ffmpegPath string) []string {
 	codecs := make([]string, 0)
 
