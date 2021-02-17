d180e61b63cf8269a6404aade155664b1b95caf1
Gabe Kangas <gabek@real-ity.com>
2021-02-17 12:20:32 -0800

    Fix comment

--- controllers/constants.go
+++ controllers/constants.go
@@ -1,7 +1,7 @@
 package controllers
 
-// POST is the HTTP POST method
+// POST is the HTTP POST method.
 const POST = "POST"
 
-// GET is the HTTP GET method
+// GET is the HTTP GET method.
 const GET = "GET"
