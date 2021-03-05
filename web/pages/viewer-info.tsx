5ff5665652a5677d96e85f0c45c71f4aff4f122e
gabek <gabek@users.noreply.github.com>
2021-03-05 09:02:10 +0000

    Prettified Code!

--- web/pages/viewer-info.tsx
+++ web/pages/viewer-info.tsx
@@ -124,12 +124,18 @@ export default function ViewersOverTime() {
       </Row>
 
       <Chart title="Viewers" data={viewerInfo} color="#2087E2" unit="" />
-      {online && 
+      {online && (
         <div>
           <Table dataSource={clients} columns={columns} rowKey={row => row.clientID} />
-          <p><Typography.Text type="disabled">Visit the <a href="https://owncast.online/docs/viewers/?source=admin">documentation</a> to configure additional details bout your viewers.</Typography.Text> </p>
+          <p>
+            <Typography.Text type="disabled">
+              Visit the{' '}
+              <a href="https://owncast.online/docs/viewers/?source=admin">documentation</a> to
+              configure additional details bout your viewers.
+            </Typography.Text>{' '}
+          </p>
         </div>
-      }
+      )}
     </>
   );
 }
