18678d203765907cf51b651a2b1f0a1c717873fe
Gabe Kangas <gabek@real-ity.com>
2021-03-20 23:44:45 -0700

    Update log message

--- core/transcoder/utils.go
+++ core/transcoder/utils.go
@@ -14,7 +14,7 @@ var errorMap = map[string]string{
 	"Unable to parse option value":              "you are likely trying to utilize a specific codec, but your version of ffmpeg or your hardware doesn't support it. either fix your ffmpeg install or try changing your codec to libx264 and restart your stream",
 	"OpenEncodeSessionEx failed: out of memory": "your NVIDIA gpu is limiting the number of concurrent stream qualities you can support. remove a stream output variant and try again.",
 	"Cannot use rename on non file protocol, this may lead to races and temporary partial files": "",
-	"No VA display found for device": "vaapi not configured. either your hardware does not support it or you need to install additional drivers for your hardware such as i965-va-driver and/or libvdpau-va-gl1",
+	"No VA display found for device": "vaapi not enabled. either your copy of ffmpeg does not support it, your hardware does not support it, or you need to install additional drivers for your hardware.",
 	"Unrecognized option":            "error with codec. if your copy of ffmpeg or your hardware does not support your selected codec you may need to select another",
 }
 
