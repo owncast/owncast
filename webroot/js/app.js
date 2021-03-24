320bd95b0f2a34675792de7124ea2a114839aaed
Gabe Kangas <gabek@real-ity.com>
2021-03-23 23:51:01 -0700

    Add noreferrer to foter link

--- webroot/js/app.js
+++ webroot/js/app.js
@@ -643,7 +643,7 @@ export default class App extends Component {
 
         <footer class="flex flex-row justify-start p-8 opacity-50 text-xs">
           <span class="mx-1 inline-block">
-            <a href="${URL_OWNCAST}" target="_blank">About Owncast</a>
+            <a href="${URL_OWNCAST}" rel="noopener noreferrer" target="_blank">About Owncast</a>
           </span>
           <span class="mx-1 inline-block">Version ${appVersion}</span>
         </footer>
