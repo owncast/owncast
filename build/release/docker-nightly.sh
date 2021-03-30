bf93d2decce92f944c7e3c7b5b8ed3d24ecf477f
Gabe Kangas <gabek@real-ity.com>
2021-03-29 19:07:29 -0700

    build docker image every night

---
+++ build/release/docker-nightly.sh
@@ -0,0 +1,19 @@
+# Docker build
+# Must authenticate first: https://docs.github.com/en/packages/using-github-packages-with-your-projects-ecosystem/configuring-docker-for-use-with-github-packages#authenticating-to-github-packages
+DOCKER_IMAGE="owncast"
+VERSION="nightly"
+GIT_COMMIT=$(git rev-list -1 HEAD)
+
+echo "Building Docker image ${DOCKER_IMAGE}..."
+
+# Change to the root directory of the repository
+cd $(git rev-parse --show-toplevel)
+
+# Docker build
+docker build --build-arg NAME=docker --build-arg VERSION=${VERSION} --build-arg GIT_COMMIT=$GIT_COMMIT -t ghcr.io/owncast/${DOCKER_IMAGE}:nightly . -f build/release/Dockerfile-build
+
+# Dockerhub
+# You must be authenticated via `docker login` with your Dockerhub credentials first.
+# docker push gabekangas/owncast:nightly
+
+docker push ghcr.io/owncast/${DOCKER_IMAGE}:nightly
