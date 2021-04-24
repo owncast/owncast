eedc07fc940bb9207f0baef9ec0af082f0056d31
Gabe Kangas <gabek@real-ity.com>
2021-04-24 14:00:30 -0700

    Ignore warning about hls segment length

--- core/transcoder/utils.go
+++ core/transcoder/utils.go
@@ -33,6 +33,7 @@ var errorMap = map[string]string{
 	`Unknown encoder 'h264_x264'`:      "your copy of ffmpeg does not have support for the default x264 codec (h264_x264). download a version of ffmpeg that supports this.",
 	`Unrecognized option 'x264-params`: "your copy of ffmpeg does not have support for the default libx264 codec (h264_x264). download a version of ffmpeg that supports this.",
 	`Failed to set value '/dev/dri/renderD128' for option 'vaapi_device': Invalid argument`: "failed to set va-api device to /dev/dri/renderD128. your system is likely not properly configured for va-api",
+
 	// Generic error for a codec
 	"Unrecognized option": "error with codec. if your copy of ffmpeg or your hardware does not support your selected codec you may need to select another",
 }
@@ -50,6 +51,7 @@ var ignoredErrors = []string{
 	"Cannot use rename on non file protocol",
 	"Device creation failed",
 	"Error parsing global options",
+	"maybe the hls segment duration will not precise",
 }
 
 func handleTranscoderMessage(message string) {
