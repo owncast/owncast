4f73da30a885a6a3534a035abfe7beb262fcdefc
Gabe Kangas <gabek@real-ity.com>
2021-04-29 17:04:33 -0700

    Add additional stream + server info to stream started webhook. Closes #971

--- core/webhooks/stream.go
+++ core/webhooks/stream.go
@@ -1,7 +1,17 @@
 package webhooks
 
-import "github.com/owncast/owncast/models"
+import (
+	"github.com/owncast/owncast/core/data"
+	"github.com/owncast/owncast/models"
+)
 
 func SendStreamStatusEvent(eventType models.EventType) {
-	SendEventToWebhooks(WebhookEvent{Type: eventType})
+	SendEventToWebhooks(WebhookEvent{
+		Type: eventType,
+		EventData: map[string]interface{}{
+			"name":        data.GetServerName(),
+			"summary":     data.GetServerSummary(),
+			"streamTitle": data.GetStreamTitle(),
+		},
+	})
 }
