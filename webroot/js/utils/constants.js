6cb73b7aecc5d2cd69bc5865f52a2ca46947ccb5
gabek <gabek@users.noreply.github.com>
2021-05-21 03:29:32 +0000

    Prettified Code!

--- webroot/js/utils/constants.js
+++ webroot/js/utils/constants.js
@@ -8,12 +8,15 @@ export const URL_VIEWER_PING = `/api/ping`;
 
 // TODO: This directory is customizable in the config.  So we should expose this via the config API.
 export const URL_STREAM = `/hls/stream.m3u8`;
-export const URL_WEBSOCKET = `${location.protocol === 'https:' ? 'wss' : 'ws'}://${location.host}/entry`;
+export const URL_WEBSOCKET = `${
+  location.protocol === 'https:' ? 'wss' : 'ws'
+}://${location.host}/entry`;
 
 export const TIMER_STATUS_UPDATE = 5000; // ms
 export const TIMER_DISABLE_CHAT_AFTER_OFFLINE = 5 * 60 * 1000; // 5 mins
 export const TIMER_STREAM_DURATION_COUNTER = 1000;
-export const TEMP_IMAGE = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
+export const TEMP_IMAGE =
+  'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
 
 export const OWNCAST_LOGO_LOCAL = '/img/logo.svg';
 
@@ -23,12 +26,12 @@ export const MESSAGE_ONLINE = 'Stream is online.';
 export const URL_OWNCAST = 'https://owncast.online'; // used in footer
 export const PLAYER_VOLUME = 'owncast_volume';
 
-
 export const KEY_USERNAME = 'owncast_username';
-export const KEY_CUSTOM_USERNAME_SET = 'owncast_custom_username_set'
+export const KEY_CUSTOM_USERNAME_SET = 'owncast_custom_username_set';
 export const KEY_CHAT_DISPLAYED = 'owncast_chat';
 export const KEY_CHAT_FIRST_MESSAGE_SENT = 'owncast_first_message_sent';
-export const CHAT_INITIAL_PLACEHOLDER_TEXT = 'Type here to chat, no account necessary.';
+export const CHAT_INITIAL_PLACEHOLDER_TEXT =
+  'Type here to chat, no account necessary.';
 export const CHAT_PLACEHOLDER_TEXT = 'Message';
 export const CHAT_PLACEHOLDER_OFFLINE = 'Chat is offline.';
 export const CHAT_MAX_MESSAGE_LENGTH = 500;
@@ -44,12 +47,7 @@ export const CHAT_OK_KEYCODES = [
   'Delete',
   'Backspace',
 ];
-export const CHAT_KEY_MODIFIERS = [
-  'Control',
-  'Shift',
-  'Meta',
-  'Alt',
-];
+export const CHAT_KEY_MODIFIERS = ['Control', 'Shift', 'Meta', 'Alt'];
 export const MESSAGE_JUMPTOBOTTOM_BUFFER = 300;
 
 // app styling
