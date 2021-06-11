2fdf510f3e91214007d95574fb0a49c9f5b69031
Meisam <39205857+MFTabriz@users.noreply.github.com>
2021-06-12 01:42:30 +0200

    Change the default backup directory

--- config/constants.go
+++ config/constants.go
@@ -11,13 +11,13 @@ const (
 	PrivateHLSStoragePath = "hls"
 	// FfmpegSuggestedVersion is the version of ffmpeg we suggest.
 	FfmpegSuggestedVersion = "v4.1.5" // Requires the v
-	// BackupDirectory is the directory we write backup files to.
-	BackupDirectory = "backup"
-	// DataDirectory is the directory we save data to.
+  // DataDirectory is the directory we save data to.
 	DataDirectory = "data"
 )
 
 var (
 	// PublicHLSStoragePath is the directory we write public HLS files to for distribution.
 	PublicHLSStoragePath = filepath.Join(WebRoot, "hls")
+  // BackupDirectory is the directory we write backup files to.
+  BackupDirectory = filepath.Join(DataDirectory, "backup")
 )
