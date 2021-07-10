76388de157f6d137215a83683bee0e8913399035
mtabriz <39205857+MFTabriz@users.noreply.github.com>
2021-07-10 17:50:48 +0200

    add emojiCacheTimestamp

--- controllers/emoji.go
+++ controllers/emoji.go
@@ -4,9 +4,11 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"path"
 	"path/filepath"
 	"strings"
+	"time"
 
 	"github.com/owncast/owncast/config"
 	"github.com/owncast/owncast/models"
@@ -18,22 +20,33 @@ import (
 const emojiDir = "/img/emoji" // Relative to webroot
 
 var emojiCache = make([]models.CustomEmoji, 0)
+var emojiCacheTimestamp time.Time
 
 func getCustomEmojiList() []models.CustomEmoji {
+	fullPath := filepath.Join(config.WebRoot, emojiDir)
+	emojiDirInfo, err := os.Stat(fullPath)
+	if err != nil {
+		log.Errorln(err)
+	}
+	if emojiDirInfo.ModTime() != emojiCacheTimestamp {
+		log.Traceln("Emoji cache invalidated!")
+		emojiCache = make([]models.CustomEmoji, 0)
+	}
+
 	if len(emojiCache) == 0 {
-		fullPath := filepath.Join(config.WebRoot, emojiDir)
 		files, err := ioutil.ReadDir(fullPath)
 		if err != nil {
 			log.Errorln(err)
 			return emojiCache
 		}
-
 		for _, f := range files {
 			name := strings.TrimSuffix(f.Name(), path.Ext(f.Name()))
 			emojiPath := filepath.Join(emojiDir, f.Name())
 			singleEmoji := models.CustomEmoji{Name: name, Emoji: emojiPath}
 			emojiCache = append(emojiCache, singleEmoji)
 		}
+
+		emojiCacheTimestamp = emojiDirInfo.ModTime()
 	}
 
 	return emojiCache
