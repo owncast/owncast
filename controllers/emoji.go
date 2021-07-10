d94a7dafef793185c9e941adce406f0562ae7fd6
mtabriz <39205857+MFTabriz@users.noreply.github.com>
2021-07-10 17:33:41 +0200

    use emojiCache

--- controllers/emoji.go
+++ controllers/emoji.go
@@ -17,27 +17,31 @@ import (
 // to need it to be.  The config is getting a bit bloated.
 const emojiDir = "/img/emoji" // Relative to webroot
 
+var emojiCache = make([]models.CustomEmoji, 0)
+
+func getCustomEmojiList() []models.CustomEmoji {
+	if len(emojiCache) == 0 {
+		fullPath := filepath.Join(config.WebRoot, emojiDir)
+		files, err := ioutil.ReadDir(fullPath)
+		if err != nil {
+			log.Errorln(err)
+			return emojiCache
+		}
+
+		for _, f := range files {
+			name := strings.TrimSuffix(f.Name(), path.Ext(f.Name()))
+			emojiPath := filepath.Join(emojiDir, f.Name())
+			singleEmoji := models.CustomEmoji{Name: name, Emoji: emojiPath}
+			emojiCache = append(emojiCache, singleEmoji)
+		}
+	}
+
+	return emojiCache
+}
+
 // GetCustomEmoji returns a list of custom emoji via the API.
 func GetCustomEmoji(w http.ResponseWriter, r *http.Request) {
-	emojiList := make([]models.CustomEmoji, 0)
-
-	fullPath := filepath.Join(config.WebRoot, emojiDir)
-	files, err := ioutil.ReadDir(fullPath)
-	if err != nil {
-		log.Errorln(err)
-		// Throw HTTP 500
-		return
-	}
-
-	// Memoize this result somewhere?  Right now it iterates through the
-	// filesystem every time the API is hit.  Should you need to restart
-	// the server to add emoji?
-	for _, f := range files {
-		name := strings.TrimSuffix(f.Name(), path.Ext(f.Name()))
-		emojiPath := filepath.Join(emojiDir, f.Name())
-		singleEmoji := models.CustomEmoji{Name: name, Emoji: emojiPath}
-		emojiList = append(emojiList, singleEmoji)
-	}
+	emojiList := getCustomEmojiList()
 
 	if err := json.NewEncoder(w).Encode(emojiList); err != nil {
 		InternalErrorHandler(w, err)
