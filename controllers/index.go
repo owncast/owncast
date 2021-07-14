bf0720aba73db7f557ffa9909d01a047497a72f3
mtabriz <39205857+MFTabriz@users.noreply.github.com>
2021-07-14 15:38:26 +0200

    link assets under the same protocol

--- controllers/index.go
+++ controllers/index.go
@@ -74,12 +74,17 @@ func IndexHandler(w http.ResponseWriter, r *http.Request) {
 // to give to Opengraph clients and web scrapers (bots, web crawlers, etc).
 func handleScraperMetadataPage(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles(path.Join("static", "metadata.html")))
+	protocol := "http"
 
-	fullURL, err := url.Parse(fmt.Sprintf("http://%s%s", r.Host, r.URL.Path))
+	if r.TLS != nil {
+		protocol = "https"
+	}
+
+	fullURL, err := url.Parse(fmt.Sprintf("%s://%s%s", protocol, r.Host, r.URL.Path))
 	if err != nil {
 		log.Panicln(err)
 	}
-	imageURL, err := url.Parse(fmt.Sprintf("http://%s%s", r.Host, "/logo"))
+	imageURL, err := url.Parse(fmt.Sprintf("%s://%s%s", protocol, r.Host, "/logo"))
 	if err != nil {
 		log.Panicln(err)
 	}
@@ -89,7 +94,7 @@ func handleScraperMetadataPage(w http.ResponseWriter, r *http.Request) {
 	// If the thumbnail does not exist or we're offline then just use the logo image
 	var thumbnailURL string
 	if status.Online && utils.DoesFileExists(filepath.Join(config.WebRoot, "thumbnail.jpg")) {
-		thumbnail, err := url.Parse(fmt.Sprintf("http://%s%s", r.Host, "/thumbnail.jpg"))
+		thumbnail, err := url.Parse(fmt.Sprintf("%s://%s%s", protocol, r.Host, "/thumbnail.jpg"))
 		if err != nil {
 			log.Errorln(err)
 			thumbnailURL = imageURL.String()
