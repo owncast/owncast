d6bd8969ef12124c131b7f1cf432a3057c49c5a9
Ginger Wong <omqmail@gmail.com>
2021-07-17 14:58:10 -0700

    console clean up

--- webroot/js/utils/chat.js
+++ webroot/js/utils/chat.js
@@ -143,7 +143,6 @@ export function convertOnPaste( event = { preventDefault() {} }) {
 }
 
 export function createEmojiMarkup(data, isCustom) {
-  console.log(data.url)
   const emojiUrl = isCustom ? data.emoji : data.url;
   const emojiName = (isCustom ? data.name : data.url.split('\\').pop().split('/').pop().split('.').shift()).toLowerCase();
   return '<img class="emoji" alt=":' + emojiName + ':" title=":' + emojiName + ':" src="' + emojiUrl + '"/>';
