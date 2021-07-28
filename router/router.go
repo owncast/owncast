556ec7dd98d4294f8a414eed6e94ee7a72609b52
Gabe Kangas <gabek@real-ity.com>
2021-07-27 19:45:44 -0700

    Support webfinger requests for the live account. Closes https://github.com/owncast/owncast/issues/1193

--- router/router.go
+++ router/router.go
@@ -7,6 +7,7 @@ import (
 
 	log "github.com/sirupsen/logrus"
 
+	"github.com/owncast/owncast/activitypub"
 	"github.com/owncast/owncast/config"
 	"github.com/owncast/owncast/controllers"
 	"github.com/owncast/owncast/controllers/admin"
@@ -232,6 +233,9 @@ func Start() error {
 
 	// set custom style css
 	http.HandleFunc("/api/admin/config/customstyles", middleware.RequireAdminAuth(admin.SetCustomStyles))
+
+	// ActivityPub has its own router
+	activitypub.Start()
 
 	// websocket
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
