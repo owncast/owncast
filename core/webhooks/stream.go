9d0ae44c3cfaf509438004fd53072a264570b510
Gabe Kangas <gabek@real-ity.com>
2021-08-12 20:02:17 -0700

    Give stream status webhooks an id and timestamp

--- core/webhooks/stream.go
+++ core/webhooks/stream.go
@@ -1,17 +1,22 @@
 package webhooks
 
 import (
+	"time"
+
 	"github.com/owncast/owncast/core/data"
 	"github.com/owncast/owncast/models"
+	"github.com/teris-io/shortid"
 )
 
 func SendStreamStatusEvent(eventType models.EventType) {
 	SendEventToWebhooks(WebhookEvent{
 		Type: eventType,
 		EventData: map[string]interface{}{
+			"id":          shortid.MustGenerate(),
 			"name":        data.GetServerName(),
 			"summary":     data.GetServerSummary(),
 			"streamTitle": data.GetStreamTitle(),
+			"timestamp":   time.Now(),
 		},
 	})
 }
