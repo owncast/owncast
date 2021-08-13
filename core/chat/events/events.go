cf87f53da3980c1c08d1b4bd0836ac6adebb5bec
Gabe Kangas <gabek@real-ity.com>
2021-08-12 21:20:06 -0700

    Do not send empty type when its not apropriate

--- core/chat/events/events.go
+++ core/chat/events/events.go
@@ -27,7 +27,7 @@ type OutboundEvent interface {
 
 // Event is any kind of event.  A type is required to be specified.
 type Event struct {
-	Type      EventType `json:"type"`
+	Type      EventType `json:"type,omitempty"`
 	Id        string    `json:"id"`
 	Timestamp time.Time `json:"timestamp"`
 }
