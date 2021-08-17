abf858aa6344838111b98a4cb1d901184dc31610
Gabe Kangas <gabek@real-ity.com>
2021-08-17 14:54:32 -0700

    Add responses to fediverse nodeinfo requests

--- activitypub/router.go
+++ activitypub/router.go
@@ -10,6 +10,15 @@ func StartRouter() {
 	// WebFinger
 	http.HandleFunc("/.well-known/webfinger", controllers.WebfingerHandler)
 
+	// Nodeinfo v1
+	http.HandleFunc("/.well-known/nodeinfo", controllers.NodeInfoController)
+
+	// Nodeinfo v2
+	http.HandleFunc("/nodeinfo/2.0", controllers.NodeInfoV2Controller)
+
+	// Instance details
+	http.HandleFunc("/api/v1/instance", controllers.InstanceV1Controller)
+
 	// Single ActivityPub Actor
 	http.HandleFunc("/federation/user/", controllers.ActorHandler)
 
