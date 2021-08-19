e5735b8d6db14fba5281d653389c7fb7ff2bcc48
Gabe Kangas <gabek@real-ity.com>
2021-08-18 18:08:07 -0700

    Remove adding <br> tags to newlines

--- core/chat/events/events.go
+++ core/chat/events/events.go
@@ -96,7 +96,6 @@ func RenderMarkdown(raw string) string {
 	markdown := goldmark.New(
 		goldmark.WithRendererOptions(
 			html.WithUnsafe(),
-			html.WithHardWraps(),
 		),
 		goldmark.WithExtensions(
 			extension.NewLinkify(
