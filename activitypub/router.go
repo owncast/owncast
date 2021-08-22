cafac8ada5b4da70e07339d4c85eb5df0edc5ce0
Gabe Kangas <gabek@real-ity.com>
2021-08-21 20:48:26 -0700

    Require AP-compatible content types for AP-related requests

--- activitypub/router.go
+++ activitypub/router.go
@@ -4,27 +4,28 @@ import (
 	"net/http"
 
 	"github.com/owncast/owncast/activitypub/controllers"
+	"github.com/owncast/owncast/router/middleware"
 )
 
 func StartRouter() {
 	// WebFinger
-	http.HandleFunc("/.well-known/webfinger", controllers.WebfingerHandler)
+	http.HandleFunc("/.well-known/webfinger", middleware.RequireActivityPubOrRedirect(controllers.WebfingerHandler))
 
 	// Host Metadata
 	http.HandleFunc("/.well-known/host-meta", controllers.HostMetaController)
 
 	// Nodeinfo v1
-	http.HandleFunc("/.well-known/nodeinfo", controllers.NodeInfoController)
+	http.HandleFunc("/.well-known/nodeinfo", middleware.RequireActivityPubOrRedirect(controllers.NodeInfoController))
 
 	// Nodeinfo v2
-	http.HandleFunc("/nodeinfo/2.0", controllers.NodeInfoV2Controller)
+	http.HandleFunc("/nodeinfo/2.0", middleware.RequireActivityPubOrRedirect(controllers.NodeInfoV2Controller))
 
 	// Instance details
 	http.HandleFunc("/api/v1/instance", controllers.InstanceV1Controller)
 
 	// Single ActivityPub Actor
-	http.HandleFunc("/federation/user/", controllers.ActorHandler)
+	http.HandleFunc("/federation/user/", middleware.RequireActivityPubOrRedirect(controllers.ActorHandler))
 
 	// Single AP object
-	http.HandleFunc("/federation/", controllers.ObjectHandler)
+	http.HandleFunc("/federation/", middleware.RequireActivityPubOrRedirect(controllers.ObjectHandler))
 }
