f42383b92c366c648a519f78d721e6a8fead6f5e
Gabe Kangas <gabek@real-ity.com>
2021-08-21 18:10:53 -0700

    Do not send out message if disabled. Use saved go live message.

--- core/streamState.go
+++ core/streamState.go
@@ -72,7 +72,9 @@ func setStreamAsConnected(rtmpOut *io.PipeReader) {
 	chat.SendAllWelcomeMessage()
 
 	// Send a delayed live Federated message.
-	_onlineTimerCancelFunc = startFederatedLiveStreamMessageTimer()
+	if data.GetFederationEnabled() {
+		_onlineTimerCancelFunc = startFederatedLiveStreamMessageTimer()
+	}
 }
 
 // SetStreamAsDisconnected sets the stream as disconnected.
