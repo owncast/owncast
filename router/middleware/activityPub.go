0b30311561dbc789ea05b4243a5b6c1ac9e189e5
Gabe Kangas <gabek@real-ity.com>
2021-08-22 00:43:10 -0700

    io -> ioutil

--- router/middleware/activityPub.go
+++ router/middleware/activityPub.go
@@ -1,13 +1,22 @@
 package middleware
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/owncast/owncast/utils"
+)
 
 // RequireUserAccessToken will validate a provided user's access token and make sure the associated user is enabled.
 // Not to be used for validating 3rd party access.
 func RequireActivityPubOrRedirect(handler http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		acceptedContentTypes := []string{"application/json", "application/json+ld", "application/activity+json"}
 		accept := r.Header.Get("Accept")
-		if accept != "application/json" && accept != "application/json+ld" && accept != "application/activity+json" {
+		contentType := r.Header.Get("Content-Type")
+
+		_, acceptMatches := utils.FindInSlice(acceptedContentTypes, accept)
+		_, contentTypeMatches := utils.FindInSlice(acceptedContentTypes, contentType)
+		if !acceptMatches && !contentTypeMatches {
 			http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
 			return
 		}
