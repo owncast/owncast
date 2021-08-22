aeb0cc59181cf94752a10f45e6365f748294ad8c
Gabe Kangas <gabek@real-ity.com>
2021-08-21 20:48:26 -0700

    Require AP-compatible content types for AP-related requests

---
+++ router/middleware/activityPub.go
@@ -0,0 +1,17 @@
+package middleware
+
+import "net/http"
+
+// RequireUserAccessToken will validate a provided user's access token and make sure the associated user is enabled.
+// Not to be used for validating 3rd party access.
+func RequireActivityPubOrRedirect(handler http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		accept := r.Header.Get("Accept")
+		if accept != "application/json" || accept != "application/json+ld" {
+			http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+			return
+		}
+
+		handler(w, r)
+	})
+}
