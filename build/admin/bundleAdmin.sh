08e99e7ca2f285ccf25146982ba61f4d221a59b7
Gabe Kangas <gabek@real-ity.com>
2021-08-31 20:27:42 -0700

    Point to the dev admin branch for ap

--- build/admin/bundleAdmin.sh
+++ build/admin/bundleAdmin.sh
@@ -17,6 +17,7 @@ trap shutdown INT TERM ABRT EXIT
 echo "Cloning owncast admin into $INSTALL_TEMP_DIRECTORY..."
 git clone https://github.com/owncast/owncast-admin 2> /dev/null
 cd owncast-admin
+git checkout gek/activity-pub-1
 
 echo "Installing npm modules for the owncast admin..."
 npm --silent install 2> /dev/null
