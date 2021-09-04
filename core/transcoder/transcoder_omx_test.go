5506e1762bbe95bed26578974c5dd30c02d5d53a
ooa113y <13thSlayer@gmail.com>
2021-09-04 21:39:11 +0300

    Use filepath.Join

--- core/transcoder/transcoder_omx_test.go
+++ core/transcoder/transcoder_omx_test.go
@@ -4,8 +4,7 @@ import (
 	"testing"
 
 	"github.com/owncast/owncast/models"
-	"strings"
-	"runtime"
+	"path/filepath"
 )
 
 func TestFFmpegOmxCommand(t *testing.T) {
@@ -13,7 +12,7 @@ func TestFFmpegOmxCommand(t *testing.T) {
 	codec := OmxCodec{}
 
 	transcoder := new(Transcoder)
-	transcoder.ffmpegPath = "/fake/path/ffmpeg"
+	transcoder.ffmpegPath = filepath.Join("fake", "path", "ffmpeg")
 	transcoder.SetInput("fakecontent.flv")
 	transcoder.SetOutputPath("fakeOutput")
 	transcoder.SetIdentifier("jdFsdfzGg")
@@ -42,11 +41,8 @@ func TestFFmpegOmxCommand(t *testing.T) {
 
 	cmd := transcoder.getString()
 
-	expected := `FFREPORT=file="data/logs/transcoder.log":level=32 /fake/path/ffmpeg -hide_banner -loglevel warning  -fflags +genpts -i  fakecontent.flv  -map v:0 -c:v:0 h264_omx -b:v:0 1200k -maxrate:v:0 1272k -g:v:0 90 -keyint_min:v:0 90 -r:v:0 30  -map a:0? -c:a:0 copy -preset veryfast -map v:0 -c:v:1 h264_omx -b:v:1 3500k -maxrate:v:1 3710k -g:v:1 72 -keyint_min:v:1 72 -r:v:1 24  -map a:0? -c:a:1 copy -preset fast -map v:0 -c:v:2 copy -map a:0? -c:a:2 copy -preset ultrafast  -var_stream_map "v:0,a:0 v:1,a:1 v:2,a:2 " -f hls -hls_time 3 -hls_list_size 3  -segment_format_options mpegts_flags=+initial_discontinuity:mpegts_copyts=1 -tune zerolatency -pix_fmt yuv420p -sc_threshold 0 -master_pl_name stream.m3u8 -strftime 1 -hls_segment_filename http://127.0.0.1:8123/%v/stream-jdFsdfzGg%s.ts -max_muxing_queue_size 400 -method PUT -http_persistent 0 http://127.0.0.1:8123/%v/stream.m3u8`
-
-	if runtime.GOOS == "windows" {
-		expected = strings.Replace(expected, "data/logs/transcoder.log", "data\\logs\\transcoder.log", 1)
-	}
+	expectedLogPath := filepath.Join("data", "logs", "transcoder.log")
+	expected := `FFREPORT=file="`+expectedLogPath+`":level=32 `+transcoder.ffmpegPath+` -hide_banner -loglevel warning  -fflags +genpts -i  fakecontent.flv  -map v:0 -c:v:0 h264_omx -b:v:0 1200k -maxrate:v:0 1272k -g:v:0 90 -keyint_min:v:0 90 -r:v:0 30  -map a:0? -c:a:0 copy -preset veryfast -map v:0 -c:v:1 h264_omx -b:v:1 3500k -maxrate:v:1 3710k -g:v:1 72 -keyint_min:v:1 72 -r:v:1 24  -map a:0? -c:a:1 copy -preset fast -map v:0 -c:v:2 copy -map a:0? -c:a:2 copy -preset ultrafast  -var_stream_map "v:0,a:0 v:1,a:1 v:2,a:2 " -f hls -hls_time 3 -hls_list_size 3  -segment_format_options mpegts_flags=+initial_discontinuity:mpegts_copyts=1 -tune zerolatency -pix_fmt yuv420p -sc_threshold 0 -master_pl_name stream.m3u8 -strftime 1 -hls_segment_filename http://127.0.0.1:8123/%v/stream-jdFsdfzGg%s.ts -max_muxing_queue_size 400 -method PUT -http_persistent 0 http://127.0.0.1:8123/%v/stream.m3u8`
 
 	if cmd != expected {
 		t.Errorf("ffmpeg command does not match expected.\nGot %s\n, want: %s", cmd, expected)
