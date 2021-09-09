6d58ebdc9d59f0ca66215c62383001fa8d379e5e
Gabe Kangas <gabek@real-ity.com>
2021-09-08 22:37:20 -0700

    Fix AP followers endpoint. Closes #1204

--- activitypub/activitypub.go
+++ activitypub/activitypub.go
@@ -5,7 +5,6 @@ import (
 	"github.com/owncast/owncast/activitypub/outbox"
 	"github.com/owncast/owncast/activitypub/persistence"
 	"github.com/owncast/owncast/core/data"
-	"github.com/owncast/owncast/models"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -38,45 +37,4 @@ func SendPublicFederatedMessage(message string) {
 
 func GetFollowerCount() int {
 	return persistence.GetFollowerCount()
-}
-
-func GetFederationFollowers() ([]models.Follower, error) {
-	followers := make([]models.Follower, 0)
-
-	var query = "SELECT iri, inbox, name, username, image FROM ap_followers"
-
-	rows, err := _datastore.DB.Query(query)
-	if err != nil {
-		return followers, err
-	}
-	defer rows.Close()
-
-	for rows.Next() {
-		var iriString string
-		var inboxString string
-		var nameString string
-		var usernameString string
-		var imageString string
-
-		if err := rows.Scan(&iriString, &inboxString, &nameString, &usernameString, &imageString); err != nil {
-			log.Error("There is a problem reading the database.", err)
-			return followers, err
-		}
-
-		singleFollower := models.Follower{
-			Name:     nameString,
-			Username: usernameString,
-			Image:    imageString,
-			Link:     iriString,
-			Inbox:    inboxString,
-		}
-
-		followers = append(followers, singleFollower)
-	}
-
-	if err := rows.Err(); err != nil {
-		return followers, err
-	}
-
-	return followers, nil
 }
