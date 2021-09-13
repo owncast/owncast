unclearParadigm/owncast-examples

add public func to lookup a ChatClient by its clientId

core/chat/chat.go
@@ -40,6 +40,11 @@ func GetClientsForUser(userID string) ([]*ChatClient, error) {
 	return clients[userID], nil
 }
 
+func FindClientById(clientId uint) (*ChatClient, bool) {
+	client, found := _server.clients[clientId]
+	return client, found
+}
+
 func GetClients() []*ChatClient {
 	clients := []*ChatClient{}
 

owncast/owncast

core/chat/chat.go
@@ -41,6 +41,12 @@ func GetClientsForUser(userID string) ([]*Client, error) {
 	return clients[userID], nil
 }
 
+// FindClientByID will return a single connected client by ID.
+func FindClientByID(clientID uint) (*Client, bool) {
+	client, found := _server.clients[clientID]
+	return client, found
+}
+
 // GetClients will return all the current chat clients connected.
 func GetClients() []*Client {
 	clients := []*Client{}