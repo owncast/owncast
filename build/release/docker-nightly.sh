7dc136acc007541acc3fad47d17e155626369518
Gabe Kangas <gabek@real-ity.com>
2021-09-12 23:45:14 -0700

    Have nightly build create a production build of tailwind

--- build/release/docker-nightly.sh
+++ build/release/docker-nightly.sh
@@ -5,6 +5,15 @@ DATE=$(date +"%Y%m%d")
 VERSION="${DATE}-nightly"
 GIT_COMMIT=$(git rev-list -1 HEAD)
 
+# Create production build of Tailwind CSS
+pushd ../../build/javascript >> /dev/null
+# Install the tailwind & postcss CLIs
+npm install --quiet --no-progress
+# Run the tailwind CLI and pipe it to postcss for minification.
+# Save it to a temp directory that we will reference below.
+NODE_ENV="production" ./node_modules/.bin/tailwind build | ./node_modules/.bin/postcss >  "../../webroot/js/web_modules/tailwindcss/dist/tailwind.min.css"
+popd
+
 echo "Building Docker image ${DOCKER_IMAGE}..."
 
 # Change to the root directory of the repository
