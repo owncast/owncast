958c78641e8adb2ebf6abb71c126622c0399866c
leo <lkrashanoff2@gmail.com>
2021-10-05 13:29:18 -0700

    Replaces redirect of /embed URLs to return content directly. (#1453)
    - Removes relative imports from embedded player, chat.
    - Replaces `Redirect` calls with `ServeFile` in `embed.go`.

--- controllers/embed.go
+++ controllers/embed.go
@@ -6,15 +6,15 @@ import (
 
 // GetChatEmbedreadwrite gets the embed for readwrite chat.
 func GetChatEmbedreadwrite(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "/index-standalone-chat-readwrite.html", http.StatusTemporaryRedirect)
+	http.ServeFile(w, r, "webroot/index-standalone-chat-readwrite.html")
 }
 
 // GetChatEmbedreadonly gets the embed for readonly chat.
 func GetChatEmbedreadonly(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "/index-standalone-chat-readonly.html", http.StatusTemporaryRedirect)
+	http.ServeFile(w, r, "webroot/index-standalone-chat-readonly.html")
 }
 
 // GetVideoEmbed gets the embed for video.
 func GetVideoEmbed(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "/index-video-only.html", http.StatusTemporaryRedirect)
+	http.ServeFile(w, r, "webroot/index-video-only.html")
 }
