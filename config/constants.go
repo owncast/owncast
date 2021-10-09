ac1cd9b901418271ce828e03c6c1e5660202d3d4
Gabe Kangas <gabek@real-ity.com>
2021-10-08 21:00:29 -0700

    Version constant bump

--- config/constants.go
+++ config/constants.go
@@ -4,7 +4,7 @@ import "path/filepath"
 
 const (
 	// StaticVersionNumber is the version of Owncast that is used when it's not overwritten via build-time settings.
-	StaticVersionNumber = "0.0.10" // Shown when you build from develop
+	StaticVersionNumber = "0.0.11" // Shown when you build from develop
 	// WebRoot is the web server root directory.
 	WebRoot = "webroot"
 	// FfmpegSuggestedVersion is the version of ffmpeg we suggest.
