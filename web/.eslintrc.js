dc0bf6db899f2886ac8f268fcd6b3ba27972d2e9
gabek <gabek@users.noreply.github.com>
2021-10-14 01:22:18 +0000

    Prettified Code!

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -3,12 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-    'prettier',
-    'plugin:@next/next/recommended'
-  ],
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier', 'plugin:@next/next/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
