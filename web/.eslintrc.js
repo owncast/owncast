5b2d215c526e832e9ffbcac3d2cc3ddd5e6031ac
Gabe Kangas <gabek@real-ity.com>
2021-10-13 18:21:51 -0700

    Update eslint workflow. Set nextjs default rules.

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -3,7 +3,12 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier',
+    'plugin:@next/next/recommended'
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
