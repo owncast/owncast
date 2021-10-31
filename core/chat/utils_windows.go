2d5ab54bb7c8d82b9dae7dbe1db933be5eeeca61
Jack <40802798+Jack073@users.noreply.github.com>
2021-10-31 19:15:29 +0000

    Fix build for Windows (#1377)

---
+++ core/chat/utils_windows.go
@@ -0,0 +1,12 @@
+//go:build windows
+// +build windows
+
+package chat
+
+func getMaximumConcurrentConnectionLimit() int64 {
+	// The maximum limit I can find for windows is 16,777,216
+	// (essentially unlimited, but add the 0.7 multiplier as well to be
+	// consistent with other systems)
+	// https://docs.microsoft.com/en-gb/archive/blogs/markrussinovich/pushing-the-limits-of-windows-handles
+	return (16777216 * 7) / 10
+}
