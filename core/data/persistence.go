1f1500edb5ff3766379a7187eae3102b9d193f46
Gabe Kangas <gabek@real-ity.com>
2021-12-25 12:44:17 -0800

    Only set server init date if not already set

--- core/data/persistence.go
+++ core/data/persistence.go
@@ -134,9 +134,9 @@ func (ds *Datastore) Setup() {
 	}
 
 	// Set the server initialization date if needed.
-	// if hasSetInitDate, _ := GetServerInitTime(); hasSetInitDate == nil || !hasSetInitDate.Valid {
-	_ = SetServerInitTime(time.Now())
-	// }
+	if hasSetInitDate, _ := GetServerInitTime(); hasSetInitDate == nil || !hasSetInitDate.Valid {
+		_ = SetServerInitTime(time.Now())
+	}
 }
 
 // Reset will delete all config entries in the datastore and start over.
