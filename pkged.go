87bd1e57e62c071b4a78fc1eacb30ecd8b1008cb
Gabe Kangas <gabek@real-ity.com>
2022-03-04 19:31:38 -0800

    Remove pkged.go

--- pkged.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	log "github.com/sirupsen/logrus"
-)
-
-func init() {
-	log.Warnln("Explicitly building with pkged.go is no longer required and the file will be removed in the future. See https://github.com/owncast/owncast/pull/1464.")
-}
