d6aa8b5d7c94f64ae018da074bd83998a9c864b3
Gabe Kangas <gabek@real-ity.com>
2022-03-07 08:45:49 -0800

    Bump gopsutil to v3

--- metrics/hardware.go
+++ metrics/hardware.go
@@ -3,9 +3,9 @@ package metrics
 import (
 	"time"
 
-	"github.com/shirou/gopsutil/cpu"
-	"github.com/shirou/gopsutil/disk"
-	"github.com/shirou/gopsutil/mem"
+	"github.com/shirou/gopsutil/v3/cpu"
+	"github.com/shirou/gopsutil/v3/disk"
+	"github.com/shirou/gopsutil/v3/mem"
 
 	log "github.com/sirupsen/logrus"
 )
