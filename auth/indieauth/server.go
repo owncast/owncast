5cf27db9cb8026b6e05e695b1f2ba001f4619cc8
Aaron Parecki <aaron@parecki.com>
2022-04-02 10:58:39 -0700

    check the code verifier in the IndieAuth server

--- auth/indieauth/server.go
+++ auth/indieauth/server.go
@@ -10,11 +10,12 @@ import (
 // ServerAuthRequest is n inbound request to authenticate against
 // this Owncast instance.
 type ServerAuthRequest struct {
-	ClientID    string
-	RedirectURI string
-	State       string
-	Me          string
-	Code        string
+	ClientID      string
+	RedirectURI   string
+  CodeChallenge string
+	State         string
+	Me            string
+	Code          string
 }
 
 // ServerProfile represents basic user-provided data about this Owncast instance.
@@ -36,12 +37,13 @@ var pendingServerAuthRequests = map[string]ServerAuthRequest{}
 // StartServerAuth will handle the authentication for the admin user of this
 // Owncast server. Initiated via a GET of the auth endpoint.
 // https://indieweb.org/authorization-endpoint
-func StartServerAuth(clientID, redirectURI, state, me string) (*ServerAuthRequest, error) {
+func StartServerAuth(clientID, redirectURI, codeChallenge, state, me string) (*ServerAuthRequest, error) {
 	code := shortid.MustGenerate()
 
 	r := ServerAuthRequest{
 		ClientID:    clientID,
 		RedirectURI: redirectURI,
+    CodeChallenge: codeChallenge,
 		State:       state,
 		Me:          me,
 		Code:        code,
@@ -54,7 +56,7 @@ func StartServerAuth(clientID, redirectURI, state, me string) (*ServerAuthReques
 
 // CompleteServerAuth will verify that the values provided in the final step
 // of the IndieAuth flow are correct, and return some basic profile info.
-func CompleteServerAuth(code, redirectURI, clientID string) (*ServerProfileResponse, error) {
+func CompleteServerAuth(code, redirectURI, clientID string, codeVerifier string) (*ServerProfileResponse, error) {
 	request, pending := pendingServerAuthRequests[code]
 	if !pending {
 		return nil, errors.New("no pending authentication request")
@@ -67,6 +69,11 @@ func CompleteServerAuth(code, redirectURI, clientID string) (*ServerProfileRespo
 	if request.ClientID != clientID {
 		return nil, errors.New("client ID does not match")
 	}
+
+  codeChallengeFromRequest := createCodeChallenge(codeVerifier)
+  if request.CodeChallenge != codeChallengeFromRequest {
+    return nil, errors.New("code verifier is incorrect")
+  }
 
 	response := ServerProfileResponse{
 		Me: data.GetServerURL(),
