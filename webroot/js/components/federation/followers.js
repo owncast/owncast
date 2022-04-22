ce6e3939b7869de8434c4a6d9318928015be4260
gabek <gabek@users.noreply.github.com>
2022-04-22 20:03:34 +0000

    Prettified Code!

--- webroot/js/components/federation/followers.js
+++ webroot/js/components/federation/followers.js
@@ -81,7 +81,9 @@ export default class FollowerList extends Component {
 
     return html`
       <div>
-        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        <div
+          class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
+        >
           ${followers.length === 0 && noFollowersInfo}
           ${followers.map((follower) => {
             return html` <${SingleFollower} user=${follower} /> `;
