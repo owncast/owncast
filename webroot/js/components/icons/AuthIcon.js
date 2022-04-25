2d018b21bd92b07b08ba560571a846ee09a6852e
Gabe Kangas <gabek@real-ity.com>
2022-04-25 15:34:19 -0700

    Add auth icon to chat menu. Closes #1879

---
+++ webroot/js/components/icons/AuthIcon.js
@@ -0,0 +1,22 @@
+import { h } from '/js/web_modules/preact.js';
+import htm from '/js/web_modules/htm.js';
+const html = htm.bind(h);
+
+export const AuthIcon = ({ className }) => {
+  return html`
+    <svg
+      class="w-6 h-6"
+      fill="none"
+      stroke="currentColor"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M15 7a2 2 0 012 2m4 0a6 6 0 01-7.743 5.743L11 17H9v2H7v2H4a1 1 0 01-1-1v-2.586a1 1 0 01.293-.707l5.964-5.964A6 6 0 1121 9z"
+      ></path>
+    </svg>
+  `;
+};
