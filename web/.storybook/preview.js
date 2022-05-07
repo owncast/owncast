c99fcf5ed992ab53890d1c9e3578b2faa7534c89
Gabe Kangas <gabek@real-ity.com>
2022-05-07 01:13:26 -0700

    Tweak storybook UI a bit

--- web/.storybook/preview.js
+++ web/.storybook/preview.js
@@ -2,6 +2,7 @@ import '../styles/variables.css';
 import '../styles/global.less';
 import '../styles/theme.less';
 import '../stories/preview.scss';
+import { themes } from '@storybook/theming';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
@@ -10,5 +11,18 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+  },
+  darkMode: {
+    current: 'dark',
+    // Override the default dark theme
+    dark: {
+      ...themes.dark,
+      appBg: '#171523',
+      brandImage: 'https://owncast.online/images/logo.svg',
+      brandTitle: 'Owncast',
+      brandUrl: 'https://owncast.online',
+    },
+    // Override the default light theme
+    light: { ...themes.normal },
   },
 };
