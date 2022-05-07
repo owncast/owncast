f835ae5086bbae67aa518e28bf4486187b059e24
Gabe Kangas <gabek@real-ity.com>
2022-05-07 16:13:06 -0700

    Add action buttons and status bar

--- web/components/action-buttons/ActionButton.tsx
+++ web/components/action-buttons/ActionButton.tsx
@@ -1,17 +1,18 @@
 import { Button } from 'antd';
 import { ExternalAction } from '../interfaces/external-action.interface';
+import s from './ActionButton.module.scss';
 
 interface Props {
   action: ExternalAction;
 }
 
-export default function ExternalActionButton(props: Props) {
+export default function ActionButton(props: Props) {
   const { action } = props;
   const { url, title, description, icon, color, openExternally } = action;
 
   return (
-    <Button type="primary" style={{ backgroundColor: color }}>
-      <img src={icon} width="30px" alt={description} />
+    <Button type="primary" className={`${s.button}`} style={{ backgroundColor: color }}>
+      <img src={icon} className={`${s.icon}`} alt={description} />
       {title}
     </Button>
   );
--- web/components/video/StatusBar.jsx
+++
@@ -1,9 +0,0 @@
-interface Props {
-  online: boolean;
-  viewers: number;
-  timer: string;
-}
-
-export default function StatusBar(props: Props) {
-  return <div>Stream status bar goes here</div>;
-}
