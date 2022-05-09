21585f5aeafe5f5b93ec6a38ffaa2e3ebf55a388
Gabe Kangas <gabek@real-ity.com>
2022-05-08 18:05:37 -0700

    Use title or name in header

--- web/components/layouts/Main.tsx
+++ web/components/layouts/Main.tsx
@@ -1,15 +1,20 @@
 import { Layout } from 'antd';
+import { useRecoilValue } from 'recoil';
 import { ServerStatusStore } from '../stores/ServerStatusStore';
-import { ClientConfigStore } from '../stores/ClientConfigStore';
+import { ClientConfigStore, clientConfigStateAtom } from '../stores/ClientConfigStore';
 import { Content, Header } from '../ui';
+import { ClientConfig } from '../../interfaces/client-config.model';
 
 function Main() {
+  const clientConfig = useRecoilValue<ClientConfig>(clientConfigStateAtom);
+  const { name, title } = clientConfig;
+
   return (
     <>
       <ServerStatusStore />
       <ClientConfigStore />
       <Layout>
-        <Header />
+        <Header name={title || name} />
         <Content />
       </Layout>
     </>
