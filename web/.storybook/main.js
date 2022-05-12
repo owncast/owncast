e13a83808bab05904fd2cee80c742ce1e3296a26
Gabe Kangas <gabek@real-ity.com>
2022-05-12 15:41:54 -0700

    Add some links and context to readme

--- web/.storybook/main.js
+++ web/.storybook/main.js
@@ -2,7 +2,11 @@ module.exports = {
   core: {
     builder: 'webpack5',
   },
-  stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: [
+    '../stories/Introduction.stories.mdx',
+    '../stories/**/*.stories.mdx',
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
--- web/components/modals/BrowserNotifyModal.tsx
+++ web/components/modals/BrowserNotifyModal.tsx
@@ -1,6 +1,36 @@
+/* eslint-disable react/no-unescaped-entities */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-interface Props {}
+import { Row, Col, Switch, Typography } from 'antd';
+import { useState } from 'react';
 
-export default function BrowserNotifyModal(props: Props) {
-  return <div>Component goes here</div>;
+const { Title } = Typography;
+
+// interface Props {}
+
+export default function BrowserNotifyModal() {
+  const [enabled, setEnabled] = useState(false);
+
+  const onSwitchToggle = (checked: Boolean) => {
+    setEnabled(true);
+  };
+
+  return (
+    <div>
+      <Row align="top">
+        <Col span={12}>
+          <Switch defaultChecked={enabled} checked={enabled} onChange={onSwitchToggle} />{' '}
+          {enabled ? 'Enabled' : 'Disabled'}
+        </Col>
+        <Col span={12}>
+          You'll need to allow your browser to receive notifications from Owncast Nightly, first.
+          Fake push notification prompt example goes here.
+        </Col>
+      </Row>
+      <Row align="top">
+        <Title>Browser Notifications</Title>
+        Get notified right in the browser each time this stream goes live. Blah blah blah more
+        description text goes here.
+      </Row>
+    </div>
+  );
 }
