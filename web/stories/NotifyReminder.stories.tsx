c189a1e6f9d0f30ba09305334e01a77389cfc586
Gabe Kangas <gabek@real-ity.com>
2022-05-16 20:06:48 -0700

    Add notification reminder popup component

--- web/stories/NotifyReminder.stories.tsx
+++ web/stories/NotifyReminder.stories.tsx
@@ -1,7 +1,16 @@
+/* eslint-disable no-alert */
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import NotifyReminder from '../components/ui/NotifyReminderPopup/NotifyReminderPopup';
 import Mock from './assets/mocks/notify-popup.png';
+
+const Example = args => (
+  <div style={{ margin: '20px', marginTop: '130px' }}>
+    <NotifyReminder {...args}>
+      <button type="button">notify button</button>
+    </NotifyReminder>
+  </div>
+);
 
 export default {
   title: 'owncast/Notify Reminder',
@@ -14,13 +23,26 @@ export default {
     docs: {
       description: {
         component: `After visiting the page three times this popup reminding you that you can register for live stream notifications shows up.
-Clicking it will make the notificaiton modal display. Clicking the "X" will hide the modal and make it never show again.`,
+Clicking it will make the notification modal display. Clicking the "X" will hide the modal and make it never show again.`,
       },
     },
   },
 } as ComponentMeta<typeof NotifyReminder>;
 
-const Template: ComponentStory<typeof NotifyReminder> = args => <NotifyReminder {...args} />;
+const Template: ComponentStory<typeof NotifyReminder> = args => <Example {...args} />;
 
-export const Example = Template.bind({});
-Example.args = {};
+export const Active = Template.bind({});
+Active.args = {
+  visible: true,
+  notificationClicked: () => {
+    alert('notification clicked');
+  },
+  notificationClosed: () => {
+    alert('notification closed');
+  },
+};
+
+export const InActive = Template.bind({});
+InActive.args = {
+  visible: false,
+};
