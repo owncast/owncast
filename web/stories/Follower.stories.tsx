3c291556e0aa21c866b3533f94329b9656aebf18
Gabe Kangas <gabek@real-ity.com>
2022-05-20 13:12:16 -0700

    Add mock to follower

--- web/stories/Follower.stories.tsx
+++ web/stories/Follower.stories.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import SingleFollower from '../components/Follower';
+import SingleFollowerMock from './assets/mocks/single-follower.png';
 
 export default {
   title: 'owncast/Components/Followers/Single Follower',
   component: SingleFollower,
-  parameters: {},
+  parameters: {
+    design: {
+      type: 'image',
+      url: SingleFollowerMock,
+    },
+    docs: {
+      description: {
+        component: `Represents a single follower.`,
+      },
+    },
+  },
 } as ComponentMeta<typeof SingleFollower>;
 
 const Template: ComponentStory<typeof SingleFollower> = args => <SingleFollower {...args} />;
