d692a2bb5c51422402ec6b08d4a8af0849e21cb0
t1enne <nasir.taov@gmail.com>
2022-05-22 16:10:34 +0200

    Styled chat messages

--- web/components/chat/index.ts
+++ web/components/chat/index.ts
@@ -1,2 +1,3 @@
 export { default as ChatContainer } from './ChatContainer';
+export { default as ChatUserMessage } from './ChatUserMessage';
 export { default as ChatTextField } from './ChatTextField/ChatTextField';
