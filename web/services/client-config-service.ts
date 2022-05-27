e9b43f0150eab65a34c38641710295a19fae007c
Gabe Kangas <gabek@real-ity.com>
2022-05-27 15:08:59 -0700

    Support retries fetching server config

--- web/services/client-config-service.ts
+++ web/services/client-config-service.ts
@@ -1,9 +1,15 @@
 import { ClientConfig } from '../interfaces/client-config.model';
+
+const originalFetch = require('isomorphic-fetch');
+const fetch = require('fetch-retry')(originalFetch);
 const ENDPOINT = `http://localhost:8080/api/config`;
 
 class ClientConfigService {
   public static async getConfig(): Promise<ClientConfig> {
-    const response = await fetch(ENDPOINT);
+    const response = await fetch(ENDPOINT, {
+      retries: 20,
+      retryDelay: (attempt, error, response) => 3 ** attempt * 1000,
+    });
     const status = await response.json();
     return status;
   }
