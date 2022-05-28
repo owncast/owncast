3fa276e7c8db0fcdff943097a4865d7a2964b59e
Gabe Kangas <gabek@real-ity.com>
2022-05-27 18:44:26 -0700

    Removing fetch retries for now until I write it from scratch

--- web/services/client-config-service.ts
+++ web/services/client-config-service.ts
@@ -1,15 +1,10 @@
 import { ClientConfig } from '../interfaces/client-config.model';
 
-const originalFetch = require('isomorphic-fetch');
-const fetch = require('fetch-retry')(originalFetch);
 const ENDPOINT = `http://localhost:8080/api/config`;
 
 class ClientConfigService {
   public static async getConfig(): Promise<ClientConfig> {
-    const response = await fetch(ENDPOINT, {
-      retries: 20,
-      retryDelay: (attempt, error, response) => 3 ** attempt * 1000,
-    });
+    const response = await fetch(ENDPOINT);
     const status = await response.json();
     return status;
   }
