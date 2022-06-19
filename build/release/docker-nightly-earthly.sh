7abc1c41ff99b2df18fccb46e56df15f4d699d7c
Gabe Kangas <gabek@real-ity.com>
2022-06-18 20:46:05 -0700

    Point to most recent earthly + cd to root on build

--- build/release/docker-nightly-earthly.sh
+++ build/release/docker-nightly-earthly.sh
@@ -8,6 +8,9 @@ VERSION="${DATE}-nightly"
 
 echo "Building Docker image ${DOCKER_IMAGE}..."
 
+# Change to the root directory of the repository
+cd $(git rev-parse --show-toplevel)
+
 earthly --ci --push +docker --image="ghcr.io/owncast/${DOCKER_IMAGE}" --tag=nightly --version=${VERSION}
 
 # docker push ghcr.io/owncast/${DOCKER_IMAGE}:nightly
