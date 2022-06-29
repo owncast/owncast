972e42abdc780c45a364733637efc20576b3ddb4
Gabe Kangas <gabek@real-ity.com>
2022-06-29 14:12:31 -0700

    Fix linter warnings

--- web/stories/Modal.stories.tsx
+++ web/stories/Modal.stories.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import Modal from '../components/ui/Modal/Modal';
 
