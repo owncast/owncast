100b43dd3a43aa69f09e2d2549a89e4419be5bf1
Gabe Kangas <gabek@real-ity.com>
2022-07-10 17:00:56 -0700

    Fix build errors

--- web/components/ui/Sidebar/Sidebar.tsx
+++ web/components/ui/Sidebar/Sidebar.tsx
@@ -6,15 +6,12 @@ import s from './Sidebar.module.scss';
 
 import {
   chatMessagesAtom,
-  appStateAtom,
   chatDisplayNameAtom,
   chatUserIdAtom,
 } from '../../stores/ClientConfigStore';
-import { AppStateOptions } from '../../stores/application-state';
 
 export default function Sidebar() {
   const messages = useRecoilValue<ChatMessage[]>(chatMessagesAtom);
-  const appState = useRecoilValue<AppStateOptions>(appStateAtom);
   const chatDisplayName = useRecoilValue<string>(chatDisplayNameAtom);
   const chatUserId = useRecoilValue<string>(chatUserIdAtom);
 
@@ -22,7 +19,6 @@ export default function Sidebar() {
     <Sider className={s.root} collapsedWidth={0} width={320}>
       <ChatContainer
         messages={messages}
-        loading={appState.chatLoading}
         usernameToHighlight={chatDisplayName}
         chatUserId={chatUserId}
         isModerator={false}
