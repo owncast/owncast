5d34279862cf539ea33572beb585ba38dadf48d1
Gabe Kangas <gabek@real-ity.com>
2022-08-02 15:13:52 -0700

    Add util for ungraceful sql execs

--- core/data/utils.go
+++ core/data/utils.go
@@ -6,8 +6,8 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-// mustExec will execute a SQL statement on a provided database instance.
-func mustExec(s string, db *sql.DB) {
+// MustExec will execute a SQL statement on a provided database instance.
+func MustExec(s string, db *sql.DB) {
 	stmt, err := db.Prepare(s)
 	if err != nil {
 		log.Fatal(err)
