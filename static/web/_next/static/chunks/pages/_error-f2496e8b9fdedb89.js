6eb9c54b3711c531bbe3e203ddd90e1e36bd744b
gabek <gabek@users.noreply.github.com>
2022-08-16 02:47:50 +0000

    Bundle embedded web app

---
+++ static/web/_next/static/chunks/pages/_error-f2496e8b9fdedb89.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[4820],{81981:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return u(89185)}])}},function(n){n.O(0,[9774,2888,179],(function(){return _=81981,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
