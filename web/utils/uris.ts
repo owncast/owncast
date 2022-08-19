077d65b458ec01d499197f955e62966e6f96693d
cel 🌸 <cel@blos.sm>
2022-08-20 00:46:14 +0100

    Improve URI validator

--- web/utils/uris.ts
+++ web/utils/uris.ts
@@ -1,3 +1,28 @@
 export default function isValidURI(uri: string): boolean {
-  return uri.includes(':');
+  if (uri.includes(':')) {
+    const splittedURI = uri.split(':', 2);
+    if (
+      splittedURI[0] === ''
+    ) {
+      return false;
+    } else if (
+      splittedURI[1] === ''
+    ) {
+      return false;
+    } else if (
+      splittedURI[0] === 'http' ||
+      splittedURI[0] === 'https'
+    ) {
+      if (
+        (uri.slice(0,7) === 'http://' ||
+        uri.slice(0,8) === 'https://') &&
+        splittedURI[1].includes('.')
+      ) {
+        return true;
+      }
+      return false;
+    }
+    return true;
+  }
+  return false;
 }
