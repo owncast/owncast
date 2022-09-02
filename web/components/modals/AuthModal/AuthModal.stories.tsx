2810bdc31747ba33de5c4169899c1ef6e46d2088
James Young <hello@jamesyoung.ch>
2022-09-03 01:54:12 +0200

    refactor: move AuthModal component

--- web/components/modals/AuthModal/AuthModal.stories.tsx
+++ web/components/modals/AuthModal/AuthModal.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { RecoilRoot } from 'recoil';
-import AuthModal from '../components/modals/AuthModal/AuthModal';
+import AuthModal from './AuthModal';
 
 const Example = () => (
   <div>
