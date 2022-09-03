c69db2d55bd8a2eaf281758007f51600b6d50107
James Young <hello@jamesyoung.ch>
2022-09-03 02:57:23 +0200

    refactor: update storybook story import path

--- web/.storybook/main.js
+++ web/.storybook/main.js
@@ -4,8 +4,9 @@ module.exports = {
   },
   stories: [
     './Introduction.stories.mdx',
-    '../stories/**/*.stories.mdx',
-    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
+    '../stories/**/*.stories.mdx', // TODO remove
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)', // TODO remove
+		'../components/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: [
     '@storybook/addon-links',
