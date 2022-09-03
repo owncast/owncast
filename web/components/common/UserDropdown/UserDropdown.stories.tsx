840720aa5d923c71c4c88ba3c711a87c8a32ce12
James Young <hello@jamesyoung.ch>
2022-09-03 02:37:34 +0200

    refactor: move UserDropdown component

--- web/components/common/UserDropdown/UserDropdown.stories.tsx
+++ web/components/common/UserDropdown/UserDropdown.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { RecoilRoot } from 'recoil';
-import { UserDropdown } from '../components/common';
+import UserDropdown from './UserDropdown';
 
 export default {
   title: 'owncast/Components/User settings menu',
