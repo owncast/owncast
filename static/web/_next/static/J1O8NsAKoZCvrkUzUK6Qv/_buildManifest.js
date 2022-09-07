06d7eb5287d2db89749642c6d555a6a063853548
gabek <gabek@users.noreply.github.com>
2022-09-07 07:02:41 +0000

    Bundle embedded web app

---
+++ static/web/_next/static/J1O8NsAKoZCvrkUzUK6Qv/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/api/:path*"},{source:"/hls/:path*"},{source:"/img/:path*"},{source:"/logo"},{source:"/thumbnail.jpg"}],fallback:[]},"/_error":["static/chunks/pages/_error-942918685354aae9.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
