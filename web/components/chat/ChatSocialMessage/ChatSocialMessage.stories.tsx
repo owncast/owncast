1ac6891446bd281c06e498358def6774e4aa2d37
Gabe Kangas <gabek@real-ity.com>
2022-10-05 22:43:24 -0700

    First pass at the social/federated chat component.  Closes #2172

--- web/components/chat/ChatSocialMessage/ChatSocialMessage.stories.tsx
+++ web/components/chat/ChatSocialMessage/ChatSocialMessage.stories.tsx
@@ -8,8 +8,37 @@ export default {
   parameters: {},
 } as ComponentMeta<typeof ChatSocialMessage>;
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const Template: ComponentStory<typeof ChatSocialMessage> = args => <ChatSocialMessage {...args} />;
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const Basic = Template.bind({});
+export const Follow = Template.bind({});
+Follow.args = {
+  message: {
+    type: 'follow',
+    body: 'james followed this live stream.',
+    title: 'james@mastodon.social',
+    image: 'https://mastodon.social/avatars/original/missing.png',
+    link: 'https://mastodon.social/@james',
+  },
+};
+
+export const Like = Template.bind({});
+Like.args = {
+  message: {
+    type: 'like',
+    body: 'james liked that this stream went live.',
+    title: 'james@mastodon.social',
+    image: 'https://mastodon.social/avatars/original/missing.png',
+    link: 'https://mastodon.social/@james',
+  },
+};
+
+export const Repost = Template.bind({});
+Repost.args = {
+  message: {
+    type: 'repost',
+    body: 'james shared this stream with their followers.',
+    title: 'james@mastodon.social',
+    image: 'https://mastodon.social/avatars/original/missing.png',
+    link: 'https://mastodon.social/@james',
+  },
+};
