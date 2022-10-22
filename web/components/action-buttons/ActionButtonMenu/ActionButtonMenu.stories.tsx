f2f3dcfea7a9ebee1cf642fe1f21cf4a79d03d9f
Gabe Kangas <gabek@real-ity.com>
2022-10-21 23:58:39 -0700

    Add mobile actions dropdown menu. Closes #2231

---
+++ web/components/action-buttons/ActionButtonMenu/ActionButtonMenu.stories.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { ActionButtonMenu } from './ActionButtonMenu';
+
+export default {
+  title: 'owncast/Components/Action Buttons/Action Menu',
+  component: ActionButtonMenu,
+  parameters: {},
+} as ComponentMeta<typeof ActionButtonMenu>;
+
+const itemSelected = a => {
+  console.log('itemSelected', a);
+  action(a.title);
+};
+
+const Template: ComponentStory<typeof ActionButtonMenu> = args => (
+  <ActionButtonMenu {...args} externalActionSelected={a => itemSelected(a)} />
+);
+
+const actions = [
+  {
+    url: 'https://owncast.online/docs',
+    title: 'Documentation',
+    description: 'Owncast Documentation',
+    icon: 'https://owncast.online/images/logo.svg',
+    color: '#5232c8',
+    openExternally: false,
+  },
+  {
+    url: 'https://opencollective.com/embed/owncast/donate',
+    title: 'Support Owncast',
+    description: 'Contribute to Owncast',
+    icon: 'https://opencollective.com/static/images/opencollective-icon.svg',
+    color: '#2b4863',
+    openExternally: false,
+  },
+];
+
+export const Example = Template.bind({});
+Example.args = {
+  actions,
+};
