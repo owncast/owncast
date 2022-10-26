c8968eaded9a40bf9ad8c2d714a9bf85cc8b3b5b
Matthew Heller <hellergood@gmail.com>
2022-10-26 16:34:20 -0500

    On narrow screens, bump the footer links to a second row. Re #2260. Thanks to @kittee for CSS assistance.

--- web/components/ui/Footer/Footer.tsx
+++ web/components/ui/Footer/Footer.tsx
@@ -7,26 +7,20 @@ export type FooterProps = {
 
 export const Footer: FC<FooterProps> = ({ version }) => (
   <div className={styles.footer}>
-    <div className={styles.text}>
+    <span>
       Powered by <a href="https://owncast.online">{version}</a>
-    </div>
-    <div className={styles.links}>
-      <div className={styles.item}>
+    </span>
+    <span className={styles.links}>
         <a href="https://owncast.online/docs" target="_blank" rel="noreferrer">
           Documentation
         </a>
-      </div>
-      <div className={styles.item}>
         <a href="https://owncast.online/help" target="_blank" rel="noreferrer">
           Contribute
         </a>
-      </div>
-      <div className={styles.item}>
         <a href="https://github.com/owncast/owncast" target="_blank" rel="noreferrer">
           Source
         </a>
-      </div>
-    </div>
+    </span>
   </div>
 );
 export default Footer;
