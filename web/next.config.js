42facacd2437c11cdafaa5193a1bc9fb8ca19fe6
Gabe Kangas <gabek@real-ity.com>
2022-10-28 20:19:11 -0700

    Set next setting to not try to optimize images

--- web/next.config.js
+++ web/next.config.js
@@ -7,6 +7,9 @@ module.exports = withBundleAnalyzer(
   withLess({
     trailingSlash: true,
     reactStrictMode: true,
+    images: {
+      unoptimized: true,
+    },
     webpack(config) {
       config.module.rules.push({
         test: /\.svg$/i,
