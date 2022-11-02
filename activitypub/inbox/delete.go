41a132635ebe67b4ab5424da2b79afdb1cbeea61
Matt Owens <matt@brokenintuition.com>
2022-11-01 22:02:52 -0400

    handle more than just ActivityStreamsPerson

--- activitypub/inbox/delete.go
+++ activitypub/inbox/delete.go
@@ -2,24 +2,32 @@ package inbox
 
 import (
 	"context"
+	"net/url"
 
 	"github.com/go-fed/activity/streams/vocab"
 	"github.com/owncast/owncast/activitypub/persistence"
-	"github.com/owncast/owncast/activitypub/resolvers"
-	log "github.com/sirupsen/logrus"
 )
 
 func handleDeleteRequest(c context.Context, activity vocab.ActivityStreamsDelete) error {
-	// Only need to handle deletes for followers
-	if !activity.GetActivityStreamsObject().At(0).IsActivityStreamsPerson() {
-		return nil
+	obj := activity.GetActivityStreamsObject()
+	if obj != nil && obj.Len() > 0 && obj.At(0) != nil {
+		o := obj.At(0)
+
+		var actorIri *url.URL
+		switch true {
+		case o.IsActivityStreamsPerson():
+			actorIri = o.GetActivityStreamsPerson().GetJSONLDId().Get()
+		case o.IsActivityStreamsService():
+			actorIri = o.GetActivityStreamsService().GetJSONLDId().Get()
+		case o.IsActivityStreamsApplication():
+			actorIri = o.GetActivityStreamsApplication().GetJSONLDId().Get()
+		default:
+			// not a type we care about here
+			return nil
+		}
+
+		return persistence.RemoveFollowIRI(actorIri)
 	}
 
-	actor, err := resolvers.GetResolvedActorFromActorProperty(activity.GetActivityStreamsActor())
-	if err != nil {
-		log.Error(err)
-		return err
-	}
-
-	return persistence.RemoveFollow(actor)
+	return nil
 }
