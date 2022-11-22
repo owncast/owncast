60991f8368254817aa260ecdff988e66a00d25ae
Meisam <39205857+MFTabriz@users.noreply.github.com>
2022-11-22 16:11:13 +0100

    Create nodeinfo.test.js

---
+++ test/automated/api/nodeinfo.test.js
@@ -0,0 +1,16 @@
+var request = require('supertest')
+const Ajv = require("ajv")
+
+request = request('http://127.0.0.1:8080');
+const ajv = new Ajv();
+
+var schema = new FileReader();
+schema.readAsText('schema/node_info_2.0_schema.json');
+
+test('nodeinfo is valid', (done) => {
+    request.get('/nodeinfo/2.0').expect(200)
+        .then((res) => {
+            expect(ajv.validate(schema, res.body)).toBe(true);
+            done();
+        });
+});
