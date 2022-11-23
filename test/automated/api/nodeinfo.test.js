d93474e7da8591b43b8cf9019e21950e0ab29b65
Meisam <39205857+MFTabriz@users.noreply.github.com>
2022-11-23 22:18:10 +0100

    rev

--- test/automated/api/nodeinfo.test.js
+++ test/automated/api/nodeinfo.test.js
@@ -9,7 +9,7 @@ var ajv = new Ajv();
 var schema = jsonfile.readFileSync('schema/node_info_2.0_schema.json');
 
 test('nodeinfo 2.0 is valid', (done) => {
-    get('/nodeinfo/2.0').expect(200)
+    request.get('/nodeinfo/2.0').expect(200)
         .then((res) => {
             expect(ajv.validate(schema, res.body)).toBe(true);
             done();
