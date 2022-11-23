62a5c1bedaef2df573bb2e082d90697ef1262707
Meisam <39205857+MFTabriz@users.noreply.github.com>
2022-11-23 21:25:17 +0100

    add json-schema-draft-04

--- test/automated/api/nodeinfo.test.js
+++ test/automated/api/nodeinfo.test.js
@@ -3,15 +3,23 @@ const Ajv = require("ajv")
 const jsonfile = require('jsonfile')
 
 request = request('http://127.0.0.1:8080');
-const ajv = new Ajv();
- 
-var schema = jsonfile.readFileSync('schema/node_info_2.0_schema.json');
-console.dir(schema);
+
+var ajv = new Ajv({
+    meta: false, // optional, to prevent adding draft-06 meta-schema
+    extendRefs: true, // optional, current default is to 'fail', spec behaviour is to 'ignore'
+    unknownFormats: 'ignore',  // optional, current default is true (fail)
+    // ...
+  });
+var metaSchema = require('ajv/lib/refs/json-schema-draft-04.json');
+ajv.addMetaSchema(metaSchema);
+ajv._opts.defaultMeta = metaSchema.id;
+
+ajv.addSchema(require('schema/node_info_2.0_schema.json'))
 
 test('nodeinfo 2.0 is valid', (done) => {
     request.get('/nodeinfo/2.0').expect(200)
         .then((res) => {
-            expect(ajv.validate(schema, res.body)).toBe(true);
+            expect(ajv.validate('http://nodeinfo.diaspora.software/ns/schema/2.0#', res.body)).toBe(true);
             done();
         });
 });
