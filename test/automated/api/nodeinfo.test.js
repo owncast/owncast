ed3223cecbc563145d5e948e56f5313031aa640f
Meisam <39205857+MFTabriz@users.noreply.github.com>
2022-11-23 17:58:43 +0100

    use read-text-file

--- test/automated/api/nodeinfo.test.js
+++ test/automated/api/nodeinfo.test.js
@@ -1,11 +1,11 @@
-var request = require('supertest')
+var request = require('supertest') 
 const Ajv = require("ajv")
+var readTextFile = require('read-text-file');
 
 request = request('http://127.0.0.1:8080');
 const ajv = new Ajv();
-
-var schema = new FileReader();
-schema.readAsText('schema/node_info_2.0_schema.json');
+ 
+var schema = readTextFile.readSync('schema/node_info_2.0_schema.json');
 
 test('nodeinfo 2.0 is valid', (done) => {
     request.get('/nodeinfo/2.0').expect(200)
