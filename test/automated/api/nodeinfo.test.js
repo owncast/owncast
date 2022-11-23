a0861628952a66741f0251eebe5ce62d21c9c553
Meisam <39205857+MFTabriz@users.noreply.github.com>
2022-11-23 21:35:13 +0100

    update schema path

--- test/automated/api/nodeinfo.test.js
+++ test/automated/api/nodeinfo.test.js
@@ -4,15 +4,9 @@ const jsonfile = require('jsonfile')
 
 request = request('http://127.0.0.1:8080');
 
-var ajv = new Ajv({
-    meta: false, // optional, to prevent adding draft-06 meta-schema
-    extendRefs: true, // optional, current default is to 'fail', spec behaviour is to 'ignore'
-    unknownFormats: 'ignore',  // optional, current default is true (fail)
-    // ...
-  });
-var metaSchema = require('ajv/lib/refs/json-schema-draft-04.json');
+var ajv = new Ajv();
+var metaSchema = require('ajv/dist/refs/json-schema-draft-04.json');
 ajv.addMetaSchema(metaSchema);
-ajv._opts.defaultMeta = metaSchema.id;
 
 ajv.addSchema(require('schema/node_info_2.0_schema.json'))
 
