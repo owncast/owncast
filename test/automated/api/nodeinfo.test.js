615df6fe4cafa70c814b2d9bb9f7251922a06bce
Meisam <39205857+MFTabriz@users.noreply.github.com>
2022-11-23 22:09:41 +0100

    read with jsonfile

--- test/automated/api/nodeinfo.test.js
+++ test/automated/api/nodeinfo.test.js
@@ -1,16 +1,17 @@
 var request = require('supertest')
+const jsonfile = require('jsonfile')
 const Ajv = require("ajv-draft-04")
 
 request = request('http://127.0.0.1:8080');
 
 var ajv = new Ajv();
 
-ajv.addSchema(require('schema/node_info_2.0_schema.json'))
+var schema = jsonfile.readFileSync('schema/node_info_2.0_schema.json');
 
 test('nodeinfo 2.0 is valid', (done) => {
     get('/nodeinfo/2.0').expect(200)
         .then((res) => {
-            expect(ajv.validate('http://nodeinfo.diaspora.software/ns/schema/2.0#', res.body)).toBe(true);
+            expect(ajv.validate(schema, res.body)).toBe(true);
             done();
         });
 });
