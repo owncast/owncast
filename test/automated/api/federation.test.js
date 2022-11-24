79c2ca768d27fc17eccc755a4b00656bf5d9c18d
Meisam <39205857+MFTabriz@users.noreply.github.com>
2022-11-25 00:42:07 +0100

    separate tests of each endpoint
    check the federation apis after switch

--- test/automated/api/federation.test.js
+++ test/automated/api/federation.test.js
@@ -9,57 +9,133 @@ var ajv = new Ajv();
 var nodeInfoSchema = jsonfile.readFileSync('schema/node_info_2.0_schema.json');
 
 test('disable federation', async (done) => {
-    const res = await sendConfigChangeRequest('federation/enable', false);
-    done();
-  });
+  const res = await sendConfigChangeRequest('federation/enable', false);
+  done();
+});
 
-test('check disabled federation responses', async (done) => {
-    const res1 = request
-    .get('/.well-known/webfinger')
-    .expect(405);
+test('check disabled federation responses of /.well-known/webfinger', async (done) => {
+  const res = request
+  .get('/.well-known/webfinger')
+  .expect(405);
+  done();
+});
 
-    const res2 = request
-    .get('/.well-known/host-meta')
-    .expect(405);
-  
-    const res3 = request
-    .get('/.well-known/nodeinfo')
-    .expect(405);
+test('check disabled federation responses of /.well-known/host-meta', async (done) => {
+  const res = request
+  .get('/.well-known/host-meta')
+  .expect(405);
+  done();
+});
 
-    const res4 = request
-    .get('/.well-known/x-nodeinfo2')
-    .expect(405);
-    
-    const res5 = request
-    .get('/nodeinfo/2.0')
-    .expect(405);
-    
-    const res6 = request
-    .get('/api/v1/instance')
-    .expect(405);
+test('check disabled federation responses of /.well-known/nodeinfo', async (done) => {
+  const res = request
+  .get('/.well-known/nodeinfo')
+  .expect(405);
+  done();
+});
 
-    const res7 = request
-    .get('/federation/user/')
-    .expect(405);
+test('check disabled federation responses of /.well-known/x-nodeinfo2', async (done) => {
+  const res = request
+  .get('/.well-known/x-nodeinfo2')
+  .expect(405);
+  done();
+});
 
-    const res8 = request
-    .get('/federation/')
-    .expect(405);
+test('check disabled federation responses of /nodeinfo/2.0', async (done) => { 
+  const res = request
+  .get('/nodeinfo/2.0')
+  .expect(405);
+  done();
+});
 
-    done();
-  });
+test('check disabled federation responses of /api/v1/instance', async (done) => {
+  const res = request
+  .get('/api/v1/instance')
+  .expect(405);
+  done();
+});
 
-  test('enable federation', async (done) => {
-    const res = await sendConfigChangeRequest('federation/enable', true);
-    done();
-  });
+test('check disabled federation responses of /federation/user/', async (done) => {
+  const res = request
+  .get('/federation/user/')
+  .expect(405);
+  done();
+});
+
+test('check disabled federation responses of /federation/', async (done) => {
+  const res = request
+  .get('/federation/')
+  .expect(405);
+  done();
+});
+
+test('enable federation', async (done) => {
+  const res = await sendConfigChangeRequest('federation/enable', true);
+  done();
+});
+
+test('check enabled federation responses of /.well-known/webfinger', async (done) => {
+  const res = request
+  .get('/.well-known/webfinger')
+  .expect(200);
+  done();
+});
+
+test('check enabled federation responses of /.well-known/host-meta', async (done) => {
+  const res = request
+  .get('/.well-known/host-meta')
+  .expect(200);
+  done();
+});
+
+test('check enabled federation responses of /.well-known/nodeinfo', async (done) => {
+  const res = request
+  .get('/.well-known/nodeinfo')
+  .expect(200);
+  done();
+});
+
+test('check enabled federation responses of /.well-known/x-nodeinfo2', async (done) => {
+  const res = request
+  .get('/.well-known/x-nodeinfo2')
+  .expect(200);
+  done();
+});
+
+test('check enabled federation responses of /nodeinfo/2.0', async (done) => { 
+  const res = request
+  .get('/nodeinfo/2.0')
+  .expect(200);
+  done();
+});
+
+test('check enabled federation responses of /api/v1/instance', async (done) => {
+  const res = request
+  .get('/api/v1/instance')
+  .expect(200);
+  done();
+});
+
+test('check enabled federation responses of /federation/user/', async (done) => {
+  const res = request
+  .get('/federation/user/')
+  .expect(200);
+  done();
+});
+
+test('check enabled federation responses of /federation/', async (done) => {
+  const res = request
+  .get('/federation/')
+  .expect(200);
+  done();
+});
 
 test('nodeinfo 2.0 is valid', (done) => {
-    request.get('/nodeinfo/2.0').expect(200)
-        .then((res) => {
-            expect(ajv.validate(nodeInfoSchema, res.body)).toBe(true);
-            done();
-        });
+  request.get('/nodeinfo/2.0').expect(200)
+    .then((res) => {
+      expect(ajv.validate(nodeInfoSchema, res.body)).toBe(true);
+      done();
+    });
 });
 
 
