21f0b3b263bac26410b7d82318a384567ac873f9
gabek <gabek@users.noreply.github.com>
2022-11-29 23:50:00 +0000

    Prettified Code!

--- test/automated/api/lib/admin.js
+++ test/automated/api/lib/admin.js
@@ -4,46 +4,54 @@ request = request('http://127.0.0.1:8080');
 const defaultAdminPassword = 'abc123';
 
 async function getAdminConfig(adminPassword = defaultAdminPassword) {
-  const res = request
-    .get('/api/admin/serverconfig')
-    .auth('admin', adminPassword)
-    .expect(200);
+	const res = request
+		.get('/api/admin/serverconfig')
+		.auth('admin', adminPassword)
+		.expect(200);
 
-  return res;
+	return res;
 }
 
 async function getAdminStatus(adminPassword = defaultAdminPassword) {
-  const res = request
-    .get('/api/admin/status')
-    .auth('admin', adminPassword)
-    .expect(200);
+	const res = request
+		.get('/api/admin/status')
+		.auth('admin', adminPassword)
+		.expect(200);
 
-  return res;
+	return res;
 }
 
-async function sendConfigChangeRequest(endpoint, value, adminPassword = defaultAdminPassword) {
-  const url = '/api/admin/config/' + endpoint;
-  const res = await request
-    .post(url)
-    .auth('admin', adminPassword)
-    .send({ value: value })
-    .expect(200);
+async function sendConfigChangeRequest(
+	endpoint,
+	value,
+	adminPassword = defaultAdminPassword
+) {
+	const url = '/api/admin/config/' + endpoint;
+	const res = await request
+		.post(url)
+		.auth('admin', adminPassword)
+		.send({ value: value })
+		.expect(200);
 
-  expect(res.body.success).toBe(true);
-  return res;
+	expect(res.body.success).toBe(true);
+	return res;
 }
 
-async function sendConfigChangePayload(endpoint, payload, adminPassword = defaultAdminPassword) {
-  const url = '/api/admin/config/' + endpoint;
-  const res = await request
-    .post(url)
-    .auth('admin', adminPassword)
-    .send(payload)
-    .expect(200);
+async function sendConfigChangePayload(
+	endpoint,
+	payload,
+	adminPassword = defaultAdminPassword
+) {
+	const url = '/api/admin/config/' + endpoint;
+	const res = await request
+		.post(url)
+		.auth('admin', adminPassword)
+		.send(payload)
+		.expect(200);
 
-  expect(res.body.success).toBe(true);
+	expect(res.body.success).toBe(true);
 
-  return res;
+	return res;
 }
 
 module.exports.getAdminConfig = getAdminConfig;
