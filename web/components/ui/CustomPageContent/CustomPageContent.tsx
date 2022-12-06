3b501fa4df574a9a04fe0ca3e91981b178ab2061
Gabe Kangas <gabek@real-ity.com>
2022-12-05 21:04:03 -0800

    Move footer to not be sticky. Closes #2288

--- web/components/ui/CustomPageContent/CustomPageContent.tsx
+++ web/components/ui/CustomPageContent/CustomPageContent.tsx
@@ -1,25 +1,13 @@
 /* eslint-disable react/no-danger */
 import { FC } from 'react';
-import { useRecoilValue } from 'recoil';
-import Footer from '../Footer/Footer';
 import styles from './CustomPageContent.module.scss';
-import { isMobileAtom, clientConfigStateAtom } from '../../stores/ClientConfigStore';
-import { ClientConfig } from '../../../interfaces/client-config.model';
 
 export type CustomPageContentProps = {
   content: string;
 };
 
-export const CustomPageContent: FC<CustomPageContentProps> = ({ content }) => {
-  const isMobile = useRecoilValue<boolean | undefined>(isMobileAtom);
-  const clientConfig = useRecoilValue<ClientConfig>(clientConfigStateAtom);
-  const { version } = clientConfig;
-  return (
-    <>
-      <div className={styles.pageContentContainer}>
-        <div className={styles.customPageContent} dangerouslySetInnerHTML={{ __html: content }} />
-      </div>
-      {isMobile && <Footer version={version} />}
-    </>
-  );
-};
+export const CustomPageContent: FC<CustomPageContentProps> = ({ content }) => (
+  <div className={styles.pageContentContainer}>
+    <div className={styles.customPageContent} dangerouslySetInnerHTML={{ __html: content }} />
+  </div>
+);
