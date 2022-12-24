96022f4d883b4ff003c702c7dbb2eedb6b981d25
Meisam <39205857+MFTabriz@users.noreply.github.com>
2022-12-24 16:18:37 +0100

    grab docker build params from env

--- build/release/docker-webv2.sh
+++ build/release/docker-webv2.sh
@@ -2,14 +2,16 @@
 
 # Docker build
 # Must authenticate first: https://docs.github.com/en/packages/using-github-packages-with-your-projects-ecosystem/configuring-docker-for-use-with-github-packages#authenticating-to-github-packages
-DOCKER_IMAGE="owncast"
+EARTHLY_IMAGE_NAME="owncast"
+BUILD_TAG=${EARTHLY_BUILD_TAG:-webv2}
 DATE=$(date +"%Y%m%d")
-TAG="webv2"
-VERSION="${DATE}-${TAG}"
-echo "Building Docker image ${DOCKER_IMAGE}..."
+VERSION="${DATE}-${BUILD_TAG}"
+
+
+echo "Building Docker image ${EARTHLY_IMAGE_NAME}:${BUILD_TAG}..."
 
 # Change to the root directory of the repository
 cd "$(git rev-parse --show-toplevel)" || exit
-git checkout webv2
+git checkout "${EARTHLY_BUILD_BRANCH:-webv2}"
 
-earthly --ci --push +docker-all --image="ghcr.io/owncast/${DOCKER_IMAGE}" --tag="${TAG}" --version="${VERSION}"
+earthly --ci --push +docker-all --image="ghcr.io/owncast/${EARTHLY_IMAGE_NAME}" --tag=${BUILD_TAG} --version="${VERSION}"
