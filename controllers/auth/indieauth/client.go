8d64a38d65ee37ea4e39f150c780750bb784b3c9
Gabe Kangas <gabek@real-ity.com>
2022-12-24 20:34:39 -0800

    Do not pass along the raw error. Closes #2491

--- controllers/auth/indieauth/client.go
+++ controllers/auth/indieauth/client.go
@@ -58,7 +58,7 @@ func HandleRedirect(w http.ResponseWriter, r *http.Request) {
 	request, response, err := ia.HandleCallbackCode(code, state)
 	if err != nil {
 		log.Debugln(err)
-		msg := fmt.Sprintf("Unable to complete authentication. <a href=\"/\">Go back.</a><hr/> %s", err.Error())
+		msg := `Unable to complete authentication. <a href="/">Go back.</a><hr/>`
 		_ = controllers.WriteString(w, msg, http.StatusBadRequest)
 		return
 	}
