c74d5b4f31d178e482fa16f5c242dbb25e2fd6f7
Meisam <39205857+MFTabriz@users.noreply.github.com>
2022-12-26 04:17:13 +0100

    add shellcheck to CI (#2478)
    * add shellcheck to ci
    
    * test ci
    
    * install bash for shellcheck
    
    * set globstar for bash
    
    * cleanup shell scripts
    
    * do not ignore automated hls tests
    
    * rm legacy build script
    
    * update shell scripts
    
    * cleanup ci
    
    * Fix misspell
    
    * cleanup ci
    
    * fail on curl error in ci

--- test/automated/browser/run.sh
+++ test/automated/browser/run.sh
@@ -26,7 +26,7 @@ if [ ! -d "ffmpeg" ]; then
 	echo "Downloading ffmpeg..."
 	mkdir -p /tmp/ffmpeg
 	pushd /tmp/ffmpeg >/dev/null
-	curl -sL https://github.com/vot/ffbinaries-prebuilt/releases/download/v4.2.1/ffmpeg-4.2.1-linux-64.zip --output ffmpeg.zip >/dev/null
+	curl -sL --fail https://github.com/vot/ffbinaries-prebuilt/releases/download/v4.2.1/ffmpeg-4.2.1-linux-64.zip --output ffmpeg.zip
 	unzip -o ffmpeg.zip >/dev/null
 	PATH=$PATH:$(pwd)
 	popd >/dev/null
@@ -36,7 +36,7 @@ fi
 echo "Building owncast..."
 go build -o owncast main.go
 echo "Running owncast..."
-./owncast -database $TEMP_DB &
+./owncast -database "$TEMP_DB" &
 SERVER_PID=$!
 
 pushd test/automated/browser
@@ -54,7 +54,7 @@ STREAMING_CLIENT=$!
 
 function finish {
 	echo "Cleaning up..."
-	rm $TEMP_DB
+	rm "$TEMP_DB"
 	kill $SERVER_PID $STREAMING_CLIENT
 }
 trap finish EXIT SIGHUP SIGINT SIGTERM SIGQUIT SIGABRT SIGTERM
--- test/ocTestStream.sh
+++ test/ocTestStream.sh
@@ -4,30 +4,31 @@
 # to repeat indefinitely.
 # Example: ./test/ocTestStream.sh ~/Downloads/*.mp4 rtmp://localhost/live/abc123
 
-if ! ([[ $1 ]])
+if ! [[ $1 ]]
 then
   echo "ocTestStream is used for sending pre-recorded content to a RTMP server."
   echo "Will default to localhost with the stream key of abc123 if one isn't provided."
   echo "./ocTestStream.sh *.mp4 [RTMPDESINATION]"
-exit
+  exit
 fi
 
 # Make the destination optional and point to localhost with default key
-if [[ ${@: -1} == *"rtmp://"* ]]; then
-  echo "RTMP is specified"
-  DESTINATION_HOST=${@: -1}
-  array=( $@ )
-  ARGS_LEN=${#array[@]}
-  CONTENT=${array[@]:0:$ARGS_LEN-1}
-  DESTINATION_HOST=${@: -1}
-  FILE_COUNT=$( expr ${#} - 1 )
+if [[ ${*: -1} == *"rtmp://"* ]]; then
+  echo "RTMP server is specified"
+  DESTINATION_HOST=${*: -1}
+  FILE_COUNT=$(( ${#} - 1 ))
 else
+  echo "RTMP server is not specified"
   DESTINATION_HOST="rtmp://localhost/live/abc123"
-  array=( $@ )
-  ARGS_LEN=${#array[@]}
-  CONTENT=${array[@]:0:$ARGS_LEN}
-  FILE_COUNT=$( expr ${#} )
+  FILE_COUNT=${#}
 fi
+
+if [[ FILE_COUNT -eq 0 ]]; then
+  echo "ERROR: ocTestStream needs a video file for sending to the RTMP server."
+  exit
+fi
+
+CONTENT=${*:1:${FILE_COUNT}}
 
 # Delete the old list of files if it exists
 if test -f list.txt; then
@@ -44,6 +45,11 @@ function finish {
 }
 trap finish EXIT
 
-echo "Streaming a loop of ${FILE_COUNT} videos to $DESTINATION_HOST.  Warning: If these files differ greatly in formats transitioning from one to another may not always work correctly...  ctl+c to exit"
+echo "Streaming a loop of ${FILE_COUNT} videos to $DESTINATION_HOST."
+if [[ FILE_COUNT -gt 1 ]]; then
+  echo "Warning: If these files differ greatly in formats transitioning from one to another may not always work correctly."
+fi
+echo "$CONTENT"
+echo "...press ctl+c to exit"
 
-ffmpeg -hide_banner -loglevel panic -stream_loop -1 -re -f concat -safe 0 -i list.txt -vcodec libx264 -profile:v high -g 48 -r 24 -sc_threshold 0 -b:v 1300k -preset veryfast -acodec copy -vf drawtext="fontfile=monofonto.ttf: fontsize=96: box=1: boxcolor=black@0.75: boxborderw=5: fontcolor=white: x=(w-text_w)/2: y=((h-text_h)/2)+((h-text_h)/4): text='%{gmtime\:%H\\\\\:%M\\\\\:%S}'" -f flv $DESTINATION_HOST
+ffmpeg -hide_banner -loglevel panic -stream_loop -1 -re -f concat -safe 0 -i list.txt -vcodec libx264 -profile:v high -g 48 -r 24 -sc_threshold 0 -b:v 1300k -preset veryfast -acodec copy -vf drawtext="fontfile=monofonto.ttf: fontsize=96: box=1: boxcolor=black@0.75: boxborderw=5: fontcolor=white: x=(w-text_w)/2: y=((h-text_h)/2)+((h-text_h)/4): text='%{gmtime\:%H\\\\\:%M\\\\\:%S}'" -f flv "$DESTINATION_HOST"
