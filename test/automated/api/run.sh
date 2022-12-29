a8d84943ef86b4408c6688bfc6e1fcf8ef5e4b15
Meisam <39205857+MFTabriz@users.noreply.github.com>
2022-12-30 00:09:22 +0100

    update ffmpeg to v4.4.1

--- test/automated/api/run.sh
+++ test/automated/api/run.sh
@@ -9,7 +9,7 @@ npm install --quiet --no-progress
 if [ ! -d "ffmpeg" ]; then
 	mkdir ffmpeg
 	pushd ffmpeg >/dev/null || exit
-	curl -sL https://github.com/vot/ffbinaries-prebuilt/releases/download/v4.2.1/ffmpeg-4.2.1-linux-64.zip --output ffmpeg.zip >/dev/null
+	curl -sL https://github.com/ffbinaries/ffbinaries-prebuilt/releases/download/v4.4.1/ffmpeg-4.4.1-linux-64.zip --output ffmpeg.zip >/dev/null
 	unzip -o ffmpeg.zip >/dev/null
 	PATH=$(pwd):$PATH
 	popd >/dev/null || exit
