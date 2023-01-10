638a1a171bcfc539c286510417e048e249c22d15
gabek <gabek@users.noreply.github.com>
2023-01-10 08:43:46 +0000

    Bundle embedded web app

--- static/web/_next/static/chunks/pages/admin/config/general/GeneralConfig-7056ab100a596adc.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[1673],{52702:function(n,_,e){(window.__NEXT_P=window.__NEXT_P||[]).push(["/admin/config/general/GeneralConfig",function(){return e(98645)}])}},function(n){n.O(0,[5762,481,5596,1407,173,611,1577,3763,4485,5212,4759,7033,8909,3903,3698,3461,6754,878,7910,2792,7916,6538,7291,745,8645,9774,2888,179],function(){return n(n.s=52702)}),_N_E=n.O()}]);
-//# sourceMappingURL=GeneralConfig-7056ab100a596adc.js.map
