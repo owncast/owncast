b1fc050a8b8174c0574f27b6d598ca7a791f87c7
gabek <gabek@users.noreply.github.com>
2023-01-10 08:49:18 +0000

    Bundle embedded web app

--- static/web/_next/static/chunks/3625.3c1a51dbf0db4583.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[3625],{3625:function(e,t,l){l.r(t),l.d(t,{FatalErrorStateModal:function(){return a}});var r=l(85893),n=l(85402);let a=e=>{let{title:t,message:l}=e;return(0,r.jsx)(n.Z,{title:t,visible:!0,footer:null,closable:!1,keyboard:!1,width:900,centered:!0,className:"modal",children:(0,r.jsx)("p",{style:{fontSize:"1.3rem"},children:l})})}}}]);
-//# sourceMappingURL=3625.3c1a51dbf0db4583.js.map
---
+++ static/web/_next/static/chunks/3625.c41a7b35bf6126d7.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[3625],{3625:function(e,t,l){l.r(t),l.d(t,{FatalErrorStateModal:function(){return n}});var r=l(85893),s=l(85402);const n=e=>{let{title:t,message:l}=e;return(0,r.jsx)(s.Z,{title:t,visible:!0,footer:null,closable:!1,keyboard:!1,width:900,centered:!0,className:"modal",children:(0,r.jsx)("p",{style:{fontSize:"1.3rem"},children:l})})}}}]);
+//# sourceMappingURL=3625.c41a7b35bf6126d7.js.map
