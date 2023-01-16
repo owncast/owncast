41504d2bf7f1842b5d3f03162f3b40dd633d2523
André <andre.rusakow@googlemail.com>
2023-01-16 02:44:34 +0100

    resolve conflict

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 import { ReactElement } from 'react';
-import { Main } from '../components/layouts/Main';
-=======
 import { Main } from '../components/layouts/Main/Main';
->>>>>>> 4efa8716a (fix scrolling issues on mobile)
 
 export default function Home() {
   return <Main />;
