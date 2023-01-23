57beaacd2c602d8ff5e5c462f1b46de26c32df24
Gabe Kangas <gabek@real-ity.com>
2023-01-22 21:07:49 -0800

    Allow HLS test to wait for stream changes

--- test/automated/hls/run.sh
+++ test/automated/hls/run.sh
@@ -13,6 +13,8 @@ start_owncast
 
 start_stream
 
+sleep 10
+
 # Run tests against a fresh install with no settings.
 npm test
 
@@ -21,14 +23,16 @@ kill_with_kids "$STREAM_PID"
 
 # Determine if we should continue testing with S3 configuration.
 if [[ -z "${S3_BUCKET}" ]]; then
-  echo "No S3 configuration is set. Skipping S3 tests!"
-  exit 0
+	echo "No S3 configuration is set. Skipping S3 tests!"
+	exit 0
 fi
 
 # Update the server config to use S3 for storage.
 update_storage_config
 
 start_stream
+
+sleep 10
 
 # Re-run the HLS test against the external storage configuration.
 npm test
