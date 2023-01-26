73184e983eafa59f548a1f3c3af6db1b936680a2
Gabe Kangas <gabek@real-ity.com>
2023-01-26 15:25:52 -0800

    Make shellcheck happy

--- test/automated/hls/run.sh
+++ test/automated/hls/run.sh
@@ -21,21 +21,20 @@ npm test
 # Kill the stream.
 kill_with_kids "$STREAM_PID"
 
-# Remove when #2615 is fixed.
-exit 0
+# Remove comments when #2615 is fixed.
 
 # Determine if we should continue testing with S3 configuration.
-if [[ -z "${S3_BUCKET}" ]]; then
-	echo "No S3 configuration is set. Skipping S3 tests!"
-	exit 0
-fi
+# if [[ -z "${S3_BUCKET}" ]]; then
+# 	echo "No S3 configuration is set. Skipping S3 tests!"
+# 	exit 0
+# fi
 
-# Update the server config to use S3 for storage.
-update_storage_config
+# # Update the server config to use S3 for storage.
+# update_storage_config
 
-start_stream
+# start_stream
 
-sleep 10
+# sleep 10
 
-# Re-run the HLS test against the external storage configuration.
-npm test
+# # Re-run the HLS test against the external storage configuration.
+# npm test
