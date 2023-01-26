a3e77687d44a758ac23b9316fc70f53e205fc51f
Gabe Kangas <gabek@real-ity.com>
2023-01-26 15:23:17 -0800

    Add select documentation to Storybook

--- web/.storybook/tools/generate-stories.sh
+++ web/.storybook/tools/generate-stories.sh
@@ -2,3 +2,8 @@
 
 # Generate the custom Emoji story
 node generate-emoji-story.mjs >../stories-category-doc-pages/Emoji.stories.mdx
+
+# Generate stories out of documentation
+
+# Pull down the doc about development
+curl -s https://raw.githubusercontent.com/owncast/owncast.github.io/master/content/development.md >/tmp/development.md
