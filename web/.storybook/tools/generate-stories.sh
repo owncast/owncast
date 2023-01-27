7d34634778f3e1e1bf26c0a751a8269f59d9c92d
Gabe Kangas <gabek@real-ity.com>
2023-01-26 16:15:06 -0800

    Run documentation story script

--- web/.storybook/tools/generate-stories.sh
+++ web/.storybook/tools/generate-stories.sh
@@ -7,3 +7,4 @@ node generate-emoji-story.mjs >../stories-category-doc-pages/Emoji.stories.mdx
 
 # Pull down the doc about development
 curl -s https://raw.githubusercontent.com/owncast/owncast.github.io/master/content/development.md >/tmp/development.md
+node generate-document-stories.mjs
