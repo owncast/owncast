ee3e81d1635ceaf1aa8605f96d9026e46fa834ef
Gabe Kangas <gabek@real-ity.com>
2023-01-27 20:36:20 -0800

    Fix storybook image paths

--- web/.storybook/tools/generate-stories.sh
+++ web/.storybook/tools/generate-stories.sh
@@ -11,7 +11,7 @@ node generate-document-stories.mjs
 
 # Project image assets
 
-node generate-image-story.mjs ../../public/img/ Images "owncast/Frontend Assets/Images" "/img" >../stories-category-doc-pages/Images.stories.mdx
-node generate-image-story.mjs ../../public/img/platformlogos/ "Social Platform Images" "owncast/Frontend Assets/Social Platform Images" "/img/platformlogos" >../stories-category-doc-pages/SocialPlatformImages.stories.mdx
-node generate-image-story.mjs ../story-assets/project/ "Logos & Graphics" "owncast/Project Assets/Logos & Graphics" "/project" >../stories-category-doc-pages/LogosAndGraphics.stories.mdx
-node generate-image-story.mjs ../story-assets/tshirt/ "T-shirt" "owncast/Project Assets/T-Shirt" "/tshirt" >../stories-category-doc-pages/Tshirt.stories.mdx
+node generate-image-story.mjs ../../public/img/ Images "owncast/Frontend Assets/Images" "img" >../stories-category-doc-pages/Images.stories.mdx
+node generate-image-story.mjs ../../public/img/platformlogos/ "Social Platform Images" "owncast/Frontend Assets/Social Platform Images" "img/platformlogos" >../stories-category-doc-pages/SocialPlatformImages.stories.mdx
+node generate-image-story.mjs ../story-assets/project/ "Logos & Graphics" "owncast/Project Assets/Logos & Graphics" "project" >../stories-category-doc-pages/LogosAndGraphics.stories.mdx
+node generate-image-story.mjs ../story-assets/tshirt/ "T-shirt" "owncast/Project Assets/T-Shirt" "tshirt" >../stories-category-doc-pages/Tshirt.stories.mdx
