28683bbf5cf34dc1ad7219550c5560ec84951af0
Gabe Kangas <gabek@real-ity.com>
2023-02-10 15:57:21 -0800

    Remove htmlcomment as it gets stripped in build time

--- web/components/common/HtmlComment/HtmlComment.tsx
+++
@@ -1,4 +0,0 @@
-/* eslint-disable react/no-danger */
-export const HtmlComment = ({ text }) => (
-  <span style={{ display: 'none' }} dangerouslySetInnerHTML={{ __html: `\n\n<!-- ${text} -->` }} />
-);
