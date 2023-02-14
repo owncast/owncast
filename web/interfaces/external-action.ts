a290770ac94f80cfba8a275ee211fd80ca62deee
Philipp <xarantolus@protonmail.com>
2023-02-14 18:08:54 +0100

    Allow embedding HTML for external actions (#2693)
    * Admin UI: implement HTML embeds
    
    * Admin UI External Actions: set correct useHTML on edits
    
    * Admin UI: edit by index, not URL
    
    * External Actions: render HTML on stream frontend
    
    * Don't open embeds externally
    
    * Remove TODO comment
    
    * Add HTML as unique action key
    
    * Admin UI: Actions: use CodeMirror editor, dropdown

--- web/interfaces/external-action.ts
+++ web/interfaces/external-action.ts
@@ -2,7 +2,8 @@ export interface ExternalAction {
   title: string;
   description?: string;
   color?: string;
-  url: string;
+  url?: string;
+  html?: string;
   icon?: string;
   openExternally?: boolean;
 }
