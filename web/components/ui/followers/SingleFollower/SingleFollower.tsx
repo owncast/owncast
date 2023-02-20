714c8c9559c0c1262dd6298ea472e1b0f7817712
Yasir761 <74600745+Yasir761@users.noreply.github.com>
2023-02-20 14:55:13 +0530

    Followed some checks description to improve code

--- web/components/ui/followers/SingleFollower/SingleFollower.tsx
+++ web/components/ui/followers/SingleFollower/SingleFollower.tsx
@@ -19,7 +19,6 @@ export const SingleFollower: FC<SingleFollowerProps> = ({ follower }) => (
         </Col>
         <Col span={18}>
           <Row className={styles.name}>
-        
             <Typography.Text ellipsis>{follower.name || follower.username}</Typography.Text>
           </Row>
           <Row className={styles.account}>
