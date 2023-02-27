61b207eb48752f1393bd83d527e44088b3a60fb6
gabek <gabek@users.noreply.github.com>
2023-02-27 00:55:16 +0000

    Prettified Code!

--- web/services/status-service.mock.ts
+++ web/services/status-service.mock.ts
@@ -1,9 +1,7 @@
 import { ServerStatus } from '../interfaces/server-status.model';
 import { ServerStatusStaticService } from './status-service';
 
-export const serverStatusServiceMockOf = (
-  serverStatus: ServerStatus,
-): ServerStatusStaticService =>
+export const serverStatusServiceMockOf = (serverStatus: ServerStatus): ServerStatusStaticService =>
   class ServerStatusServiceMock {
     public static async getStatus(): Promise<ServerStatus> {
       return serverStatus;
