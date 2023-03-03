4ea64be72e540ae65b6b5eafb5c7ab5c1f3b3d17
Gabe Kangas <gabek@real-ity.com>
2023-03-03 14:40:03 -0800

    Revert "Add the Client-side Input Validators for Stream Keys and the Admin Password (#2619)"
    This reverts commit d1566d1d568f7d5fb1652cecb23c4bd6db2bb967.

--- web/components/admin/TextField.tsx
+++ web/components/admin/TextField.tsx
@@ -1,11 +1,10 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC } from 'react';
 import classNames from 'classnames';
-import { Input, Form, InputNumber, Button } from 'antd';
+import { Input, InputNumber } from 'antd';
 import { FieldUpdaterFunc } from '../../types/config-section';
 // import InfoTip from '../info-tip';
 import { StatusState } from '../../utils/input-statuses';
 import { FormStatusIndicator } from './FormStatusIndicator';
-import { PASSWORD_COMPLEXITY_RULES, REGEX_PASSWORD } from '../../utils/config-constants';
 
 export const TEXTFIELD_TYPE_TEXT = 'default';
 export const TEXTFIELD_TYPE_PASSWORD = 'password'; // Input.Password
@@ -18,7 +17,7 @@ export type TextFieldProps = {
 
   onSubmit?: () => void;
   onPressEnter?: () => void;
-  onHandleSubmit?: () => void;
+
   className?: string;
   disabled?: boolean;
   label?: string;
@@ -32,7 +31,6 @@ export type TextFieldProps = {
   useTrim?: boolean;
   useTrimLead?: boolean;
   value?: string | number;
-  hasComplexityRequirements?: boolean;
   onBlur?: FieldUpdaterFunc;
   onChange?: FieldUpdaterFunc;
 };
@@ -46,7 +44,6 @@ export const TextField: FC<TextFieldProps> = ({
   onBlur,
   onChange,
   onPressEnter,
-  onHandleSubmit,
   pattern,
   placeholder,
   required,
@@ -55,29 +52,14 @@ export const TextField: FC<TextFieldProps> = ({
   type,
   useTrim,
   value,
-  hasComplexityRequirements,
 }) => {
-  const [hasPwdChanged, setHasPwdChanged] = useState(false);
-  const [showPwdButton, setShowPwdButton] = useState(false);
-  const [form] = Form.useForm();
   const handleChange = (e: any) => {
     // if an extra onChange handler was sent in as a prop, let's run that too.
     if (onChange) {
       const val = type === TEXTFIELD_TYPE_NUMBER ? e : e.target.value;
-      setShowPwdButton(true);
-      if (hasComplexityRequirements && REGEX_PASSWORD.test(val)) {
-        setHasPwdChanged(true);
-      } else {
-        setHasPwdChanged(false);
-      }
-
       onChange({ fieldName, value: useTrim ? val.trim() : val });
     }
   };
-
-  useEffect(() => {
-    form.setFieldsValue({ inputFieldPassword: value });
-  }, [value]);
 
   // if you blur a required field with an empty value, restore its original value in state (parent's state), if an onChange from parent is available.
   const handleBlur = (e: any) => {
@@ -92,8 +74,7 @@ export const TextField: FC<TextFieldProps> = ({
       onPressEnter();
     }
   };
-  // Password Complexity rules
-  const passwordComplexityRules = [];
+
   // display the appropriate Ant text field
   let Field = Input as
     | typeof Input
@@ -107,9 +88,6 @@ export const TextField: FC<TextFieldProps> = ({
       autoSize: true,
     };
   } else if (type === TEXTFIELD_TYPE_PASSWORD) {
-    PASSWORD_COMPLEXITY_RULES.forEach(element => {
-      passwordComplexityRules.push(element);
-    });
     Field = Input.Password;
     fieldProps = {
       visibilityToggle: true,
@@ -150,66 +128,25 @@ export const TextField: FC<TextFieldProps> = ({
         </div>
       ) : null}
 
-      {!hasComplexityRequirements ? (
-        <div className="input-side">
-          <div className="input-group">
-            <Field
-              id={fieldId}
-              className={`field ${className} ${fieldId}`}
-              {...fieldProps}
-              {...(type !== TEXTFIELD_TYPE_NUMBER && { allowClear: true })}
-              placeholder={placeholder}
-              maxLength={maxLength}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              onPressEnter={handlePressEnter}
-              disabled={disabled}
-              value={value as number | (readonly string[] & number)}
-            />
-          </div>
-          <FormStatusIndicator status={status} />
-          <p className="field-tip">{tip}</p>
+      <div className="input-side">
+        <div className="input-group">
+          <Field
+            id={fieldId}
+            className={`field ${className} ${fieldId}`}
+            {...fieldProps}
+            {...(type !== TEXTFIELD_TYPE_NUMBER && { allowClear: true })}
+            placeholder={placeholder}
+            maxLength={maxLength}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            onPressEnter={handlePressEnter}
+            disabled={disabled}
+            value={value as number | (readonly string[] & number)}
+          />
         </div>
-      ) : (
-        <div className="input-side">
-          <div className="input-group">
-            <Form
-              name="basic"
-              form={form}
-              initialValues={{ inputFieldPassword: value }}
-              style={{ width: '100%' }}
-            >
-              <Form.Item name="inputFieldPassword" rules={passwordComplexityRules}>
-                <Input.Password
-                  id={fieldId}
-                  className={`field ${className} ${fieldId}`}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  placeholder={placeholder}
-                  onPressEnter={handlePressEnter}
-                  disabled={disabled}
-                  value={value as number | (readonly string[] & number)}
-                />
-              </Form.Item>
-              {showPwdButton && (
-                <div style={{ display: 'flex', flexDirection: 'row-reverse' }}>
-                  <Button
-                    type="primary"
-                    size="small"
-                    className="submit-button"
-                    onClick={onHandleSubmit}
-                    disabled={!hasPwdChanged}
-                  >
-                    Update
-                  </Button>
-                </div>
-              )}
-              <FormStatusIndicator status={status} />
-              <p className="field-tip">{tip}</p>
-            </Form>
-          </div>
-        </div>
-      )}
+        <FormStatusIndicator status={status} />
+        <p className="field-tip">{tip}</p>
+      </div>
     </div>
   );
 };
@@ -231,11 +168,9 @@ TextField.defaultProps = {
   pattern: '',
   useTrim: false,
   useTrimLead: false,
-  hasComplexityRequirements: false,
 
   onSubmit: () => {},
   onBlur: () => {},
   onChange: () => {},
   onPressEnter: () => {},
-  onHandleSubmit: () => {},
 };
