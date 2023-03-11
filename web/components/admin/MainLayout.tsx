84a0c2f16953b4f71615a7c62f3066bcbefed02d
Nathan <34570423+thisprojects@users.noreply.github.com>
2023-03-11 20:16:35 +0000

    fix: #1904 CSS Improvements on Admin Page (#2790)
    * fix: #1904 CSS Improvements on Admin Page
    
    * Prettified Code!
    
    ---------
    
    Co-authored-by: thisProjects <wibbet@wobbet.com>
    Co-authored-by: thisprojects <thisprojects@users.noreply.github.com>

--- web/components/admin/MainLayout.tsx
+++ web/components/admin/MainLayout.tsx
@@ -277,7 +277,7 @@ export const MainLayout: FC<MainLayoutProps> = ({ children }) => {
     },
   ];
   return (
-    <Layout className={appClass}>
+    <Layout id="admin-page" className={appClass}>
       <Head>
         <title>Owncast Admin</title>
         <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon/favicon-32x32.png" />
