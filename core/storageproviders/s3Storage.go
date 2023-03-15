b350ee6518453a6f6adfb4c0a50462215c5cc683
Jacob Wrenn <jacob@wrenn.me>
2023-03-15 15:01:40 +0000

    clean up code

--- core/storageproviders/s3Storage.go
+++ core/storageproviders/s3Storage.go
@@ -151,26 +151,19 @@ func (s *S3Storage) Save(filePath string, retryCount int) (string, error) {
 	maxAgeSeconds := utils.GetCacheDurationSecondsForPath(filePath)
 	cacheControlHeader := fmt.Sprintf("max-age=%d", maxAgeSeconds)
 
-	var uploadInput *s3manager.UploadInput
+	uploadInput := &s3manager.UploadInput{
+		Bucket:       aws.String(s.s3Bucket), // Bucket to be used
+		Key:          aws.String(remotePath), // Name of the file to be saved
+		Body:         file,                   // File
+		CacheControl: &cacheControlHeader,
+	}
 
 	if path.Ext(filePath) == ".m3u8" {
 		noCacheHeader := "no-cache, no-store, must-revalidate"
 		contentType := "application/x-mpegURL"
 
-		uploadInput = &s3manager.UploadInput{
-			Bucket:       aws.String(s.s3Bucket), // Bucket to be used
-			Key:          aws.String(remotePath), // Name of the file to be saved
-			Body:         file,                   // File
-			CacheControl: &noCacheHeader,
-			ContentType:  &contentType,
-		}
-	} else {
-		uploadInput = &s3manager.UploadInput{
-			Bucket:       aws.String(s.s3Bucket), // Bucket to be used
-			Key:          aws.String(remotePath), // Name of the file to be saved
-			Body:         file,                   // File
-			CacheControl: &cacheControlHeader,
-		}
+		uploadInput.CacheControl = &noCacheHeader
+		uploadInput.ContentType = &contentType
 	}
 
 	if s.s3ACL != "" {
