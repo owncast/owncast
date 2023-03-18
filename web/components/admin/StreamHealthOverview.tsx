f292af59d54083a87595e4e2ca452c4e6feec12e
dhanu <andreasdhanu@gmail.com>
2023-03-18 13:23:51 +0700

    Loose div in Utilities > Stream health screen

--- web/components/admin/StreamHealthOverview.tsx
+++ web/components/admin/StreamHealthOverview.tsx
@@ -1,4 +1,4 @@
-import { Alert, Button, Col, Row, Statistic, Typography } from 'antd';
+import { Alert, Button, Card, Col, Row, Statistic, Typography } from 'antd';
 import dynamic from 'next/dynamic';
 import Link from 'next/link';
 import React, { FC, useContext } from 'react';
@@ -41,54 +41,56 @@ export const StreamHealthOverview: FC<StreamHealthOverviewProps> = ({ showTroubl
 
   return (
     <div>
-      <Row gutter={8}>
-        <Col span={12}>
-          <Statistic
-            title="Healthy Stream"
-            value={healthy ? 'Yes' : 'No'}
-            valueStyle={{ color }}
-            prefix={healthy ? <CheckCircleOutlined /> : <ExclamationCircleOutlined />}
-          />
-        </Col>
-        <Col span={12}>
-          <Statistic
-            title="Playback Health"
-            value={healthPercentage}
-            valueStyle={{ color }}
-            suffix="%"
-          />
-        </Col>
-      </Row>
-      <Row style={{ display: representation < 100 && representation !== 0 ? 'grid' : 'none' }}>
-        <Typography.Text
-          type="secondary"
-          style={{ textAlign: 'center', fontSize: '0.7em', opacity: '0.3' }}
+      <Card type="inner">
+        <Row gutter={8}>
+          <Col span={12}>
+            <Statistic
+              title="Healthy Stream"
+              value={healthy ? 'Yes' : 'No'}
+              valueStyle={{ color }}
+              prefix={healthy ? <CheckCircleOutlined /> : <ExclamationCircleOutlined />}
+            />
+          </Col>
+          <Col span={12}>
+            <Statistic
+              title="Playback Health"
+              value={healthPercentage}
+              valueStyle={{ color }}
+              suffix="%"
+            />
+          </Col>
+        </Row>
+        <Row style={{ display: representation < 100 && representation !== 0 ? 'grid' : 'none' }}>
+          <Typography.Text
+            type="secondary"
+            style={{ textAlign: 'center', fontSize: '0.7em', opacity: '0.3' }}
+          >
+            Stream health represents {representation}% of all known players. Other player status is
+            unknown.
+          </Typography.Text>
+        </Row>
+        <Row
+          gutter={16}
+          style={{ width: '100%', display: message ? 'grid' : 'none', marginTop: '10px' }}
         >
-          Stream health represents {representation}% of all known players. Other player status is
-          unknown.
-        </Typography.Text>
-      </Row>
-      <Row
-        gutter={16}
-        style={{ width: '100%', display: message ? 'grid' : 'none', marginTop: '10px' }}
-      >
-        <Col span={24}>
-          <Alert
-            message={message}
-            type={icon}
-            showIcon
-            action={
-              showTroubleshootButton && (
-                <Link passHref href="/stream-health">
-                  <Button size="small" type="text" style={{ color: 'black' }}>
-                    TROUBLESHOOT
-                  </Button>
-                </Link>
-              )
-            }
-          />
-        </Col>
-      </Row>
+          <Col span={24}>
+            <Alert
+              message={message}
+              type={icon}
+              showIcon
+              action={
+                showTroubleshootButton && (
+                  <Link passHref href="/stream-health">
+                    <Button size="small" type="text" style={{ color: 'black' }}>
+                      TROUBLESHOOT
+                    </Button>
+                  </Link>
+                )
+              }
+            />
+          </Col>
+        </Row>
+      </Card>
     </div>
   );
 };
