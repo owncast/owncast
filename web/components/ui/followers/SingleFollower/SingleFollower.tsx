f74245bd27da49e6aaea0d595374949537555d61
Gabe Kangas <gabek@real-ity.com>
2023-03-22 14:38:29 -0700

    Rework loading followers and pagination a bit. Closes #2835

--- web/components/ui/followers/SingleFollower/SingleFollower.tsx
+++ web/components/ui/followers/SingleFollower/SingleFollower.tsx
@@ -12,7 +12,7 @@ export const SingleFollower: FC<SingleFollowerProps> = ({ follower }) => (
   <div className={cn([styles.follower, 'followers-follower'])}>
     <a href={follower.link} target="_blank" rel="noreferrer">
       <Row wrap={false}>
-        <Col span={6}>
+        <Col span={4}>
           <Avatar
             src={follower.image}
             alt="Avatar"
