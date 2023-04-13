fcbb3023d0bd36c77219d36f5a100a355d203fd8
bennett1412 <bennett1412@users.noreply.github.com>
2023-04-13 10:24:23 +0000

    Prettified Code!

--- web/components/admin/Chart.tsx
+++ web/components/admin/Chart.tsx
@@ -1,5 +1,5 @@
 import format from 'date-fns/format';
-import { FC,  useRef } from 'react';
+import { FC, useRef } from 'react';
 
 import { DownloadOutlined } from '@ant-design/icons';
 import {
@@ -15,7 +15,6 @@ import {
 } from 'chart.js';
 import { Line } from 'react-chartjs-2';
 import { Button } from 'antd';
-
 
 ChartJS.register(
   CategoryScale,
@@ -68,13 +67,13 @@ export const Chart: FC<ChartProps> = ({
 
   const chartRef = useRef();
   const downloadChart = () => {
-    if(chartRef.current){
+    if (chartRef.current) {
       let link = document.createElement('a');
       link.download = 'chart.png';
       link.href = chartRef.current.canvas.toDataURL();
       link.click();
     }
-  }
+  };
 
   if (data && data.length > 0) {
     renderData.push({
@@ -118,7 +117,12 @@ export const Chart: FC<ChartProps> = ({
   return (
     <div className="line-chart-container">
       <Line ref={chartRef} data={{ datasets: renderData }} options={options} height="70vh" />
-      <Button onClick={downloadChart} type="default" icon={<DownloadOutlined />} className="download-btn" />
+      <Button
+        onClick={downloadChart}
+        type="default"
+        icon={<DownloadOutlined />}
+        className="download-btn"
+      />
     </div>
   );
 };
