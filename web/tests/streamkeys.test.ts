4cc0062acdb9cedd27e0446ab9ce36efc97cf2b9
Dev Gupta <73140257+dev265545@users.noreply.github.com>
2023-04-18 04:03:30 +0530

    adding tests for generateRndKey (#2932)
    changing jest.config.js
    transform option  from -
    transform: { '^.+\\.ts?$': 'ts-jest' },
    to
      transform: {   '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }], },

---
+++ web/tests/streamkeys.test.ts
@@ -0,0 +1,26 @@
+import { generateRndKey } from '../components/admin/config/server/StreamKeys';
+describe('generateRndKey', () => {
+  test('should generate a key that matches the regular expression', () => {
+    const key = generateRndKey();
+    const regex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#$^&*]).{8,192}$/;
+    expect(regex.test(key)).toBe(true);
+  });
+
+  test('returns a string', () => {
+    const result = generateRndKey();
+    expect(typeof result).toBe('string');
+  });
+
+
+  test('should generate a key of length between 8 and 192 characters', () => {
+    const key = generateRndKey();
+    expect(key.length).toBeGreaterThanOrEqual(8);
+    expect(key.length).toBeLessThanOrEqual(192);
+  });
+
+  test('should generate a unique key on each invocation', () => {
+    const key1 = generateRndKey();
+    const key2 = generateRndKey();
+    expect(key1).not.toBe(key2);
+  });
+});
