f608b5622e41f7275353b95a771c8c0b16437c0e
Gabe Kangas <gabek@real-ity.com>
2023-04-28 13:57:37 -0700

    fix(mobile): request to disable mobile web auto scaling. Closes #2970

--- web/components/layouts/Main/Main.tsx
+++ web/components/layouts/Main/Main.tsx
@@ -91,6 +91,12 @@ export const Main: FC = () => {
         <meta name="msapplication-TileColor" content="#ffffff" />
         <meta name="msapplication-TileImage" content="/img/favicon/ms-icon-144x144.png" />
         <meta name="theme-color" content="#ffffff" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+
         <base target="_blank" />
       </Head>
 
