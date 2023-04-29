b225cd9dc72b337c25bbf506eb4c14739191357b
Gabe Kangas <gabek@real-ity.com>
2023-04-28 20:47:25 -0700

    feat(footer): use simpler version string

--- web/components/ui/Footer/Footer.tsx
+++ web/components/ui/Footer/Footer.tsx
@@ -8,7 +8,7 @@ export type FooterProps = {
 export const Footer: FC<FooterProps> = ({ version }) => (
   <footer className={styles.footer} id="footer">
     <span>
-      Powered by <a href="https://owncast.online">{version}</a>
+      Powered by <a href="https://owncast.online">Owncast v{version}</a>
     </span>
     <span className={styles.links}>
       <a href="https://owncast.online/docs" target="_blank" rel="noreferrer">
