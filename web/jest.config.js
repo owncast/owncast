405476c6b0440d702e60ab0a3bb78b4288297e2d
Dev Gupta <guptadev265@gmail.com>
2023-05-01 04:32:42 +0530

    Update jest.config.js

--- web/jest.config.js
+++ web/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  transform: {'^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel']}]},
+  transform: { '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }] },
   testEnvironment: 'node',
   testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
