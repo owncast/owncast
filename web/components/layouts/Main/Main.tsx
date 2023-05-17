1a312cba26032258bfbaf1c95dda4c7b89691c2b
gingervitis <gingervitis@users.noreply.github.com>
2023-05-17 10:59:22 +0000

    Prettified Code!

--- web/components/layouts/Main/Main.tsx
+++ web/components/layouts/Main/Main.tsx
@@ -64,7 +64,7 @@ export const Main: FC = () => {
   const showChat = online && !chatDisabled && isChatVisible;
   const dynamicFooterPadding = showChat && !isMobile ? DYNAMIC_PADDING_VALUE : '';
 
-	console.log({showChat, isChatVisible,appState})
+  console.log({ showChat, isChatVisible, appState });
 
   useEffect(() => {
     setupNoLinkReferrer(layoutRef.current);
