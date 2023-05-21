447ab10738fe09ac77410ab36a84e8312143ae09
Gabe Kangas <gabek@real-ity.com>
2023-05-21 14:12:14 -0700

    feat: add ios specific push notification instructions
    Closes #2992

--- web/utils/browserPushNotifications.ts
+++ web/utils/browserPushNotifications.ts
@@ -1,3 +1,15 @@
-export default function isPushNotificationSupported() {
-  return 'serviceWorker' in navigator && 'PushManager' in window;
+import { isMobileSafariIos } from './helpers';
+
+export function arePushNotificationSupported() {
+  return 'Notification' in window && 'serviceWorker' in navigator && 'PushManager' in window;
+}
+
+export function canPushNotificationsBeSupported() {
+  // Mobile safari will return false for supporting push notifications, but
+  // it does support them. So we need to check for mobile safari and return
+  // true if it is.
+  if (isMobileSafariIos()) {
+    return true;
+  }
+  return 'Notification' in window && 'serviceWorker' in navigator && 'PushManager' in window;
 }
