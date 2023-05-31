8e6521537dcf5911920878cdd1e7584a68545a63
Jerin Thomas <jerinthomas1404@gmail.com>
2023-06-01 00:06:41 +0530

    Extending webfinger response (issue:2851) (#3053)
    * Extending webfinger response #2851
    1. Added Logo
     - rel: avatar as there wasn't any appropriate value in [Link relations registry](https://www.iana.org/assignments/link-relations/)
     - type: default value image/png or else it is determined file type extension
    2. Added Stream
     - rel: stream as there wasn't appropriate value in [Link relations registry](https://www.iana.org/assignments/link-relations/)
     - type: video/H264 based on [IANA media types](https://www.iana.org/assignments/media-types/media-types.xhtml#video)
    
    Changes after review:
    
    1. Updated the rel type for avatar based on webfinger rel.
    2. Updated the rel type for stream link and href value that closely associates to it.
    
    * adding period after comments
    
    * updating typo

--- activitypub/apmodels/utils.go
+++ activitypub/apmodels/utils.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"net/url"
 	"path"
+	"path/filepath"
 
 	"github.com/go-fed/activity/streams"
 	"github.com/go-fed/activity/streams/vocab"
@@ -59,4 +60,51 @@ func Serialize(obj vocab.Type) ([]byte, error) {
 	b, err := json.Marshal(jsonmap)
 
 	return b, err
+}
+
+// MakeLocalIRIForStreamURL will return a full IRI for the local server stream url.
+func MakeLocalIRIForStreamURL() *url.URL {
+	host := data.GetServerURL()
+	u, err := url.Parse(host)
+	if err != nil {
+		log.Errorln("unable to parse local IRI stream url", err)
+		return nil
+	}
+
+	u.Path = path.Join(u.Path, "/hls/stream.m3u8")
+
+	return u
+}
+
+// MakeLocalIRIforLogo will return a full IRI for the local server logo.
+func MakeLocalIRIforLogo() *url.URL {
+	host := data.GetServerURL()
+	u, err := url.Parse(host)
+	if err != nil {
+		log.Errorln("unable to parse local IRI stream url", err)
+		return nil
+	}
+
+	u.Path = path.Join(u.Path, "/logo/external")
+
+	return u
+}
+
+// GetLogoType will return the rel value for the webfinger response and
+// the default static image is of type png.
+func GetLogoType() string {
+	imageFilename := data.GetLogoPath()
+	if imageFilename == "" {
+		return "image/png"
+	}
+
+	logoType := "image/jpeg"
+	if filepath.Ext(imageFilename) == ".svg" {
+		logoType = "image/svg+xml"
+	} else if filepath.Ext(imageFilename) == ".gif" {
+		logoType = "image/gif"
+	} else if filepath.Ext(imageFilename) == ".png" {
+		logoType = "image/png"
+	}
+	return logoType
 }
