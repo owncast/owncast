6212cd5bafed7ce682a091c40e42739e9c8a0ac7
yk-jp <k82.y12@gmail.com>
2023-06-05 17:05:10 -0700

    Revert web/components/action-buttons/NotifyButton.tsx

--- web/components/action-buttons/NotifyButton.tsx
+++ web/components/action-buttons/NotifyButton.tsx
@@ -15,15 +15,13 @@ export type NotifyButtonProps = {
 };
 
 export const NotifyButton: FC<NotifyButtonProps> = ({ onClick, text }) => (
-  <div>
-    <Button
-      type="primary"
-      className={`${styles.button}`}
-      icon={<BellFilled />}
-      onClick={onClick}
-      id="notify-button"
-    >
-      {text || 'Notify'}
-    </Button>
-  </div>
+  <Button
+    type="primary"
+    className={`${styles.button}`}
+    icon={<BellFilled />}
+    onClick={onClick}
+    id="notify-button"
+  >
+    {text || 'Notify'}
+  </Button>
 );
