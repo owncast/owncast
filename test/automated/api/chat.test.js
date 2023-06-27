6428be90a37d399a26ca3673714eaece930c4a79
Gabe Kangas <gabek@real-ity.com>
2023-06-27 15:19:41 -0700

    fix(test): fix test to reflect chat message updates

--- test/automated/api/chat.test.js
+++ test/automated/api/chat.test.js
@@ -11,52 +11,51 @@ var userDisplayName;
 const message = randomNumber(100) + ' test 123';
 
 const testMessage = {
-  body: message,
-  type: 'CHAT',
+	body: message,
+	type: 'CHAT',
 };
 
 test('send a chat message', async (done) => {
-  const registration = await registerChat();
-  const accessToken = registration.accessToken;
-  userDisplayName = registration.displayName;
+	const registration = await registerChat();
+	const accessToken = registration.accessToken;
+	userDisplayName = registration.displayName;
 
-  sendChatMessage(testMessage, accessToken, done);
+	sendChatMessage(testMessage, accessToken, done);
 });
 
 test('fetch chat messages by admin', async (done) => {
-  const res = await getAdminResponse('chat/messages');
+	const res = await getAdminResponse('chat/messages');
+	const expectedBody = `<p>` + testMessage.body + `</p>`;
 
-  const message = res.body.filter((m) => m.body === testMessage.body)[0];
-  if (!message) {
-    throw new Error('Message not found');
-  }
+	const message = res.body.filter((m) => m.body === expectedBody)[0];
+	if (!message) {
+		throw new Error('Message not found');
+	}
 
-  const expectedBody = testMessage.body;
+	expect(message.body).toBe(expectedBody);
+	expect(message.user.displayName).toBe(userDisplayName);
+	expect(message.type).toBe(testMessage.type);
 
-  expect(message.body).toBe(expectedBody);
-  expect(message.user.displayName).toBe(userDisplayName);
-  expect(message.type).toBe(testMessage.type);
-
-  done();
+	done();
 });
 
 test('derive display name from user header', async (done) => {
-  const res = await request
-    .post('/api/chat/register')
-    .set('X-Forwarded-User', 'test-user')
-    .expect(200);
+	const res = await request
+		.post('/api/chat/register')
+		.set('X-Forwarded-User', 'test-user')
+		.expect(200);
 
-  expect(res.body.displayName).toBe('test-user');
-  done();
+	expect(res.body.displayName).toBe('test-user');
+	done();
 });
 
 test('overwrite user header derived display name with body', async (done) => {
-  const res = await request
-    .post('/api/chat/register')
-    .send({ displayName: 'TestUserChat' })
-    .set('X-Forwarded-User', 'test-user')
-    .expect(200);
+	const res = await request
+		.post('/api/chat/register')
+		.send({ displayName: 'TestUserChat' })
+		.set('X-Forwarded-User', 'test-user')
+		.expect(200);
 
-  expect(res.body.displayName).toBe('TestUserChat');
-  done();
+	expect(res.body.displayName).toBe('TestUserChat');
+	done();
 });
