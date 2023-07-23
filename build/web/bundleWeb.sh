45213549adc0495cc133bb05ee32543a45a85b66
Gabe Kangas <gabek@real-ity.com>
2023-07-22 21:15:20 -0700

    Revert "add offline option to bundleWeb.sh (#3193)"
    This reverts commit b3fe7c63b89c9e263eaf4a4453e9ecc4f4e81426.

--- build/web/bundleWeb.sh
+++ build/web/bundleWeb.sh
@@ -5,26 +5,13 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-OFFLINE=0
-while [[ $# -gt 0 ]]; do
-  case $1 in
-    --offline)
-      OFFLINE=1
-      ;;
-  esac
-  shift
-done
-
 # Change to the root directory of the repository
 cd "$(git rev-parse --show-toplevel)"
 
 cd web
 
-if [ ! "$OFFLINE" ]
-then
-  echo "Installing npm modules for the owncast web..."
-  npm --silent install 2>/dev/null
-fi
+echo "Installing npm modules for the owncast web..."
+npm --silent install 2>/dev/null
 
 echo "Building owncast web..."
 rm -rf .next
