d947c4b4a4c8b4a9edf6ce95aea1476c34b6fadb
Gabe Kangas <gabek@real-ity.com>
2023-08-31 22:19:37 -0700

    WIP replay integration tests

---
+++ test/automated/replays/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+source ../tools.sh
+
+# Install the node test framework
+npm install --silent >/dev/null
+
+install_ffmpeg
+
+start_owncast "--enableReplayFeatures"
+
+start_stream
+
+sleep 10
+
+# Run tests against a fresh install with no settings.
+npm test
