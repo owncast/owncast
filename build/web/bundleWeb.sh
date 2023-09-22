44401f7e7c59add3aa06ce7058f4c5bd17daa874
janWilejan <119548498+janWilejan@users.noreply.github.com>
2023-09-22 02:07:40 +0000

    Add offline option to bundle web.sh (#3202)
    * add offline option to bundleWeb.sh
    
    * fix offline flag for bundleWeb.sh
    
    ---------
    
    Co-authored-by: janWilejan <>

--- build/web/bundleWeb.sh
+++ build/web/bundleWeb.sh
@@ -5,13 +5,26 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+OFFLINE=
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --offline)
+      OFFLINE=1
+      ;;
+  esac
+  shift
+done
+
 # Change to the root directory of the repository
 cd "$(git rev-parse --show-toplevel)"
 
 cd web
 
-echo "Installing npm modules for the owncast web..."
-npm --silent install 2>/dev/null
+if [ ! "$OFFLINE" ]
+then
+  echo "Installing npm modules for the owncast web..."
+  npm --silent install 2>/dev/null
+fi
 
 echo "Building owncast web..."
 rm -rf .next
