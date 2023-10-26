ab8c2279e3d16898da9edd44063b0c0f9e006ca6
Gabe Kangas <gabek@real-ity.com>
2023-10-25 21:39:19 -0700

    fix(storybook): update stories to support mdx2

--- web/.storybook/main.js
+++ web/.storybook/main.js
@@ -110,20 +110,6 @@ module.exports = {
       use: ['@svgr/webpack'],
     });
 
-    // config.module.rules.push({
-    //   test: /\.less$/,
-    //   use: [
-    //     require.resolve('style-loader'),
-    //     require.resolve('css-loader'),
-    //     {
-    //       loader: require.resolve('less-loader'),
-    //       options: {
-    //         lessOptions: { javascriptEnabled: true },
-    //       },
-    //     },
-    //   ],
-    // });
-
     return config;
   },
 
@@ -135,6 +121,6 @@ module.exports = {
   staticDirs: ['../public', '../../static', './story-assets'],
 
   docs: {
-    autodocs: true,
+    autodocs: false,
   },
 };
