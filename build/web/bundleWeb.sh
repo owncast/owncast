5089f0dfb2f2169bd7a109e0b80fcd6157d57e90
Gabe Kangas <gabek@real-ity.com>
2023-11-08 20:56:02 -0800

    chore: update build scripts to support next 14. Closes #3413

--- build/web/bundleWeb.sh
+++ build/web/bundleWeb.sh
@@ -7,12 +7,12 @@ set -o pipefail
 
 OFFLINE=
 while [[ $# -gt 0 ]]; do
-  case $1 in
-    --offline)
-      OFFLINE=1
-      ;;
-  esac
-  shift
+	case $1 in
+	--offline)
+		OFFLINE=1
+		;;
+	esac
+	shift
 done
 
 # Change to the root directory of the repository
@@ -20,15 +20,14 @@ cd "$(git rev-parse --show-toplevel)"
 
 cd web
 
-if [ ! "$OFFLINE" ]
-then
-  echo "Installing npm modules for the owncast web..."
-  npm --silent install 2>/dev/null
+if [ ! "$OFFLINE" ]; then
+	echo "Installing npm modules for the owncast web..."
+	npm --silent install 2>/dev/null
 fi
 
 echo "Building owncast web..."
 rm -rf .next
-(node_modules/.bin/next build && node_modules/.bin/next export) | grep info
+node_modules/.bin/next build | grep info
 
 echo "Copying web project to dist directory..."
 
