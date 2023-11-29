444ddf65a863ea4dd36ee1624260d9aa2c432852
Gabe Kangas <gabek@real-ity.com>
2023-11-28 20:15:42 -0800

    Update configs

--- web/i18next-parser.config.mjs
+++ web/i18next-parser.config.mjs
@@ -4,7 +4,7 @@ export default {
   contextSeparator: '_',
   // Key separator used in your translation keys
 
-  createOldCatalogs: false,
+  createOldCatalogs: true,
   // Save the \_old files
 
   defaultNamespace: 'translation',
@@ -55,7 +55,7 @@ export default {
   // Namespace separator used in your translation keys
   // If you want to use plain english keys, separators such as `.` and `:` will conflict. You might want to set `keySeparator: false` and `namespaceSeparator: false`. That way, `t('Status: Loading...')` will not think that there are a namespace and three separator dots for instance.
 
-  output: 'i18n/translations.$LOCALE.json',
+  output: 'i18n/locales/$LOCALE/$LOCALE.json',
   // Supports $LOCALE and $NAMESPACE injection
   // Supports JSON (.json) and YAML (.yml) file formats
   // Where to write the locale files relative to process.cwd()
@@ -98,7 +98,7 @@ export default {
   // for that key across locales are reset to the default value, and existing translations are moved to
   // the `_old` file.
 
-  i18nextOptions: { returnDetails: true, lng: '$LOCALE' },
+  // i18nextOptions: { returnDetails: true, lng: '$LOCALE' },
   // If you wish to customize options in internally used i18next instance, you can define an object with any
   // configuration property supported by i18next (https://www.i18next.com/overview/configuration-options).
   // { compatibilityJSON: 'v3' } can be used to generate v3 compatible plurals.
