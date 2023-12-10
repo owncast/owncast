79443a26597e1b547277fca88d0763045b1f0bce
Gabe Kangas <gabek@real-ity.com>
2023-12-09 16:31:50 -0800

    feat(api): add server-side caching for requests that could benefit (#3463)
    * feat(api): add server-side caching for requests that could benefit for them
    
    * fix(tests): do not cache responses while in tests
    
    * fix: remove commented out leftover code
    
    * chore(deps): update dependency html-webpack-plugin to v5.5.4
    
    * Bundle embedded web app
    
    * fix: remove caching for web app assets under test
    
    * chore(tests): re-enable temporarily disabled test
    
    * chore(deps): update dependency typescript to v5.3.3
    
    * Bundle embedded web app
    
    * chore(deps): update dependency npm to v10.2.5
    
    * Bundle embedded web app
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: Owncast <owncast@owncast.online>

--- main.go
+++ main.go
@@ -17,17 +17,18 @@ import (
 )
 
 var (
-	dbFile                = flag.String("database", "", "Path to the database file.")
-	logDirectory          = flag.String("logdir", "", "Directory where logs will be written to")
-	backupDirectory       = flag.String("backupdir", "", "Directory where backups will be written to")
-	enableDebugOptions    = flag.Bool("enableDebugFeatures", false, "Enable additional debugging options.")
-	enableVerboseLogging  = flag.Bool("enableVerboseLogging", false, "Enable additional logging.")
-	restoreDatabaseFile   = flag.String("restoreDatabase", "", "Restore an Owncast database backup")
-	newAdminPassword      = flag.String("adminpassword", "", "Set your admin password")
-	newStreamKey          = flag.String("streamkey", "", "Set a temporary stream key for this session")
-	webServerPortOverride = flag.String("webserverport", "", "Force the web server to listen on a specific port")
-	webServerIPOverride   = flag.String("webserverip", "", "Force web server to listen on this IP address")
-	rtmpPortOverride      = flag.Int("rtmpport", 0, "Set listen port for the RTMP server")
+	dbFile                 = flag.String("database", "", "Path to the database file.")
+	logDirectory           = flag.String("logdir", "", "Directory where logs will be written to")
+	backupDirectory        = flag.String("backupdir", "", "Directory where backups will be written to")
+	enableDebugOptions     = flag.Bool("enableDebugFeatures", false, "Enable additional debugging options.")
+	enableVerboseLogging   = flag.Bool("enableVerboseLogging", false, "Enable additional logging.")
+	restoreDatabaseFile    = flag.String("restoreDatabase", "", "Restore an Owncast database backup")
+	newAdminPassword       = flag.String("adminpassword", "", "Set your admin password")
+	newStreamKey           = flag.String("streamkey", "", "Set a temporary stream key for this session")
+	webServerPortOverride  = flag.String("webserverport", "", "Force the web server to listen on a specific port")
+	webServerIPOverride    = flag.String("webserverip", "", "Force web server to listen on this IP address")
+	rtmpPortOverride       = flag.Int("rtmpport", 0, "Set listen port for the RTMP server")
+	disableResponseCaching = flag.Bool("disableResponseCaching", false, "Do not optimize performance by caching of web responses")
 )
 
 // nolint:cyclop
@@ -40,6 +41,10 @@ func main() {
 
 	if *backupDirectory != "" {
 		config.BackupDirectory = *backupDirectory
+	}
+
+	if *disableResponseCaching {
+		config.DisableResponseCaching = *disableResponseCaching
 	}
 
 	// Create the data directory if needed
