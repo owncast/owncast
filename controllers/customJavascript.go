5de8ed9ebacb23d8a139fa0f7bde83493f32d668
Max <mail@heavygale.de>
2024-03-05 06:40:57 +0100

    set content-type for customjavascript (#3612)
    * set content-type for customjavascript
    
    * change content-type in order to comply with RFC 9239

--- controllers/customJavascript.go
+++ controllers/customJavascript.go
@@ -8,6 +8,8 @@ import (
 
 // ServeCustomJavascript will serve optional custom Javascript.
 func ServeCustomJavascript(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/javascript; charset=utf-8")
+
 	js := data.GetCustomJavascript()
 	_, _ = w.Write([]byte(js))
 }
