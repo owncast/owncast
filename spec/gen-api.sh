f45481a61834a80e2d0c05330e59daec50ce9157
Brenden Ehlers <brenden@theehlers.org>
2024-04-02 10:09:20 -0600

    update gen api script

--- spec/gen-api.sh
+++ spec/gen-api.sh
@@ -1,18 +1,41 @@
 #!/bin/bash
 
+# go install github.com/deepmap/oapi-codegen/v2/cmd/oapi-codegen@latest
+
 # setup
-folder="generated"
 package="generated"
-oapiFile="openapi.yaml"
+folderPath="handler/generated"
+specPath="spec/openapi.yaml"
+
+# validate scripts are installed
+if ! command -v swagger-cli &> /dev/null
+then
+	echo "Please install \`swagger-cli\` before running this script"
+	exit 1
+fi
+
+if ! command -v oapi-codegen &> /dev/null
+then
+	echo "Please install \`oapi-codegen\` before running this script"
+	echo "Hint: run \`go install github.com/deepmap/oapi-codegen/v2/cmd/oapi-codegen@latest\` to install"
+	exit 1
+fi
+
+# validate schema
+swagger-cli validate $specPath
+if [ $? -ne 0 ];
+then
+	echo "Open API specification is not valid"
+	exit 1
+fi
 
 # cleanup
-rm -rf ./handler/$folder
-mkdir -p ./handler/$folder
+rm -r $folderPath
+mkdir -p $folderPath
 
 # codegen
-go install github.com/deepmap/oapi-codegen/v2/cmd/oapi-codegen@latest
-oapi-codegen -generate types -o handler/$folder/$package-types.gen.go -package $package spec/$oapiFile
-oapi-codegen -generate "chi-server" -o handler/$folder/$package.gen.go -package $package spec/$oapiFile
+oapi-codegen -generate types -o $folderPath/$package-types.gen.go -package $package $specPath
+oapi-codegen -generate "chi-server" -o $folderPath/$package.gen.go -package $package $specPath
 
 # go
 go mod tidy
