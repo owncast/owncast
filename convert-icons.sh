98bbc2f3df1897ba98ad8611fd7cf0f5eff94362
Rob Cakebread <cakebread@gmail.com>
2024-05-17 16:23:14 -0700

    Changed background to transparency

--- convert-icons.sh
+++ convert-icons.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 
-
+convert -size  70x70 xc:transparent ./static/web/img/favicon/ms-icon-70x70.png
+convert -size  72x72 xc:transparent ./static/web/img/favicon/android-icon-72x72.png
+convert -size  152x152 xc:transparent ./static/web/img/favicon/apple-icon-152x152.png
+convert -size  120x120 xc:transparent ./static/web/img/favicon/apple-icon-120x120.png
+convert -size  192x192 xc:transparent ./static/web/img/favicon/android-icon-192x192.png
+convert -size  72x72 xc:transparent ./static/web/img/favicon/apple-icon-72x72.png
+convert -size  192x192 xc:transparent ./static/web/img/favicon/apple-icon.png
+convert -size  57x57 xc:transparent ./static/web/img/favicon/apple-icon-57x57.png
+convert -size  150x150 xc:transparent ./static/web/img/favicon/ms-icon-150x150.png
+convert -size  96x96 xc:transparent ./static/web/img/favicon/android-icon-96x96.png
+convert -size  60x60 xc:transparent ./static/web/img/favicon/apple-icon-60x60.png
+convert -size  144x144 xc:transparent ./static/web/img/favicon/apple-icon-144x144.png
+convert -size  144x144 xc:transparent ./static/web/img/favicon/android-icon-144x144.png
+convert -size  192x192 xc:transparent ./static/web/img/favicon/apple-icon-precomposed.png
+convert -size  16x16 xc:transparent ./static/web/img/favicon/favicon-16x16.png
+convert -size  310x310 xc:transparent ./static/web/img/favicon/ms-icon-310x310.png
+convert -size  96x96 xc:transparent ./static/web/img/favicon/favicon-96x96.png
+convert -size  76x76 xc:transparent ./static/web/img/favicon/apple-icon-76x76.png
+convert -size  180x180 xc:transparent ./static/web/img/favicon/apple-icon-180x180.png
+convert -size  144x144 xc:transparent ./static/web/img/favicon/ms-icon-144x144.png
+convert -size  114x114 xc:transparent ./static/web/img/favicon/apple-icon-114x114.png
+convert -size  48x48 xc:transparent ./static/web/img/favicon/android-icon-48x48.png
+convert -size  36x36 xc:transparent ./static/web/img/favicon/android-icon-36x36.png
+convert -size  32x32 xc:transparent ./static/web/img/favicon/favicon-32x32.png
 convert -size 16x16 xc:transparent web/public/favicon.ico
 convert -size 48x48 xc:transparent web/favicon.ico 
 convert -size 16x16 xc:transparent static/web/favicon.ico
