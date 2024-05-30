39d84117e4bf4d0d8824fe7b9d36bcc0e6b502f0
Muaz Ahmad <mahmad2000@protonmail.com>
2024-05-30 12:57:22 +0500

    Remove old implementation, add new function to work with the chi router

--- utils/restendpointhelper.go
+++ utils/restendpointhelper.go
@@ -2,68 +2,16 @@ package utils
 
 import (
 	"errors"
-	"fmt"
 	"net/http"
-	"strings"
+
+	"github.com/go-chi/chi/v5"
 )
 
-const RestURLPatternHeaderKey = "Owncast-Resturl-Pattern"
-
-// takes the segment pattern of an Url string and returns the segment before the first dynamic REST parameter.
-func getPatternForRestEndpoint(pattern string) string {
-	firstIndex := strings.Index(pattern, "/{")
-	if firstIndex == -1 {
-		return pattern
+// GetURLParam retrieves the specified URL param from a given request.
+func GetURLParam(r *http.Request, key string) (value string, err error) {
+	value = chi.URLParam(r, key)
+	if value == "" {
+		err = errors.New("Request does not contain requested URL param")
 	}
-
-	return strings.TrimRight(pattern[:firstIndex], "/") + "/"
-}
-
-func zip2D(iterable1 *[]string, iterable2 *[]string) map[string]string {
-	dict := make(map[string]string)
-	for index, key := range *iterable1 {
-		dict[key] = (*iterable2)[index]
-	}
-	return dict
-}
-
-func mapPatternWithRequestURL(pattern string, requestURL string) (map[string]string, error) {
-	patternSplit := strings.Split(pattern, "/")
-	requestURLSplit := strings.Split(requestURL, "/")
-
-	if len(patternSplit) == len(requestURLSplit) {
-		return zip2D(&patternSplit, &requestURLSplit), nil
-	}
-	return nil, errors.New("the length of pattern and request Url does not match")
-}
-
-func readParameter(pattern string, requestURL string, paramName string) (string, error) {
-	all, err := mapPatternWithRequestURL(pattern, requestURL)
-	if err != nil {
-		return "", err
-	}
-
-	if value, exists := all[fmt.Sprintf("{%s}", paramName)]; exists {
-		return value, nil
-	}
-	return "", fmt.Errorf("parameter with name %s not found", paramName)
-}
-
-// ReadRestURLParameter will return the parameter from the request of the requested name.
-func ReadRestURLParameter(r *http.Request, parameterName string) (string, error) {
-	pattern, found := r.Header[RestURLPatternHeaderKey]
-	if !found {
-		return "", fmt.Errorf("this HandlerFunc is not marked as REST-Endpoint. Cannot read Parameter '%s' from Request", parameterName)
-	}
-
-	return readParameter(pattern[0], r.URL.Path, parameterName)
-}
-
-// RestEndpoint wraps a handler to use the rest endpoint helper.
-func RestEndpoint(pattern string, handler http.HandlerFunc) (string, http.HandlerFunc) {
-	baseURL := getPatternForRestEndpoint(pattern)
-	return baseURL, func(w http.ResponseWriter, r *http.Request) {
-		r.Header[RestURLPatternHeaderKey] = []string{pattern}
-		handler(w, r)
-	}
+	return
 }
