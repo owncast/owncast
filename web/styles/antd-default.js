5c07071bd617d5488da6a662082c6f6548d5aab2
Muaz Ahmad <mahmad2000@protonmail.com>
2024-06-12 14:54:31 +0500

    Add a better default theme for styleprovider

---
+++ web/styles/antd-default.js
@@ -0,0 +1,24 @@
+// TODO generate this file using style-definitions somehow
+
+export const AntdDefaultTokens = {
+  colorLink: '#6544e9',
+  colorLinkHover: '#7a5cf3',
+  Modal: {
+    headerBg: '#2d3748',
+    contentBg: '#e2e8f0',
+    colorIcon: '#e2e8f0'
+  },
+  Alert: {
+    colorErrorBg: '#fff',
+    colorErrorBorder: '#ff4b39'
+  },
+  colorBgElevated: '#e2e8f0',
+  Tag: {
+    defaultBg: '#6544e9',
+  },
+  borderRadius: 5,
+  colorPrimary: '#6544e9',
+  colorPrimaryHover: '#2386e2',
+  colorPrimaryActive: '#7a5cf3',
+  colorBgBase: '#e2e8f0',
+};
