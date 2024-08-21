2d9f7149d3bf1875a2a170cac764968989366830
Gabe Kangas <gabek@real-ity.com>
2024-08-21 14:09:03 -0700

    fix(test): remove done callback in tests

--- test/automated/api/003_chat.test.js
+++ test/automated/api/003_chat.test.js
@@ -15,15 +15,15 @@ const testMessage = {
 	type: 'CHAT',
 };
 
-test('send a chat message', async (done) => {
+test('send a chat message', async () => {
 	const registration = await registerChat();
 	const accessToken = registration.accessToken;
 	userDisplayName = registration.displayName;
 
-	sendChatMessage(testMessage, accessToken, done);
+	sendChatMessage(testMessage, accessToken);
 });
 
-test('fetch chat messages by admin', async (done) => {
+test('fetch chat messages by admin', async () => {
 	const res = await getAdminResponse('chat/messages');
 	const expectedBody = `<p>` + testMessage.body + `</p>`;
 
@@ -35,26 +35,22 @@ test('fetch chat messages by admin', async (done) => {
 	expect(message.body).toBe(expectedBody);
 	expect(message.user.displayName).toBe(userDisplayName);
 	expect(message.type).toBe(testMessage.type);
-
-	done();
 });
 
-test('derive display name from user header', async (done) => {
+test('derive display name from user header', async () => {
 	const res = await request
 		.post('/api/chat/register')
 		.set('X-Forwarded-User', 'test-user')
 		.expect(200);
 
 	expect(res.body.displayName).toBe('test-user');
-	done();
 });
 
-test('overwrite user header derived display name with body', async (done) => {
+test('overwrite user header derived display name with body', async () => {
 	const res = await request
 		.post('/api/chat/register')
 		.send({ displayName: 'TestUserChat' })
 		.expect(200);
 
 	expect(res.body.displayName).toBe('TestUserChat');
-	done();
 });
--- test/automated/api/lib/chat.js
+++ test/automated/api/lib/chat.js
@@ -3,41 +3,41 @@ request = request('http://127.0.0.1:8080');
 const WebSocket = require('ws');
 
 async function registerChat() {
-  try {
-    const response = await request.post('/api/chat/register');
-    return response.body;
-  } catch (e) {
-    console.error(e);
-  }
+	try {
+		const response = await request.post('/api/chat/register');
+		return response.body;
+	} catch (e) {
+		console.error(e);
+	}
 }
 
 async function sendChatMessage(message, accessToken, done) {
-  const ws = new WebSocket(`ws://localhost:8080/ws?accessToken=${accessToken}`);
+	const ws = new WebSocket(`ws://localhost:8080/ws?accessToken=${accessToken}`);
 
-  async function onOpen() {
-    ws.send(JSON.stringify(message), async function () {
-      ws.close();
-      done();
-    });
-  }
+	async function onOpen() {
+		ws.send(JSON.stringify(message), async function () {
+			ws.close();
+			// done();
+		});
+	}
 
-  ws.on('open', onOpen);
+	ws.on('open', onOpen);
 }
 
 async function listenForEvent(name, accessToken, done) {
-  const ws = new WebSocket(`ws://localhost:8080/ws?accessToken=${accessToken}`);
+	const ws = new WebSocket(`ws://localhost:8080/ws?accessToken=${accessToken}`);
 
-  ws.on('message', function incoming(message) {
-    const messages = message.split('\n');
-    messages.forEach(function (message) {
-      const event = JSON.parse(message);
+	ws.on('message', function incoming(message) {
+		const messages = message.split('\n');
+		messages.forEach(function (message) {
+			const event = JSON.parse(message);
 
-      if (event.type === name) {
-        done();
-        ws.close();
-      }
-    });
-  });
+			if (event.type === name) {
+				done();
+				ws.close();
+			}
+		});
+	});
 }
 
 module.exports.sendChatMessage = sendChatMessage;
