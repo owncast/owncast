4d332a494b0fd4619390e839d099becfe184444f
zockicookie <133583695+zockicookie@users.noreply.github.com>
2024-09-09 03:40:41 +0200

    parked test for statusbar string

--- web/components/ui/Statusbar/Statusbar.tsx
+++ web/components/ui/Statusbar/Statusbar.tsx
@@ -18,16 +18,8 @@ export type StatusbarProps = {
   className?: string;
 };
 
-function makeDurationString(lastConnectTime: Date): string {
+export function makeDurationString(lastConnectTime: Date): string {
   const diff = intervalToDuration({ start: lastConnectTime, end: new Date() });
-  
-  // fallback values in case undefined or null is passed to diff values
-  diff.years = diff.years ?? 0;
-  diff.weeks = diff.weeks ?? 0;
-  diff.days = diff.days ?? 0;
-  diff.hours = diff.hours ?? 0;
-  diff.minutes = diff.minutes ?? 0;
-  diff.seconds = diff.seconds ?? 0;
   
   if (diff.days >= 1) {
     return formatDuration({
