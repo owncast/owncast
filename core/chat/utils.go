4a59a6fa5606095c74231253ae3a39de49d0c7e6
Gabe Kangas <gabek@real-ity.com>
2024-09-17 08:11:35 -0700

    fix(go): int64 -> uint64 for connection limit value

--- core/chat/utils.go
+++ core/chat/utils.go
@@ -9,14 +9,14 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-func getMaximumConcurrentConnectionLimit() int64 {
+func getMaximumConcurrentConnectionLimit() uint64 {
 	var rLimit syscall.Rlimit
 	if err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rLimit); err != nil {
 		log.Fatalln(err)
 	}
 
 	// Return the limit to 70% of max so the machine doesn't die even if it's maxed out for some reason.
-	proposedLimit := int64(float32(rLimit.Max) * 0.7)
+	proposedLimit := uint64(float32(rLimit.Max) * 0.7)
 
 	return proposedLimit
 }
