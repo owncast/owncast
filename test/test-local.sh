f5e323375894bcb2398d90cb41277ed391a7a677
Gabe Kangas <gabek@real-ity.com>
2024-12-09 10:37:40 -0800

    Update local tunneling script to no longer use serveo.net

--- test/test-local.sh
+++ test/test-local.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 # This script will make your local Owncast server available at a public URL.
-# It's particularly useful for testing on mobile devices.
+# It's particularly useful for testing on mobile devices or want to test
+# activitypub integration.
 
 PORT=8080
-USERNAME=$(whoami)
-BRANCH=$(git symbolic-ref --short -q HEAD | sed 's/[^a-z A-Z 0-9]//g')
-HOSTNAME="oc-$USERNAME-$BRANCH"
+HOST="localhost"
 
-echo "Please wait. Making the local server port $PORT available at https://$HOSTNAME.serveo.net"
-ssh -R "$HOSTNAME".serveo.net:80:localhost:$PORT serveo.net
+# Using nc (netcat) to check if the port is open
+if nc -zv $HOST $PORT 2>/dev/null; then
+	echo "Your web server is running on port $PORT. Good."
+else
+	echo "Please make sure your Owncast server is running on port $PORT."
+	exit 1
+fi
+
+ssh -R 80:localhost:$PORT localhost.run
