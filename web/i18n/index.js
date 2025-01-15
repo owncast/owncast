5b816590c15f8b424d0228bb797042d74e68279e
Gabe Kangas <gabek@real-ity.com>
2025-01-15 14:48:54 -0800

    chore(i18n): enable languages

--- web/i18n/index.js
+++ web/i18n/index.js
@@ -1,14 +1,54 @@
+const ar = require('./ar/translation.json');
+const bn = require('./bn/translation.json');
+const de = require('./de/translation.json');
+const el = require('./el/translation.json');
 const en = require('./en/translation.json');
-// const es = require('./es/translation.json');
-// const de = require('./de/translation.json');
-// const fr = require('./fr/translation.json');
+const es = require('./es/translation.json');
+const fr = require('./fr/translation.json');
+const ga = require('./ga/translation.json');
+const hi = require('./hi/translation.json');
+const hr = require('./hr/translation.json');
+const it = require('./it/translation.json');
+const ja = require('./ja/translation.json');
+const ko = require('./ko/translation.json');
+const ms = require('./ms/translation.json');
+const nl = require('./nl/translation.json');
+const no = require('./no/translation.json');
+const pa = require('./pa/translation.json');
+const pl = require('./pl/translation.json');
+const pt = require('./pt/translation.json');
+const ru = require('./ru/translation.json');
+const sv = require('./sv/translation.json');
+const th = require('./th/translation.json');
+const vi = require('./vi/translation.json');
+const zh = require('./zh/translation.json');
 
 const i18n = {
   translations: {
+    ar,
+    bn,
+    de,
+    el,
     en,
-    // es,
-    // de,
-    // fr,
+    es,
+    fr,
+    ga,
+    hi,
+    hr,
+    it,
+    ja,
+    ko,
+    ms,
+    nl,
+    no,
+    pa,
+    pl,
+    pt,
+    ru,
+    sv,
+    th,
+    vi,
+    zh,
   },
   defaultLang: 'en',
   useBrowserDefault: true,
