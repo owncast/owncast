b77b399c7dd7ac401a520ad60cbc18091edc422b
Gabe Kangas <gabek@real-ity.com>
2025-05-20 20:27:15 -0700

    chore(web): remove unpopulated meta tags. Closes #4324

--- web/components/layouts/Main/Main.tsx
+++ web/components/layouts/Main/Main.tsx
@@ -98,41 +98,7 @@ export const Main: FC = () => {
       </Head>
 
       {isProduction ? (
-        <Head>
-          {name ? <title>{name}</title> : <title>{'{{.Name}}'}</title>}
-          <meta name="description" content="{{.Summary}}" />
-
-          <meta property="og:title" content="{{.Name}}" />
-          <meta property="og:site_name" content="{{.Name}}" />
-          <meta property="og:url" content="{{.RequestedURL}}" />
-          <meta property="og:description" content="{{.Summary}}" />
-          <meta property="og:type" content="video.other" />
-          <meta property="video:tag" content="{{.TagsString}}" />
-
-          <meta property="og:image" content="{{.RequestedURL}}{{.Thumbnail}}" />
-          <meta property="og:image:url" content="{{.RequestedURL}}{{.Thumbnail}}" />
-          <meta property="og:image:alt" content="{{.RequestedURL}}{{.Image}}" />
-
-          <meta property="og:video" content="{{.RequestedURL}}hls/stream.m3u8" />
-          <meta property="og:video:secure_url" content="{{.RequestedURL}}hls/stream.m3u8" />
-          <meta property="og:video:type" content="application/x-mpegURL" />
-
-          <meta property="og:video" content="{{.RequestedURL}}{{.EmbedVideo}}" />
-          <meta property="og:video:secure_url" content="{{.RequestedURL}}{{.EmbedVideo}}" />
-          <meta property="og:video:height" content="315" />
-          <meta property="og:video:width" content="560" />
-          <meta property="og:video:type" content="text/html" />
-          <meta property="og:video:actor" content="{{.Name}}" />
-
-          <meta property="twitter:title" content="{{.Name}}" />
-          <meta property="twitter:url" content="{{.RequestedURL}}" />
-          <meta property="twitter:description" content="{{.Summary}}" />
-          <meta property="twitter:image" content="{{.Image}}" />
-          <meta property="twitter:card" content="player" />
-          <meta property="twitter:player" content="{{.RequestedURL}}{{.EmbedVideo}}" />
-          <meta property="twitter:player:width" content="560" />
-          <meta property="twitter:player:height" content="315" />
-        </Head>
+        <Head>{name ? <title>{name}</title> : <title>{'{{.Name}}'}</title>}</Head>
       ) : (
         <Head>
           <title>{name}</title>
