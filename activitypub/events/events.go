ef5d6967b7f7499780990e2fbf5907b54743f6b5
olive <oambrois@uwaterloo.ca>
2025-05-29 11:06:35 +0400

    Removes unnecessary Type fiels from Events struct

--- activitypub/events/events.go
+++ activitypub/events/events.go
@@ -3,15 +3,13 @@ package events
 import (
 	"time"
 
-	"github.com/owncast/owncast/models"
 	"github.com/teris-io/shortid"
 )
 
-// Event is any kind of event.  A type is required to be specified.
+// Event is any kind of event.
 type Event struct {
-	Timestamp time.Time        `json:"timestamp"`
-	Type      models.EventType `json:"type,omitempty"`
-	ID        string           `json:"id"`
+	Timestamp time.Time `json:"timestamp"`
+	ID        string    `json:"id"`
 }
 
 // SetDefaults will set default properties of all inbound events.
