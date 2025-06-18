811b422a9fb3cd91f157c10192a33fa196fa7f17
olive <oambrois@uwaterloo.ca>
2025-06-18 17:37:25 +0400

    fix: fixes linter errors

--- web/components/ui/OfflineEmbed/OfflineEmbed.tsx
+++ web/components/ui/OfflineEmbed/OfflineEmbed.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable react/no-danger */
 import React, { useContext, useState } from 'react';
 import Head from 'next/head';
-import styles from './OfflineEmbed.module.scss';
 import { Typography } from 'antd';
+import styles from './OfflineEmbed.module.scss';
 import { Modal } from '../Modal/Modal';
 import { FollowForm } from '../../modals/FollowModal/FollowForm';
 import { ServerStatusContext } from '../../../utils/server-status-context';
@@ -16,6 +16,27 @@ export type OfflineEmbedProps = {
   supportsFollows: boolean;
 };
 
+const Header = ({ instanceUrl, image, streamName }) =>
+  instanceUrl ? (
+    <a href={instanceUrl} className={styles.header}>
+      <div className={styles.logo} style={{ backgroundImage: `url(${image})` }} />
+      <div className={styles.title}>
+        <Title level={1} className={styles.text} ellipsis={{ rows: 2 }}>
+          {streamName}
+        </Title>
+      </div>
+    </a>
+  ) : (
+    <div className={styles.header}>
+      <div className={styles.logo} style={{ backgroundImage: `url(${image})` }} />
+      <div className={styles.title}>
+        <Title level={1} className={styles.text} ellipsis={{ rows: 2 }}>
+          {streamName}
+        </Title>
+      </div>
+    </div>
+  );
+
 export const OfflineEmbed = ({
   streamName,
   image,
@@ -26,34 +47,13 @@ export const OfflineEmbed = ({
 
   const { serverConfig } = useContext(ServerStatusContext);
 
-  const Header = ({ instanceUrl }) =>
-    instanceUrl ? (
-      <a href={serverConfig.yp.instanceUrl} className={styles.header}>
-        <div className={styles.logo} style={{ backgroundImage: `url(${image})` }} />
-        <div className={styles.title}>
-          <Title level={1} className={styles.text} ellipsis={{ rows: 2 }}>
-            {streamName}
-          </Title>
-        </div>
-      </a>
-    ) : (
-      <div className={styles.header}>
-        <div className={styles.logo} style={{ backgroundImage: `url(${image})` }} />
-        <div className={styles.title}>
-          <Title level={1} className={styles.text} ellipsis={{ rows: 2 }}>
-            {streamName}
-          </Title>
-        </div>
-      </div>
-    );
-
   return (
     <div className={styles.canvas}>
       <Head>
         <title>{streamName}</title>
       </Head>
       <div className={styles.content}>
-        <Header instanceUrl={serverConfig.yp.instanceUrl} />
+        <Header instanceUrl={serverConfig.yp.instanceUrl} image={image} streamName={streamName} />
         <div className={styles.body}>
           <Title level={2} className={styles.text}>
             This stream is not currently live.
@@ -65,7 +65,9 @@ export const OfflineEmbed = ({
           )}
           {supportsFollows && (
             <>
-              <button onClick={() => setShowFollowModal(true)}>Follow</button>
+              <button type="button" onClick={() => setShowFollowModal(true)}>
+                Follow
+              </button>
               <Modal
                 title={`Follow ${streamName}`}
                 open={showFollowModal}
