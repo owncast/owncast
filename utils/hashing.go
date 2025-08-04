78c7ec41804e11ba5bf794661ab2866a2d8ece3a
Muaz Ahmad <mahmad2000@protonmail.com>
2025-08-04 19:12:14 +0500

    rename compress_hashing to go conventions

--- utils/hashing.go
+++ utils/hashing.go
@@ -7,19 +7,19 @@ import (
 )
 
 func HashPassword(password string) (string, error) {
-	password_bytes := compress_password([]byte(password))
+	password_bytes := compressPassword([]byte(password))
 	// 0 will use the default cost of 10 instead
 	hash, err := bcrypt.GenerateFromPassword(password_bytes, 0)
 	return string(hash), err
 }
 
 func ComparseHash(hash string, password string) error {
-	password_bytes := compress_password([]byte(password))
+	password_bytes := compressPassword([]byte(password))
 	return bcrypt.CompareHashAndPassword([]byte(hash), password_bytes)
 }
 
 // Takes a password and computes a sha-512 hash of it if it is longer than 72 bytes, guaranteeing it is less than 72 bytes long.
-func compress_password(password []byte) []byte {
+func compressPassword(password []byte) []byte {
 	if len(password) > 72 {
 		sha512_hashed := sha512.Sum512(password)
 		password = sha512_hashed[:]
