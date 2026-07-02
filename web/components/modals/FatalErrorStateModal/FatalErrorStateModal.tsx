95207f3984da49160f62b8662b7ed0e9c810e74d
Chitransh <cvxanonymous007@gmail.com>
2026-07-02 06:05:06 +0530

    feat: added closable fatal state modal (#4987)
    * feat: added closable fatal state modal
    
    * chore: lint

--- web/components/modals/FatalErrorStateModal/FatalErrorStateModal.tsx
+++ web/components/modals/FatalErrorStateModal/FatalErrorStateModal.tsx
@@ -1,22 +1,36 @@
 import { Modal } from 'antd';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 
 export type FatalErrorStateModalProps = {
   title: string;
   message: string;
 };
 
-export const FatalErrorStateModal: FC<FatalErrorStateModalProps> = ({ title, message }) => (
-  <Modal
-    title={title}
-    visible
-    footer={null}
-    closable={false}
-    keyboard={false}
-    width={900}
-    centered
-    className="modal"
-  >
-    <p style={{ fontSize: '1.3rem' }}>{message}</p>
-  </Modal>
-);
+export const FatalErrorStateModal: FC<FatalErrorStateModalProps> = ({ title, message }) => {
+  const [isOpen, setOpen] = useState(true);
+
+  const handleCancel = () => {
+    setOpen(false);
+  };
+
+  const handleOk = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Modal
+      title={title}
+      footer={null}
+      closable
+      keyboard={false}
+      width={900}
+      centered
+      open={isOpen}
+      onCancel={handleCancel}
+      onOk={handleOk}
+      className="modal"
+    >
+      <p style={{ fontSize: '1.3rem' }}>{message}</p>
+    </Modal>
+  );
+};
