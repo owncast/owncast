642c8424d343aacd78dbe9fb7b78fbff8849f748
Owncast <owncast@owncast.online>
2026-07-09 23:45:14 +0000

    Bundle embedded web app

---
+++ static/web/_next/static/k-1hh1pA3oNnNzu_Q-zm-/_buildManifest.js
@@ -0,0 +1,130 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/0dqbmivxlnaf5.js"
+  ],
+  "/_error": [
+    "static/chunks/29a_jpa_bvh0a.js"
+  ],
+  "/admin": [
+    "static/chunks/2kk4za1rs5yzr.js"
+  ],
+  "/admin/access-tokens": [
+    "static/chunks/1yqagpd6tzyye.js"
+  ],
+  "/admin/actions": [
+    "static/chunks/0i2k5c1qtsrhe.js"
+  ],
+  "/admin/chat/emojis": [
+    "static/chunks/0qd50kjoens06.js"
+  ],
+  "/admin/chat/messages": [
+    "static/chunks/1ute8d1y3uyj3.js"
+  ],
+  "/admin/config-chat": [
+    "static/chunks/364ocwdlw-nqu.js"
+  ],
+  "/admin/config-featured": [
+    "static/chunks/172mj5kn77jni.js"
+  ],
+  "/admin/config-federation": [
+    "static/chunks/1w1mkgcjjszxj.js"
+  ],
+  "/admin/config-notify": [
+    "static/chunks/2ldnhn-d75afh.js"
+  ],
+  "/admin/config-social-items": [
+    "static/chunks/2vmokixyoetvn.js"
+  ],
+  "/admin/config-video": [
+    "static/chunks/2fve5s65ca3o7.js"
+  ],
+  "/admin/config/general": [
+    "static/chunks/2570rz4ls02ua.js"
+  ],
+  "/admin/config/server": [
+    "static/chunks/3i17w_pcu743s.js"
+  ],
+  "/admin/federation/actions": [
+    "static/chunks/0islr79al_k4m.js"
+  ],
+  "/admin/federation/followers": [
+    "static/chunks/0tm31a71ierg2.js"
+  ],
+  "/admin/hardware-info": [
+    "static/chunks/0p8za7crusyqq.js"
+  ],
+  "/admin/help": [
+    "static/chunks/1uc-s-1x1h36r.js"
+  ],
+  "/admin/logs": [
+    "static/chunks/0nx8vc1i1nrm9.js"
+  ],
+  "/admin/plugins": [
+    "static/chunks/2cxdgl8pp1rqi.js"
+  ],
+  "/admin/plugins/configure": [
+    "static/chunks/3f051bzvs3khy.js"
+  ],
+  "/admin/stream-health": [
+    "static/chunks/1-tdvjc3c8n8j.js"
+  ],
+  "/admin/upgrade": [
+    "static/chunks/2c3irimpeuqej.js"
+  ],
+  "/admin/users": [
+    "static/chunks/2akgnu0xi53zz.js"
+  ],
+  "/admin/viewer-info": [
+    "static/chunks/3w2rsxtryz5bg.js"
+  ],
+  "/admin/webhooks": [
+    "static/chunks/3_cm426h8kv5x.js"
+  ],
+  "/embed/chat/readonly": [
+    "static/chunks/0c75x0d-r87lg.js"
+  ],
+  "/embed/chat/readwrite": [
+    "static/chunks/1r_4fa1s64o8t.js"
+  ],
+  "/embed/video": [
+    "static/chunks/1766rroe7cwh2.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error",
+    "/admin",
+    "/admin/access-tokens",
+    "/admin/actions",
+    "/admin/chat/emojis",
+    "/admin/chat/messages",
+    "/admin/config/general",
+    "/admin/config/server",
+    "/admin/config-chat",
+    "/admin/config-featured",
+    "/admin/config-federation",
+    "/admin/config-notify",
+    "/admin/config-social-items",
+    "/admin/config-video",
+    "/admin/federation/actions",
+    "/admin/federation/followers",
+    "/admin/hardware-info",
+    "/admin/help",
+    "/admin/logs",
+    "/admin/plugins",
+    "/admin/plugins/configure",
+    "/admin/stream-health",
+    "/admin/upgrade",
+    "/admin/users",
+    "/admin/viewer-info",
+    "/admin/webhooks",
+    "/embed/chat/readonly",
+    "/embed/chat/readwrite",
+    "/embed/video"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
