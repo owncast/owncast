dc96484f8a41d6f22807a5c46e2aeed99a5a6ff5
Owncast <owncast@owncast.online>
2026-07-19 23:07:22 +0000

    Bundle embedded web app

---
+++ static/web/_next/static/chunks/1b5y2nkpfhfv9.js
@@ -0,0 +1,20 @@
+__turbopack_load_page_chunks__("/embed/chat/readonly", [
+  "static/chunks/42scg7n9hqwj5.js",
+  "static/chunks/2t1_nx-h8e0a-.js",
+  "static/chunks/02if5pgqr6o9c.js",
+  "static/chunks/2-ubslnbs5nt-.js",
+  "static/chunks/3d59ns9k8l_eh.js",
+  "static/chunks/3nxwz_0i1y65m.js",
+  "static/chunks/3ks6c47u33lni.js",
+  "static/chunks/0yskmokwi_7db.js",
+  "static/chunks/08ncx1vjsvcx_.js",
+  "static/chunks/2ii8si9sk1wly.js",
+  "static/chunks/0tz6_10jr3hn4.js",
+  "static/chunks/20n6xtciqpoz0.js",
+  "static/chunks/3-37nv3rf10se.js",
+  "static/chunks/2xlx6h5xthqc-.js",
+  "static/chunks/0fw_mn16ugp1k.js",
+  "static/chunks/34q-ksq18l-e_.js",
+  "static/chunks/370r3ttpahoe8.css",
+  "static/chunks/turbopack-1wp6kks7tqx0a.js"
+])
