3e81257cc91d336a2d79d6b7a4e8a06aa4ed2410
Owncast <owncast@owncast.online>
2026-08-02 06:46:25 +0000

    Bundle embedded web app

---
+++ static/web/_next/static/chunks/2b188sy8yqtx4.js
@@ -0,0 +1,20 @@
+__turbopack_load_page_chunks__("/embed/chat/readonly", [
+  "static/chunks/2x5d7pm64q9fm.js",
+  "static/chunks/2l97fvoaofhed.js",
+  "static/chunks/02if5pgqr6o9c.js",
+  "static/chunks/2m87c-63bdo9w.js",
+  "static/chunks/027zv3g8irmme.js",
+  "static/chunks/3-37nv3rf10se.js",
+  "static/chunks/2bpq111ksiy6y.js",
+  "static/chunks/0yskmokwi_7db.js",
+  "static/chunks/08ncx1vjsvcx_.js",
+  "static/chunks/2ii8si9sk1wly.js",
+  "static/chunks/0tz6_10jr3hn4.js",
+  "static/chunks/3veqj_169c58s.js",
+  "static/chunks/2k8g7zb9bac3i.js",
+  "static/chunks/2xlx6h5xthqc-.js",
+  "static/chunks/0fw_mn16ugp1k.js",
+  "static/chunks/34q-ksq18l-e_.js",
+  "static/chunks/022mg-8k3zi0c.css",
+  "static/chunks/turbopack-0jah_30bq_kt1.js"
+])
