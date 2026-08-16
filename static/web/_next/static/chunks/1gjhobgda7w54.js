20753bd7c9bf807b55b7d0cb103f4abc2756371f
Owncast <owncast@owncast.online>
2026-08-16 04:52:37 +0000

    Bundle embedded web app

--- static/web/_next/static/chunks/1gjhobgda7w54.js
+++ static/web/_next/static/chunks/1gjhobgda7w54.js
@@ -1,5 +1,5 @@
 __turbopack_load_page_chunks__("/_app", [
-  "static/chunks/0dmi-b-g-y8tn.js",
+  "static/chunks/072z65q5m3wgq.js",
   "static/chunks/2bbj3xnn7n6mg.js",
   "static/chunks/2m87c-63bdo9w.js",
   "static/chunks/02if5pgqr6o9c.js",
@@ -11,5 +11,5 @@ __turbopack_load_page_chunks__("/_app", [
   "static/chunks/373088e7vtrza.css",
   "static/chunks/3gk_13ezf4fl_.css",
   "static/chunks/1nagaaiy_1kil.css",
-  "static/chunks/turbopack-2w0ga2-qd-ejd.js"
+  "static/chunks/turbopack-1f2t8robfqgw_.js"
 ])
